514e497c8dc980e0985becb48fb097841ada2585
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-01-31 19:07:09 +0100

    add install types

--- src/commands/install/install.py
+++ src/commands/install/install.py
@@ -1,7 +1,19 @@
+from typing_extensions import TypedDict
+from typing import Optional
 from .utils import verify_repo_url
+from enum import Enum
 
 
-def install(url: str):
+class InstallErrorEnum(Enum):
+    INCORRECT_URL = "INCORRECT_URL"
+
+
+InstallResultType = TypedDict(
+    "InstallResult", {"ok": bool, "error": Optional[InstallErrorEnum]}
+)
+
+
+def install(url: str) -> InstallResultType:
     if not verify_repo_url(url):
-        print("FAIL")
-    print("SUCCESS")
+        return {"ok": False, "error": InstallErrorEnum.INCORRECT_URL}
+    return {"ok": True, "error": None}
--- tests/commands/install/install_test.py
+++ tests/commands/install/install_test.py
@@ -2,4 +2,4 @@ from src.commands.install import install
 
 
 def test_install():
-    install(1)
+    install("foo")
