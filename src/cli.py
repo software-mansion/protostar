55f3c95e5faa6890d84d63de7ae6a564f8e35aaa
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-02-04 17:28:41 +0100

    Merge branch 'master' of https://github.com/software-mansion/protostar into allow-removing-dependencies

--- src/cli.py
+++ src/cli.py
@@ -1,11 +1,22 @@
 import os
+from logging import INFO, StreamHandler, getLogger
+
+from colorama import init as init_colorama
 
 from .commands import install, remove
+from .utils import StandardLogFormatter
 
+init_colorama()
 cwd = os.getcwd()
 
 
 def cli(args):
+    logger = getLogger()
+    logger.setLevel(INFO)
+    handler = StreamHandler()
+    handler.setFormatter(StandardLogFormatter())
+    logger.addHandler(handler)
+
     if args.command == "install":
         install(args.package, cwd)
     elif args.command == "remove":
--- src/commands/install/install.py
+++ src/commands/install/install.py
@@ -1,5 +1,7 @@
+from logging import getLogger
 from os import path
 
+from colorama import Fore
 from git import InvalidGitRepositoryError, NoSuchPathError
 from git.objects import Submodule
 from git.repo import Repo
@@ -13,6 +15,7 @@ def install(
     path_to_repo_root: str,
     destination="./lib",
 ):
+    logger = getLogger()
 
     try:
         repo = Repo(path_to_repo_root)
@@ -22,10 +25,22 @@ def install(
         raise installation_exceptions.InvalidLocalRepository()
 
     package_name = map_url_or_ssh_to_package_name(url_or_ssh)
+    package_dir = path.join(destination, package_name)
+
+    logger.info(
+        "Installing %s%s%s in %s %s(%s)%s",
+        Fore.CYAN,
+        package_name,
+        Fore.RESET,
+        package_dir,
+        Fore.LIGHTBLACK_EX,
+        url_or_ssh,
+        Fore.RESET,
+    )
 
     Submodule.add(
         repo,
         package_name,
-        path.join(destination, package_name),
+        package_dir,
         url_or_ssh,
     )
