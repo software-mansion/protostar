d8b1522bcb341f02332970166d1ee17e36d5914c
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-02-08 15:28:23 +0100

    add base test

--- src/commands/update/test_update_package.py
+++ src/commands/update/test_update_package.py
@@ -1,6 +1,9 @@
 from os import path
+from typing import Optional
 
 import pytest
+from git.cmd import Git
+from git.repo import Repo
 
 from src.commands.update.update_package import update_package
 
@@ -10,29 +13,62 @@ def fixture_package_name() -> str:
     return "foobar"
 
 
-@pytest.fixture(name="repo_root_dir")
+@pytest.fixture(name="repo_dir")
 def fixture_repo_root_dir(tmpdir) -> str:
     return tmpdir
 
 
 @pytest.fixture(name="packages_dir")
-def fixture_packages_dir(repo_root_dir) -> str:
-    return path.join(repo_root_dir, "./lib")
+def fixture_packages_dir(repo_dir) -> str:
+    return path.join(repo_dir, "./lib")
 
 
-def test_updating_specific_package(
-    package_name: str, repo_root_dir: str, packages_dir: str
+@pytest.fixture(name="repo")
+def fixture_repo(repo_dir):
+    return Repo().init(repo_dir)
+
+
+@pytest.fixture(name="current_tag")
+def fixture_current_tag() -> Optional[str]:
+    return None
+
+
+@pytest.fixture(name="submodule")
+def fixture_submodule(
+    repo: Repo, package_name: str, packages_dir: str, current_tag: Optional[str]
 ):
-    update_package(package_name, repo_root_dir, packages_dir)
+    package_dir = path.join(packages_dir, package_name)
+    submodule = repo.create_submodule(
+        package_name,
+        package_dir,
+        "https://github.com/software-mansion/starknet.py",
+        current_tag,
+    )
+    repo.git.add(submodule.path)
+    repo.index.commit("add submodule")
+    return submodule
 
 
-def test_updating_all_packages():
-    pass
+@pytest.mark.usefixtures("submodule")
+@pytest.mark.parametrize("current_tag", ["0.1.0-alpha"])
+def test_updating_specific_package(package_name: str, repo_dir: str, packages_dir: str):
+    cmd = Git(path.join(packages_dir, package_name))
+    current_tag = cmd.execute(["git", "describe", "--tags"])
+    assert current_tag == "0.1.0-alpha"
+
+    update_package(package_name, repo_dir, packages_dir)
+
+    new_tag = cmd.execute(["git", "describe", "--tags"])
+    assert new_tag != "0.1.0-alpha"
 
 
-def test_updating_when_repo_not_initiated():
-    pass
+# def test_updating_all_packages():
+#     pass
 
 
-def test_updating_not_existing_package():
-    pass
+# def test_updating_when_repo_not_initiated():
+#     pass
+
+
+# def test_updating_not_existing_package():
+#     pass
--- src/commands/update/update_package.py
+++ src/commands/update/update_package.py
@@ -1,22 +1,33 @@
-from typing import cast
+from os import path
+from typing import Optional, cast
 
 from git.cmd import Git
+from git.exc import GitCommandError
 from git.objects import Submodule
 from git.repo import Repo
 
 
-def update_package(package_name: str, repo_root_dir: str, _packages_dir: str):
+def update_package(package_name: str, repo_root_dir: str, packages_dir: str):
     repo = Repo(repo_root_dir)
 
     submodule = repo.submodule(package_name)
 
-    print(submodule.path)
+    cmd = Git(path.join(packages_dir, package_name))
 
-    cmd = Git(submodule.path)
-    current_tag = cmd.execute(["git", "describe", "--tags"])
+    current_tag = Optional[str]
+    try:
+        current_tag = cmd.execute(["git", "describe", "--tags"])
+    except GitCommandError as _err:
+        current_tag = None
+
     cmd.execute(["git", "fetch", "--tags"])
-    rev = cmd.execute(["git", "rev-list", "--tags", "--max-count=1"])
-    latest_tag = cast(str, cmd.execute(["git", "describe", "--tags", rev]))
+
+    latest_tag = Optional[str]
+    try:
+        rev = cmd.execute(["git", "rev-list", "--tags", "--max-count=1"])
+        latest_tag = cast(str, cmd.execute(["git", "describe", "--tags", rev]))
+    except GitCommandError as _err:
+        latest_tag = None
 
     if latest_tag != current_tag:
         package_url = submodule.url
