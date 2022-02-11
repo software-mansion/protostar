cce24dd8bf7406cfffb3a12d000163a207730069
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-02-11 14:17:53 +0100

    Implement the script which downloads and installs protostar

--- install.sh
+++ install.sh
@@ -5,9 +5,42 @@ echo Installing protostar
 
 PROTOSTAR_DIR=${PROTOSTAR_DIR-"$HOME/.protostar"}
 PROTOSTAR_BIN_DIR="$PROTOSTAR_DIR/bin"
-mkdir -p $PROTOSTAR_BIN_DIR
+mkdir -p "$PROTOSTAR_BIN_DIR"
 
-PROTOSTAR_REPO="https://github.com/software-mansion/protostar/" # TODO: Pull binary from repo
+
+PLATFORM="$(uname -s)"
+case $PLATFORM in
+  Linux)
+    PLATFORM="Linux"
+    ;;
+  Darwin)
+    PLATFORM="macOS"
+    ;;
+  *)
+    echo "unsupported platform: $PLATFORM"
+    exit 1
+    ;;
+esac
+
+PROTOSTAR_REPO="https://github.com/software-mansion/protostar"
+
+echo Retrieving the latest version from $PROTOSTAR_REPO...
+
+LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' "${PROTOSTAR_REPO}/releases/latest")
+LATEST_VERSION=$(echo $LATEST_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+
+echo Using version $LATEST_VERSION
+
+LATEST_RELEASE_URL="${PROTOSTAR_REPO}/releases/download/${LATEST_VERSION}"
+PROTOSTAR_EXECUTABLE_NAME="protostar-${PLATFORM}"
+BINARY_DOWNLOAD_URL="${LATEST_RELEASE_URL}/${PROTOSTAR_EXECUTABLE_NAME}"
+
+echo "Downloading binary from ${BINARY_DOWNLOAD_URL}"
+curl -L $BINARY_DOWNLOAD_URL --output protostar
+
+BINARY_DESTINATION="${PROTOSTAR_BIN_DIR}/protostar"
+mv "./protostar" $BINARY_DESTINATION
+chmod +x $BINARY_DESTINATION
 
 case $SHELL in
 */zsh)
