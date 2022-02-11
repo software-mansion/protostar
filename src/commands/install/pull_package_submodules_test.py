e595e0011357886eec14bda1bc0883bb43a5bdff
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-02-11 17:09:04 +0100

    add test

--- src/commands/install/pull_package_submodules_test.py
+++ src/commands/install/pull_package_submodules_test.py
@@ -1,7 +1,12 @@
-from os import mkdir, path
+import os
+from os import mkdir, name, path
+from time import sleep
+from unittest.mock import MagicMock
 
 import pytest
+from git import Git
 from git.repo import Repo
+from pytest_mock import MockerFixture
 
 from src.commands.install.pull_package_submodules import pull_package_submodules
 
@@ -12,6 +17,21 @@ from src.commands.install.pull_package_submodules import pull_package_submodules
 #   - ...
 # - package_repo
 #   - foo.txt
+
+
+@pytest.fixture(name="packages_dir_name")
+def fixture_packages_dir_name() -> str:
+    return "lib"
+
+
+@pytest.fixture(name="the_packages_file_name")
+def fixture_the_packages_file_name() -> str:
+    return "foo.txt"
+
+
+@pytest.fixture(name="the_package_name")
+def fixture_the_package_name() -> str:
+    return "package"
 
 
 @pytest.fixture(name="repo_dir")
@@ -29,23 +49,101 @@ def fixture_package_repo_dir(tmpdir: str) -> str:
     return path.join(tmpdir, "package_repo")
 
 
-@pytest.fixture
-def fixture_package_repo(package_repo_dir: str) -> Repo:
+@pytest.fixture(name="package_repo")
+def fixture_package_repo(package_repo_dir: str, the_packages_file_name: str) -> Repo:
     repo = Repo.init(package_repo_dir)
 
-    with open(path.join(package_repo_dir, "foo.txt"), "w", encoding="utf-8") as file:
+    the_file_path = path.join(package_repo_dir, the_packages_file_name)
+    with open(path.join(the_file_path), "w", encoding="utf-8") as file:
         file.write("foo")
+
+    repo.git.add("-A")
+    repo.index.commit("add foo")
+
+    assert path.exists(the_file_path)
 
     return repo
 
 
-@pytest.fixture
-def fixture_repo(repo_dir: str) -> Repo:
-    return Repo.init(repo_dir)
+@pytest.fixture(name="repo")
+# pylint: disable=unused-argument
+def fixture_repo(
+    repo_dir: str,
+    package_repo_dir: str,
+    packages_dir_name: str,
+    the_package_name: str,
+    package_repo: Repo,
+) -> Repo:
+    repo = Repo.init(repo_dir)
+
+    packages_dir = path.join(repo_dir, packages_dir_name)
+    package_dir = path.join(packages_dir, the_package_name)
+    mkdir(packages_dir)
+    repo.create_submodule(the_package_name, package_dir, package_repo_dir)
+
+    repo.git.add()
+    repo.index.commit("add submodule")
+
+    return repo
+
+
+@pytest.fixture(name="repo_clone")
+# pylint: disable=unused-argument
+def fixture_repo_clone(
+    repo_clone_dir: str, repo_dir: str, repo: Repo, packages_dir_name: str
+) -> Repo:
+    # cloned_repo =
+    git = Git()
+    git.execute(["git", "clone", repo_dir, repo_clone_dir])
+
+    assert path.exists(
+        path.join(repo_clone_dir, packages_dir_name)
+    ), "./lib exists in the cloned repo directory"
+    return Repo(repo_clone_dir)
 
 
 # ----------------------------------- TESTS ---------------------------------- #
 
 
-def test_pulling_all_package_submodules() -> None:
-    return
+def test_pulling_all_package_submodules(
+    repo_clone: Repo,
+    repo_dir: str,
+    repo_clone_dir: str,
+    packages_dir_name: str,
+    the_package_name: str,
+    the_packages_file_name: str,
+    mocker: MockerFixture,
+) -> None:
+    assert (
+        path.exists(
+            path.join(
+                repo_clone_dir,
+                packages_dir_name,
+                the_package_name,
+                the_packages_file_name,
+            )
+        )
+        is False
+    )
+    os.chdir(repo_clone_dir)
+    # callback: MagicMock = mocker.MagicMock()
+    pull_package_submodules(on_submodule_update_start=lambda arg: None)
+
+    print(
+        os.listdir(
+            path.join(
+                repo_clone_dir,
+                packages_dir_name,
+                the_package_name,
+            )
+        )
+    )
+
+    assert path.exists(
+        path.join(
+            repo_clone_dir,
+            packages_dir_name,
+            the_package_name,
+            the_packages_file_name,
+        )
+    )
