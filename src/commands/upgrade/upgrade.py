ffe3298b3d24efa20ff7b7ea47ffe7019a39247c
Maksymilian Demitraszek <maksymilian.demitraszek@gmail.com>
2022-02-16 16:51:40 +0100

    refactor, improved logging

--- src/commands/upgrade/upgrade.py
+++ src/commands/upgrade/upgrade.py
@@ -6,48 +6,105 @@ import requests
 from urllib.request import urlretrieve
 import tarfile
 
-logget = getLogger()
+from packaging import version
+
+logger = getLogger()
 
 PROTOSTAR_REPO = "https://github.com/software-mansion/protostar"
-PROTOSTAR_DIR = "~/.protostar"
-
-
-def get_latest_release():
-    headers = {'Accept': 'application/json'}
-    response = requests.get(
-        f'{PROTOSTAR_REPO}/releases/latest',
-        headers=headers
-    )
-    return response.json()
-
-
-def get_platform():
-    platform = os.uname()[0]
-    if platform == "Darwin":
-        return "macOS"
-    if platform == "Linux":
-        return "Linux"
+PROTOSTAR_DIR = Path.home() / ".protostar"
 
 
 def upgrade() -> None:
-    platform = get_platform()
-    lastest_release = get_latest_release()
-    # TODO check if up to date then skip
+    manager = UpgradeManager(PROTOSTAR_DIR)
+    manager.upgrade()
 
-    old_version = PROTOSTAR_DIR / "previous_version_tmp"
-    shutil.move(PROTOSTAR_DIR / "dist", old_version)
 
-    tarball_name = f"protostar-{platform}.tar.gz"
-    tarball_loc = PROTOSTAR_DIR / tarball_name
+class UpgradeManagerException(Exception):
+    pass
 
-    download_url = f"{PROTOSTAR_REPO}/releases/download/{lastest_release['tag_name']}/{tarball_name}"
-    urlretrieve(download_url, tarball_loc)
 
-    tar = tarfile.open(tarball_loc, "r:gz")
-    tar.extractall(PROTOSTAR_DIR)
-    tar.close()
+class UpgradeManager:
+    def __init__(self, protostar_dir: Path):
+        assert os.path.isdir(protostar_dir)
+        assert os.path.isdir(protostar_dir / "dist")
+        self.protostar_dir = protostar_dir
+        self.old_version = protostar_dir / "previous_version_tmp"
 
-    # rollback
-    os.remove(tarball_loc)
-    shutil.rmtree(old_version)
-    return
+        platform = self.get_platform()
+        self.tarball_name = f"protostar-{platform}.tar.gz"
+        self.tarball_loc = PROTOSTAR_DIR / self.tarball_name
+
+        self.current_version = version.parse(self.get_current_version())
+
+        self.latest_version_tag = self.get_latest_release()['tag_name']
+        self.latest_version = version.parse(self.latest_version_tag)
+
+    def is_current_latest(self):
+        return self.latest_version == self.current_version
+    
+    def upgrade(self):
+        logger.info("Looking for a new version ...")
+        if self.latest_version == self.current_version:
+            logger.info("Protostar is up to date")
+            return
+
+        logger.info(f"Starting upgrade from version {self.current_version} to version {self.latest_version}")
+        self._backup()
+        try:
+            self._pull_tarball()
+            self._install_new_version()
+            self.cleanup()
+        except Exception as e:
+            logger.error("Upgrade failed")
+            self._rollback()
+            self.cleanup()
+            raise e
+
+    def _backup(self):
+        shutil.move(self.protostar_dir / "dist", self.old_version)
+
+    def _pull_tarball(self):
+        logger.info(f"Pulling latest binary, version: {self.latest_version}")
+        tar_url = f"{PROTOSTAR_REPO}/releases/download/{self.latest_version_tag}/{self.tarball_name}"
+        urlretrieve(tar_url, self.tarball_loc)
+
+    def _install_new_version(self):
+        logger.info(f"Installing latest Protostar version: {self.latest_version}")
+        with tarfile.open(self.tarball_loc, "r:gz") as tar:
+            tar.extractall(self.protostar_dir)
+
+    def _rollback(self):
+        logger.info(f"Rolling back to the version {self.current_version}")
+        shutil.rmtree(self.protostar_dir / "dist", ignore_errors=True)
+        shutil.move(self.old_version, self.protostar_dir / "dist")
+
+    def cleanup(self):
+        logger.info("Cleaning up after installation")
+        shutil.rmtree(self.old_version, ignore_errors=True)
+        try:
+            os.remove(self.tarball_loc)
+        except FileNotFoundError:
+            pass
+ 
+    @classmethod
+    def get_platform(cls):
+        platform = os.uname()[0]
+        if platform == "Darwin":
+            return "macOS"
+        if platform == "Linux":
+            return "Linux"
+        raise UpgradeManagerException(f"{platform} is not supported")
+
+    @classmethod
+    def get_latest_release(cls):
+        headers = {'Accept': 'application/json'}
+        response = requests.get(
+            f'{PROTOSTAR_REPO}/releases/latest',
+            headers=headers
+        )
+        return response.json()
+
+    @classmethod
+    def get_current_version(cls):
+        # TODO
+        return '0.0.0'
