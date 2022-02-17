3b6c8d4f0fe11782d73cffea88d86c25e44a8bb3
tomasz.rejowski <tomasz.rejowski@swmansion.com>
2022-02-17 13:02:05 +0100

    Add a simple test for parsing the pkg info

--- src/utils/config/package_test.py
+++ src/utils/config/package_test.py
@@ -1,4 +1,8 @@
+from pathlib import Path
+
 from src.utils.config.package import Package, PackageConfig
+
+current_directory = Path(__file__).parent
 
 
 def mock_package(mocker, contracts, libs_path) -> Package:
@@ -14,3 +18,16 @@ def mock_package(mocker, contracts, libs_path) -> Package:
     )
     mocker.patch.object(pkg, attribute="load_config").return_value = mock_config
     return pkg
+
+
+def test_parsing_pkg_info():
+    pkg = Package(project_root=Path(current_directory, "examples"))
+    config = pkg.load_config()
+    assert config.name == "testproj"
+    assert config.description == "descr"
+    assert config.license == "MIT"
+    assert config.version == "1.0"
+    assert config.authors == [
+        "tomekgsd@gmail.com",
+    ]
+    assert config.libs_path == "lib"
