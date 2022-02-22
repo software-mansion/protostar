68ec492e589bd011848ee9831a571bd5091e18c7
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-02-22 18:06:29 +0100

    add removing package test

--- tests/e2e/test_package_management.py
+++ tests/e2e/test_package_management.py
@@ -3,13 +3,42 @@ from os import listdir
 import pytest
 
 
-@pytest.mark.usefixtures("init")
-def test_package_installation(protostar):
+@pytest.fixture(name="install_package")
+# pylint: disable=unused-argument
+def fixture_install_package(init, protostar):
+    def install_package():
+        result = protostar(
+            ["install", "https://github.com/software-mansion/starknet.py"]
+        )
+        normalized_package_name = "starknet_py"
+        return (result, normalized_package_name)
+
+    return install_package
+
+
+def test_adding_package(install_package):
     with pytest.raises(FileNotFoundError):
-        assert "starknet_py" not in listdir("lib")
+        listdir("lib")
 
-    result = protostar(["install", "https://github.com/software-mansion/starknet.py"])
+    (result, normalized_package_name) = install_package()
 
-    print("result", result)
-    assert "Installing starknet_py" in result
+    assert f"Installing {normalized_package_name}" in result
+    assert normalized_package_name in listdir("lib")
+
+
+def test_updating_package(install_package, protostar):
+    install_package()
+
+    result = protostar(["update", "starknet_py"])
+
+    assert "Updating starknet_py" in result
     assert "starknet_py" in listdir("lib")
+
+
+def test_removing_package(install_package, protostar):
+    install_package()
+
+    result = protostar(["remove", "starknet_py"])
+
+    assert "Removing starknet_py" in result
+    assert "starknet_py" not in listdir("lib")
