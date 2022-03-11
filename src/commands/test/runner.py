ff85902696f8f8fb75d7091d9f61704f2379accd
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2022-03-11 14:38:48 +0100

    debugging CI

--- src/commands/test/runner.py
+++ src/commands/test/runner.py
@@ -135,4 +135,5 @@ class TestRunner:
             self._is_test_error_expected = False
 
     def except_revert(self):
+        raise BaseException("PATCHING WORKS")
         self._is_test_error_expected = True
