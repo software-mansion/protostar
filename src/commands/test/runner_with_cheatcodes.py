ed4a764fb19d20cdd1f4031ef38f2a1574e2f374
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-03-14 17:39:21 +0100

    add runner with cheatcodes

---
+++ src/commands/test/runner_with_cheatcodes.py
@@ -0,0 +1,93 @@
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Pattern
+
+from starkware.cairo.common.cairo_function_runner import CairoFunctionRunner
+from starkware.starknet.public.abi import get_selector_from_name
+
+from src.commands.test.cheatcodes.syscall_handler import CheatableSysCallHandler
+from src.commands.test.runner import TestRunner
+from src.utils.modules import replace_class
+
+
+class TestRunnerWithCheatcodes(TestRunner):
+    _cheatable_syscall_handler: Optional[CheatableSysCallHandler]
+
+    @replace_class(
+        "starkware.starknet.core.os.syscall_utils.BusinessLogicSysCallHandler",
+        CheatableSysCallHandler,
+    )
+    async def run_tests_in(
+        self,
+        src: Path,
+        match_pattern: Optional[Pattern] = None,
+        omit_pattern: Optional[Pattern] = None,
+    ):
+        original_run_from_entrypoint = CairoFunctionRunner.run_from_entrypoint
+        CairoFunctionRunner.run_from_entrypoint = (
+            self._get_run_from_entrypoint_with_custom_hint_locals(
+                CairoFunctionRunner.run_from_entrypoint
+            )
+        )
+        await super().run_tests_in(src, match_pattern, omit_pattern)
+        CairoFunctionRunner.run_from_entrypoint = original_run_from_entrypoint
+
+    def _get_run_from_entrypoint_with_custom_hint_locals(
+        self, fn_run_from_entrypoint: Any
+    ):
+        def modified_run_from_entrypoint(
+            *args,
+            **kwargs,
+        ):
+            if "hint_locals" in kwargs and kwargs["hint_locals"] is not None:
+                self._cheatable_syscall_handler = kwargs["hint_locals"][
+                    "syscall_handler"
+                ]
+                self._inject_cheats_into_hint_locals(
+                    kwargs["hint_locals"],
+                )
+
+            return fn_run_from_entrypoint(
+                *args,
+                **kwargs,
+            )
+
+        return modified_run_from_entrypoint
+
+    def _inject_cheats_into_hint_locals(self, hint_locals: Dict[str, Any]):
+        def register_cheatcode(func):
+            hint_locals[func.__name__] = func
+            return func
+
+        @register_cheatcode
+        def roll(blk_number: int):
+            self._cheatable_syscall_handler.set_block_number(blk_number)
+
+        @register_cheatcode
+        def warp(blk_timestamp: int):
+            self._cheatable_syscall_handler.set_block_timestamp(blk_timestamp)
+
+        @register_cheatcode
+        def start_prank(caller_address: int):
+            self._cheatable_syscall_handler.set_caller_address(caller_address)
+
+        @register_cheatcode
+        def stop_prank():
+            self._cheatable_syscall_handler.set_caller_address(None)
+
+        @register_cheatcode
+        def mock_call(contract_address: int, fn_name: str, ret_data: List[int]):
+            selector = get_selector_from_name(fn_name)
+            self._cheatable_syscall_handler.register_mock_call(
+                contract_address, selector=selector, ret_data=ret_data
+            )
+
+        @register_cheatcode
+        def clear_mock_call(contract_address: int, fn_name: str):
+            selector = get_selector_from_name(fn_name)
+            self._cheatable_syscall_handler.unregister_mock_call(
+                contract_address, selector
+            )
+
+        @register_cheatcode
+        def expect_revert():
+            self.expect_revert()
