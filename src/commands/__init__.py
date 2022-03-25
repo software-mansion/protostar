8ed49434e0b839f18967f9725f1a435238a2ae31
Maksymilian Demitraszek <maksymilian.demitraszek@gmail.com>
2022-03-25 14:23:37 +0100

    Added version flag

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -2,4 +2,4 @@ from src.commands.init import init
 from src.commands.install import handle_install_command
 from src.commands.remove import handle_remove_command, removal_exceptions
 from src.commands.update import handle_update_command
-from src.commands.upgrade import upgrade
+from src.commands.upgrade import upgrade, print_current_version
