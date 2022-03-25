ae75294eada0a2cc660a19840ad0e5dbd5e3a5aa
Maksymilian Demitraszek <32527780+MaksymilianDemitraszek@users.noreply.github.com>
2022-03-25 11:53:50 +0100

    Added `--existing` flag, adopting protostar to existing project (#87)
    * Added --only-config flag
    
    * Lint, rename
    
    * Added test
    
    * Fixed empty name bug
    
    * Added auto prompt
    
    * Lint
    
    * Lint
    
    * typefix
    
    * fix
    
    * bug fix
    
    * Added git handling
    
    * added case
    
    * Update protostar.py
    
    Co-authored-by: Arkadiusz <arkadiusz.kasprzyk@swmansion.com>
    
    Co-authored-by: Arkadiusz <arkadiusz.kasprzyk@swmansion.com>

--- src/commands/init/init.py
+++ src/commands/init/init.py
@@ -1,41 +1,67 @@
+import glob
 import shutil
 from pathlib import Path
+from typing import Any, Type
 
-from colorama import Fore
 from git.repo import Repo
+from git.exc import InvalidGitRepositoryError
+
 
 from src.utils import log_color_provider
 from src.utils.config.project import Project, ProjectConfig
 
 
-def init(script_root: str):
+def init(args: Any, script_root: str):
     """
     Creates init protostar project
     """
-    project_name = input(f"{Fore.CYAN}Project name: ")
+    project_creator = get_creator(args)
+    project_creator = project_creator(script_root)
+    project_creator.create()
 
-    project_root = Path() / project_name
-    copy_template(script_root, "default", project_root)
-    project = Project(project_root=project_root)
 
-    def colored_input(message: str):
+def input_yes_no(message: str) -> bool:
+    res = ProjectCreator.request_input(message + " [y/n]")
+    while res not in ("y", "n"):
+        res = ProjectCreator.request_input("Please provide one of the [y/n]")
+    return res == "y"
+
+
+class ProjectCreator:
+    def __init__(self, script_root: str):
+        self.script_root = script_root
+        self.config = ProjectConfig()
+
+    @staticmethod
+    def request_input(message: str):
         return input(
             f"{log_color_provider.get_color('CYAN')}{message}:{log_color_provider.get_color('RESET')} "
         )
 
-    project_description = colored_input("Project description")
-    author = colored_input("Author")
-    version = colored_input("Version")
-    project_license = colored_input("License")
-    lib_dir = colored_input("Libraries directory name (optional)") or "lib"
+    @staticmethod
+    def request_input_warning(message: str):
+        return input(
+            f"{log_color_provider.get_color('RED')}{message}:{log_color_provider.get_color('RESET')} "
+        )
 
-    lib_pth = Path(project_root, lib_dir)
+    def create(self):
+        self.interactive_input()
+        self.project_creation()
 
-    if lib_dir and not lib_pth.is_dir():
-        lib_pth.mkdir(parents=True)
-
-    project.write_config(
-        ProjectConfig(
+    def interactive_input(self):
+        project_name = ProjectCreator.request_input("Project name")
+        while project_name == "":
+            project_name = ProjectCreator.request_input_warning(
+                "Please provide a non-empty project name"
+            )
+        project_description = ProjectCreator.request_input("Project description")
+        author = ProjectCreator.request_input("Author")
+        version = ProjectCreator.request_input("Version")
+        project_license = ProjectCreator.request_input("License")
+        lib_dir = (
+            ProjectCreator.request_input("Libraries directory name (optional)") or "lib"
+        )
+        self.config = ProjectConfig(
             name=project_name,
             description=project_description,
             license=project_license,
@@ -43,11 +69,60 @@ def init(script_root: str):
             authors=[author],
             libs_path=lib_dir,
         )
-    )
 
-    Repo.init(project_root)
+    def project_creation(self):
+        project_root = Path() / self.config.name
+        self.copy_template(self.script_root, "default", project_root)
+        project = Project(project_root=project_root)
+
+        lib_pth = Path(project_root, self.config.libs_path)
+
+        if self.config.libs_path and not lib_pth.is_dir():
+            lib_pth.mkdir(parents=True)
+
+        project.write_config(self.config)
+
+        Repo.init(project_root)
+
+    @staticmethod
+    def copy_template(script_root: str, template_name: str, project_path: Path):
+        template_path = f"{script_root}/templates/{template_name}"
+        shutil.copytree(template_path, project_path)
 
 
-def copy_template(script_root: str, template_name: str, project_path: Path):
-    template_path = f"{script_root}/templates/{template_name}"
-    shutil.copytree(template_path, project_path)
+class OnlyConfigCreator(ProjectCreator):
+    def project_creation(self):
+        project_root = Path()
+
+        lib_pth = Path(project_root, self.config.libs_path)
+
+        if self.config.libs_path and not lib_pth.is_dir():
+            lib_pth.mkdir(parents=True)
+
+        project = Project(project_root=project_root)
+        project.write_config(self.config)
+
+        try:
+            Repo(project_root)
+        except InvalidGitRepositoryError:
+            Repo.init(project_root)
+
+
+def get_creator(args: Any) -> Type[ProjectCreator]:
+    if args.existing:
+        return OnlyConfigCreator
+
+    files_depth_3 = glob.glob("*") + glob.glob("*/*") + glob.glob("*/*/*")
+    can_be_a_project = any(
+        map(lambda f: f.endswith(".cairo") or f == ".git", files_depth_3)
+    ) and "protostar.toml" not in glob.glob("*")
+
+    out = False
+    if can_be_a_project:
+        out = input_yes_no(
+            "Your current directory may be a cairo project.\n"
+            "Do you want to adapt current working directory "
+            "as a project instead of creating a new project?."
+        )
+
+    return OnlyConfigCreator if out else ProjectCreator
