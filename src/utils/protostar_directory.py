784a68c47da7f4baf563457e968fb27d18722d53
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2022-04-06 15:54:52 +0200

    _add protostar_directory_test

--- src/utils/protostar_directory.py
+++ src/utils/protostar_directory.py
@@ -7,9 +7,17 @@ import tomli
 
 from packaging import version
 from packaging.version import LegacyVersion, Version as PackagingVersion
+from src.protostar_exception import ProtostarException
 
 
 class ProtostarDirectory:
+    class ProtostarNotInstalledException(ProtostarException):
+        def __init__(self):
+            super().__init__(
+                "Couldn't find the protostar binary\n"
+                "Did you add the protostar binary to the $PATH variable?"
+            )
+
     def __init__(self) -> None:
         self._protostar_binary_dir: Optional[Path] = None
         self._protostar_root_dir: Optional[Path] = None
@@ -20,19 +28,20 @@ class ProtostarDirectory:
             return self._protostar_binary_dir
 
         protostar_path = shutil.which("protostar")
-        if protostar_path:
-            self._protostar_binary_dir = Path(os.path.split(protostar_path)[0])
+        if protostar_path is None:
+            raise ProtostarDirectory.ProtostarNotInstalledException()
+
+        self._protostar_binary_dir = Path(os.path.split(protostar_path)[0])
 
         return self._protostar_binary_dir
 
     @property
     def directory_root_path(self) -> Path:
         protostar_binary_dir = self.protostar_binary_dir_path
-        self._protostar_root_dir = (
-            protostar_binary_dir / ".." / ".."
-            if protostar_binary_dir
-            else Path.home() / ".protostar"
-        )
+
+        assert protostar_binary_dir is not None
+
+        self._protostar_root_dir = protostar_binary_dir / ".." / ".."
         return self._protostar_root_dir
 
 
---
+++ src/utils/protostar_directory_test.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from pytest_mock import MockerFixture
+
+import pytest
+
+from src.utils.protostar_directory import ProtostarDirectory
+
+
+@pytest.fixture(name="home_path")
+def home_path_fixture(tmpdir: str) -> Path:
+    return Path(tmpdir)
+
+
+@pytest.fixture(name="home_path")
+def protostar_bin_dir_path_fixture(home_path) -> Path:
+    return home_path / ".protostar" / "dist" / "protostar"
+
+
+@pytest.fixture(name="protostar_in_path")
+def protostar_in_path_fixture(mocker: MockerFixture, protostar_bin_dir_path: Path):
+    protostar_bin_dir_path.mkdir(parents=True)
+    mocker.patch("shutil.which").return_value = protostar_bin_dir_path / "protostar"
+
+
+@pytest.fixture(name="protostar_not_in_path")
+def protostar_not_in_path_fixture(mocker: MockerFixture):
+    mocker.patch("shutil.which").return_value = None
+
+
+@pytest.mark.usefixtures("protostar_in_path")
+def test_protostar_binary_dir_path(protostar_bin_dir_path: Path):
+    protostar_directory = ProtostarDirectory()
+
+    assert protostar_directory.protostar_binary_dir_path == protostar_bin_dir_path
+
+
+@pytest.mark.usefixtures("protostar_not_in_path")
+def test_not_existing_protostar_binary_dir_path():
+    protostar_directory = ProtostarDirectory()
+
+    with pytest.raises(ProtostarDirectory.ProtostarNotInstalledException):
+        # pylint: disable=pointless-statement
+        protostar_directory.protostar_binary_dir_path
