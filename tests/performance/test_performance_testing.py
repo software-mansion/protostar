5074cac2b5b0d185af78c1cb40f3c920c7780a0c
Maksymilian Demitraszek <maksymilian.demitraszek@gmail.com>
2022-04-15 15:13:21 +0200

    Fixed performance tests

--- tests/performance/test_performance_testing.py
+++ tests/performance/test_performance_testing.py
@@ -1,6 +1,10 @@
+from os import path
 from pathlib import Path
 import pytest
+from subprocess import run
 from string import Template
+
+from tests.conftest import ACTUAL_CWD
 header = """
 %lang starknet
 %builtins pedersen range_check
@@ -15,7 +19,7 @@ end
 
 case = Template("""
 @external
-func test-$file-$case{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}():
+func test_m$file$case{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}():
     let (target_contract) = target.read()
     assert target_contract=0
     target.write(1)
@@ -28,16 +32,21 @@ end
 
 @pytest.fixture
 def test_contracts():
-    for file in range(1):
+    for file in range(5):
         cases = "".join([case.substitute(file=str(file), case=str(i)) for i in range(5)])
         content = header + cases
-        print(content)
         with open(Path() / "tests" / f"test_file_{file}.cairo", "w") as f:
             f.write(content)
 
 
-@pytest.mark.skip(reason="performance_test")
+@pytest.mark.skip(reason="performance test")
 @pytest.mark.usefixtures("test_contracts")
 @pytest.mark.usefixtures("init")
-def test_testing_performance(protostar):
-    protostar(["test", "tests"])
+def test_testing_performance():
+    run(
+        [
+            f"{path.join(ACTUAL_CWD, 'dist', 'protostar', 'protostar')}",
+            "test",
+            "tests"
+        ]
+    )
