2d1d5538eb94d877c16f6fa95604b05c4bea965e
Arkadiusz <arkadiusz.kasprzyk@swmansion.com>
2022-04-20 10:45:19 +0200

    Fix Git version check (#221)
    * fix git version check
    
    * fix formatting

--- src/cli.py
+++ src/cli.py
@@ -42,7 +42,7 @@ async def cli(args, script_root: Path):
         git_version = version_manager.git_version
         if git_version and git_version < VersionManager.parse("2.28"):
             raise ProtostarException(
-                "Protostar requires version 2.28 or greater of Git"
+                f"Protostar requires version 2.28 or greater of Git (current version: {git_version})"
             )
 
         if args.version:
