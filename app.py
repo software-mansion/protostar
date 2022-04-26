94781e199820008e70de595583fa649d8d3bbd8a
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2022-04-26 14:25:27 +0200

    remove argument parser from app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 # TODO: rename to protostar.py
 import asyncio
-from argparse import ArgumentParser
 
 from src.core import ArgumentDefaultValueFromConfigProvider, ArgumentParserFacade
 from src.protostar_cli import PROJECT, protostar_cli
 
 parser = ArgumentParserFacade(
-    ArgumentParser(),
     protostar_cli,
     default_value_provider=ArgumentDefaultValueFromConfigProvider(PROJECT),
 )
