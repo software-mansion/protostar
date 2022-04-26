33214fdbd43c1343e549e89b5d08c160e712f978
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2022-04-26 14:24:08 +0200

    test injecting cairo dir

--- src/utils/protostar_directory_test.py
+++ src/utils/protostar_directory_test.py
@@ -20,3 +20,12 @@ def test_directory_root_path(script_root: Path):
     protostar_directory = ProtostarDirectory(script_root)
 
     assert protostar_directory.directory_root_path == script_root / ".." / ".."
+
+
+def test_injecting_cairo_dir(script_root: Path):
+    protostar_directory = ProtostarDirectory(script_root)
+
+    foo_path = Path("foo")
+    cairo_paths = protostar_directory.inject_protostar_cairo_dir([foo_path])
+
+    assert Path(script_root / "cairo") in cairo_paths
