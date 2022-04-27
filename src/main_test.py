982acdbdc624e751f71c218ccd1be57dabb98d73
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2022-04-27 14:02:05 +0200

    test main file

--- src/main_test.py
+++ src/main_test.py
@@ -1,5 +1,7 @@
 import asyncio
 from pathlib import Path
+from typing import List, cast
+from unittest.mock import MagicMock
 
 import pytest
 from pytest_mock import MockerFixture
@@ -7,22 +9,56 @@ from pytest_mock import MockerFixture
 from .main import main
 
 
-@pytest.mark.asyncio
-def test_should_run_protostar_cli(mocker: MockerFixture):
+@pytest.fixture(name="protostar_cli_create_mock")
+def protostar_cli_create_mock_fixture(mocker: MockerFixture):
+    return mocker.patch("src.protostar_cli.ProtostarCLI.create")
+
+
+@pytest.fixture(name="run_mock")
+def run_fixture(mocker: MockerFixture):
+    mock = mocker.MagicMock()
+    return mock
+
+
+@pytest.fixture(autouse=True)
+def protostar_cli_fixture(
+    mocker: MockerFixture, protostar_cli_create_mock: MagicMock, run_mock: MagicMock
+):
     mocker.patch("src.cli.ArgumentParserFacade")
-    protostar_cli_create_mock = mocker.patch("src.protostar_cli.ProtostarCLI.create")
     protostar_cli_mock = mocker.MagicMock()
     protostar_cli_create_mock.return_value = protostar_cli_mock
-    run_mock = mocker.MagicMock()
     mocker.patch.object(
         protostar_cli_mock, attribute="run", new=asyncio.coroutine(run_mock)
     )
 
+
+def test_should_run_protostar_cli(run_mock: MagicMock):
     main(Path())
 
     run_mock.assert_called_once()
 
 
-# test_should_tell_user_where_to_report_unexpected_errors
+def test_should_tell_user_where_to_report_unexpected_errors(
+    capsys, run_mock: MagicMock
+):
+    run_mock.side_effect = Exception()
 
-# test_should_handle_no_git_scenario
+    with pytest.raises(Exception):
+        main(Path())
+
+    captured = capsys.readouterr()
+    output = captured.out.split("\n")
+    assert "https://github.com/software-mansion/protostar/issues" in output
+
+
+def test_handling_no_git_error(capsys, protostar_cli_create_mock: MagicMock):
+    protostar_cli_create_mock.side_effect = ImportError(
+        "Failed to initialize: Bad git executable."
+    )
+
+    with pytest.raises(SystemExit):
+        main(Path())
+
+    captured = capsys.readouterr()
+    output = cast(List[str], captured.out.split("\n"))
+    assert output[0].startswith("Protostar requires git")
