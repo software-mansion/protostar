7c77a79ab7995f9023c001ed45fa59052de42655
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2022-04-27 13:06:24 +0200

    refine main test case

--- src/main_test.py
+++ src/main_test.py
@@ -4,20 +4,12 @@ from pathlib import Path
 import pytest
 from pytest_mock import MockerFixture
 
-from src.cli import ArgumentParserFacade
-from src.protostar_cli import ProtostarCLI
-
 from .main import main
 
 
 @pytest.mark.asyncio
 def test_should_run_protostar_cli(mocker: MockerFixture):
-    protostar_cli = ProtostarCLI.create(Path())
-    args = ArgumentParserFacade(protostar_cli).parse(["init"])
-
-    argument_parser_facade_mock = mocker.patch("src.cli.ArgumentParserFacade")
-    # argument_parser_facade_mock.parse = mocker.MagicMock()
-
+    mocker.patch("src.cli.ArgumentParserFacade")
     protostar_cli_create_mock = mocker.patch("src.protostar_cli.ProtostarCLI.create")
     protostar_cli_mock = mocker.MagicMock()
     protostar_cli_create_mock.return_value = protostar_cli_mock
@@ -25,9 +17,6 @@ def test_should_run_protostar_cli(mocker: MockerFixture):
     mocker.patch.object(
         protostar_cli_mock, attribute="run", new=asyncio.coroutine(run_mock)
     )
-
-    protostar_cli = ProtostarCLI.create(Path())
-    argument_parser_facade_mock.parse.return_value = args
 
     main(Path())
 
