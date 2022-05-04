28ca550b5fd827a34021ba34506bfaf43eaf6381
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2022-05-04 14:08:41 +0200

    use __name__

--- protostar.py
+++ protostar.py
@@ -1,16 +1,17 @@
 import sys
 from pathlib import Path
 
-try:
-    from src import main
+if __name__ == "__main__":
+    try:
+        from src import main
 
-    main(Path(__file__).parent)
+        main(Path(__file__).parent)
 
-except ImportError as err:
-    # pylint: disable=no-member
-    if err.msg.startswith("Failed to initialize: Bad git executable."):
-        print(
-            "Protostar requires git executable to be specified in $PATH. Did you install git?"
-        )
-        sys.exit()
-    raise err
+    except ImportError as err:
+        # pylint: disable=no-member
+        if err.msg.startswith("Failed to initialize: Bad git executable."):
+            print(
+                "Protostar requires git executable to be specified in $PATH. Did you install git?"
+            )
+            sys.exit()
+        raise err
