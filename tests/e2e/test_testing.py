ef63ef741e6ba4f36fac85824312014f029e4b8b
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-05-12 17:30:43 +0200

    fix e2e test

--- tests/e2e/test_testing.py
+++ tests/e2e/test_testing.py
@@ -1,6 +1,9 @@
+import shutil
 from subprocess import CalledProcessError
 
 import pytest
+
+from tests.e2e.conftest import ACTUAL_CWD
 
 
 @pytest.mark.usefixtures("init")
@@ -22,31 +25,39 @@ def test_complex(protostar, copy_fixture):
 
 
 @pytest.mark.usefixtures("init")
-def test_expect_revert(protostar, copy_fixture):
-    copy_fixture("test_expect_revert.cairo", "./tests")
+def test_expect_revert(protostar):
+    shutil.copy(
+        ACTUAL_CWD
+        / "tests"
+        / "integration"
+        / "cheatcodes"
+        / "expect_revert"
+        / "expect_revert_test.cairo",
+        "./tests",
+    )
 
-    result = protostar(["--no-color", "test", "tests/test_expect_revert.cairo"])
+    result = protostar(["--no-color", "test", "tests/expect_revert_test.cairo"])
 
-    assert "[FAIL] tests/test_expect_revert.cairo test_fail_error_message" in result
+    assert "[FAIL] tests/expect_revert_test.cairo test_fail_error_message" in result
 
     assert (
-        "[FAIL] tests/test_expect_revert.cairo test_with_except_revert_fail_expected"
+        "[FAIL] tests/expect_revert_test.cairo test_with_except_revert_fail_expected"
         in result
     )
 
     assert (
-        "[FAIL] tests/test_expect_revert.cairo test_with_except_out_of_scope_revert_fail_expected"
+        "[FAIL] tests/expect_revert_test.cairo test_with_except_out_of_scope_revert_fail_expected"
         in result
     )
 
     assert (
-        "[FAIL] tests/test_expect_revert.cairo test_call_not_existing_contract_fail_expected"
+        "[FAIL] tests/expect_revert_test.cairo test_call_not_existing_contract_fail_expected"
         in result
     )
     assert "[error_type] RANDOM_ERROR_NAME" in result
 
     assert (
-        "[FAIL] tests/test_expect_revert.cairo test_error_was_not_raised_before_stopping_expect_revert_fail_expected"
+        "[FAIL] tests/expect_revert_test.cairo test_error_was_not_raised_before_stopping_expect_revert_fail_expected"
         in result
     )
     assert "5 failed, 5 passed, 10 total" in result
