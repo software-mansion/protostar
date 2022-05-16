9e0cd66669781482f6ca6e139985798af3ec8ca5
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2022-05-16 13:20:08 +0200

    define deploy command arguments

--- src/commands/deploy/deploy_command.py
+++ src/commands/deploy/deploy_command.py
@@ -18,7 +18,29 @@ class DeployCommand(Command):
 
     @property
     def arguments(self) -> List[Command.Argument]:
-        return []
+        return [
+            Command.Argument(
+                name="contract",
+                description='A name of the contract defined in protostar.toml::["protostar.contracts"]',
+                type="str",
+            ),
+            Command.Argument(
+                name="inputs",
+                description="The inputs to the constructor",
+                type="str",
+                is_array=True,
+            ),
+            Command.Argument(
+                name="network",
+                description="A name of the network defined in protostar.toml",
+                type="str",
+            ),
+            Command.Argument(
+                name="token",
+                description="Used for deploying contracts in Alpha MainNet.",
+                type="str",
+            ),
+        ]
 
     async def run(self, _args):
         return await self.deploy()
