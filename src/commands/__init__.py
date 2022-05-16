5984719ff2f582dbe6a814d26008d22246b8d472
Arkadiusz <arkadiusz.kasprzyk@swmansion.com>
2022-05-16 14:12:58 +0200

    Deploy command interface (#275)
    * register deploy command
    
    * define deploy command arguments
    
    * unregister deploy command
    
    * throw not implemented
    
    * sort lock

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -1,4 +1,5 @@
 from src.commands.build import BuildCommand
+from src.commands.deploy import DeployCommand
 from src.commands.init import InitCommand, init
 from src.commands.install import InstallCommand, handle_install_command
 from src.commands.remove import RemoveCommand, handle_remove_command, removal_exceptions
---
+++ src/commands/deploy/deploy_command.py
@@ -0,0 +1,49 @@
+from typing import List, Optional
+
+from src.cli.command import Command
+
+
+class DeployCommand(Command):
+    @property
+    def name(self) -> str:
+        return "deploy"
+
+    @property
+    def description(self) -> str:
+        return "Deploys contracts."
+
+    @property
+    def example(self) -> Optional[str]:
+        return None
+
+    @property
+    def arguments(self) -> List[Command.Argument]:
+        return [
+            Command.Argument(
+                name="contract",
+                description='A name of the contract defined in protostar.toml::["protostar.contracts"]',
+                type="str",
+            ),
+            Command.Argument(
+                name="inputs",
+                description="The inputs to the constructor",
+                type="str",
+                is_array=True,
+            ),
+            Command.Argument(
+                name="network",
+                description="A name of the network defined in protostar.toml",
+                type="str",
+            ),
+            Command.Argument(
+                name="token",
+                description="Used for deploying contracts in Alpha MainNet.",
+                type="str",
+            ),
+        ]
+
+    async def run(self, _args):
+        return await self.deploy()
+
+    async def deploy(self):
+        raise NotImplementedError()
