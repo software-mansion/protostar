17f2fe12807af5f7d246dbe2b55ace000f782b08
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2022-05-17 13:18:40 +0200

    update deploy command

--- protostar/commands/deploy/deploy_command.py
+++ protostar/commands/deploy/deploy_command.py
@@ -1,9 +1,17 @@
+from pathlib import Path
 from typing import List, Optional
 
 from protostar.cli.command import Command
+from protostar.commands.deploy.deploy_contract import deploy_contract
+from protostar.commands.deploy.network_config import NetworkConfig
+from protostar.utils.config.project import Project
 
 
 class DeployCommand(Command):
+    def __init__(self, project: Project, build_output_path: Path) -> None:
+        self._project = project
+        self._build_output_path = build_output_path
+
     @property
     def name(self) -> str:
         return "deploy"
@@ -40,10 +48,47 @@ class DeployCommand(Command):
                 description="Used for deploying contracts in Alpha MainNet.",
                 type="str",
             ),
+            Command.Argument(
+                name="salt",
+                description=(
+                    "An optional salt controlling where the contract will be deployed. "
+                    "The contract deployment address is determined by the hash "
+                    "of contract, salt and caller. "
+                    "If the salt is not supplied, the contract will be deployed with a random salt."
+                ),
+                type="str",
+            ),
         ]
 
-    async def run(self, _args):
-        return await self.deploy()
+    async def run(self, args):
+        return await self.deploy(
+            contract_name=args.contract,
+            network=args.network,
+            inputs=args.inputs,
+            token=args.token,
+            salt=args.salt,
+        )
 
-    async def deploy(self):
-        raise NotImplementedError()
+    # pylint: disable=too-many-arguments
+    async def deploy(
+        self,
+        contract_name: str,
+        network: str,
+        inputs: Optional[List[str]] = None,
+        token: Optional[str] = None,
+        salt: Optional[str] = None,
+    ):
+        with open(
+            self._build_output_path / f"{contract_name}.json",
+            mode="r",
+            encoding="utf-8",
+        ) as compiled_contract_file:
+            network_config = NetworkConfig.from_config_file(network, self._project)
+
+            await deploy_contract(
+                gateway_url=network_config.gateway_url,
+                compiled_contract_file=compiled_contract_file,
+                constructor_args=inputs,
+                salt=salt,
+                token=token,
+            )
