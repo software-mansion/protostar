c1f67fe6bc52790038651f48f27f24b6e00147b7
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2022-05-18 11:13:23 +0200

    use pytest-datadir

--- tests/integration/deploying/deploy_test.py
+++ tests/integration/deploying/deploy_test.py
@@ -1,23 +1,22 @@
 # pylint: disable=consider-using-with
 import subprocess
 import time
-from distutils.file_util import copy_file
 from pathlib import Path
 from socket import socket as Socket
 
 import pytest
 
-from protostar.commands.deploy.deploy_contract import deploy_contract
+from protostar.commands.deploy.starkware.deploy import deploy
 
 
-@pytest.fixture(name="devnet_gateway_port")
+@pytest.fixture(name="devnet_gateway_port", scope="session")
 def devnet_gateway_port_fixture() -> int:
     with Socket() as socket:
         socket.bind(("", 0))
         return socket.getsockname()[1]
 
 
-@pytest.fixture(name="devnet_gateway_url")
+@pytest.fixture(name="devnet_gateway_url", scope="session")
 def devnet_gateway_url_fixture(devnet_gateway_port: int):
     command = [
         "poetry",
@@ -36,20 +35,15 @@ def devnet_gateway_url_fixture(devnet_gateway_port: int):
 
 
 @pytest.fixture(name="compiled_contract_file")
-def compiled_contract_file_fixture(tmpdir):
-    copy_file(
-        str(Path(__file__).parent / "data" / "main.json"),
-        str(Path(tmpdir) / "main.json"),
-    )
-
-    file_handle = open(Path(tmpdir) / "main.json", "r", encoding="utf_8")
+def compiled_contract_file_fixture(shared_datadir: Path):
+    file_handle = open(shared_datadir / "main.json", "r", encoding="utf_8")
     yield file_handle
     file_handle.close()
 
 
 @pytest.mark.asyncio
 async def test_deploying_contract(devnet_gateway_url: str, compiled_contract_file):
-    response = await deploy_contract(
+    response = await deploy(
         gateway_url=devnet_gateway_url, compiled_contract_file=compiled_contract_file
     )
 
