b2fe473fba92415d44ac5ef38c6e25845ac35c9f
Maksymilian Demitraszek <maksymilian.demitraszek@gmail.com>
2022-05-18 12:17:08 +0200

    fix case

--- protostar/commands/test/test_collector_test.py
+++ protostar/commands/test/test_collector_test.py
@@ -114,6 +114,7 @@ def test_logging_collected_one_test_suite_and_one_test_case(mocker: MockerFixtur
             TestSuite(
                 test_case_names=["foo"],
                 test_path=Path(),
+                preprocessed_contract=mocker.MagicMock()
             )
         ],
         test_cases_count=1,
@@ -132,10 +133,12 @@ def test_logging_many_test_suites_and_many_test_cases(mocker: MockerFixture):
             TestSuite(
                 test_case_names=["foo"],
                 test_path=Path(),
+                preprocessed_contract=mocker.MagicMock()
             ),
             TestSuite(
                 test_case_names=["foo"],
                 test_path=Path(),
+                preprocessed_contract=mocker.MagicMock()
             ),
         ],
         test_cases_count=2,
