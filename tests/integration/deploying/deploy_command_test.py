8353f514d3511b7f608260712d03ce8670a95736
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2022-05-19 15:14:04 +0200

    add integration test

--- tests/integration/deploying/deploy_command_test.py
+++ tests/integration/deploying/deploy_command_test.py
@@ -9,21 +9,23 @@ from protostar.utils.config.project import Project
 
 
 @pytest.mark.asyncio
-@pytest.mark.usefixtures("compiled_contract_file")
+@pytest.mark.parametrize("contract_name", ["main_with_constructor"])
+@pytest.mark.usefixtures("compiled_contract_filepath")
 async def test_deploying_contract(
     mocker: MockerFixture,
     devnet_gateway_url: str,
     project_root_path: Path,
-    output_path: Path,
+    output_dir: Path,
 ):
     project_mock = mocker.MagicMock()
     cast(Project, project_mock).project_root = project_root_path
     deploy_command = DeployCommand(project_mock)
 
     response = await deploy_command.deploy(
-        contract_name="main",
+        contract_name="main_with_constructor",
         gateway_url=devnet_gateway_url,
-        build_output_dir=output_path,
+        build_output_dir=output_dir,
+        inputs=["42"],
     )
 
     assert response["address"] is not None
