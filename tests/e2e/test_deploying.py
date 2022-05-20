04b3b768bfbec141efe5fe1668d7cb7ea86c321e
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-05-20 17:24:23 +0200

    add e2e tests

--- tests/e2e/test_deploying.py
+++ tests/e2e/test_deploying.py
@@ -1,8 +1,44 @@
+from distutils.file_util import copy_file
+from pathlib import Path
+
 import pytest
 
 
 @pytest.mark.usefixtures("init")
-def test_deploying_basic_example_to_devnet(protostar, devnet_gateway_url):
+def test_deploying_contract_with_constructor(
+    protostar, devnet_gateway_url, datadir: Path
+):
+    copy_file(
+        src=str(datadir / "contract_with_constructor.cairo"), dst="./src/main.cairo"
+    )
+    protostar(["build"])
+
+    result = protostar(
+        [
+            "--no-color",
+            "deploy",
+            "./build/main.json",
+            "--inputs",
+            "42",
+            "--gateway-url",
+            devnet_gateway_url,
+        ]
+    )
+
+    assert "Deploy transaction was sent" in result
+
+
+@pytest.mark.usefixtures("init")
+def test_deploying_contract_with_constructor_and_inputs_defined_in_config_file(
+    protostar, devnet_gateway_url, datadir: Path
+):
+    copy_file(
+        src=str(datadir / "contract_with_constructor.cairo"), dst="./src/main.cairo"
+    )
+    copy_file(
+        src=str(datadir / "protostar_toml_with_constructor_args.toml"),
+        dst="./protostar.toml",
+    )
     protostar(["build"])
 
     result = protostar(
---
+++ tests/e2e/test_deploying/contract_with_constructor.cairo
@@ -0,0 +1,34 @@
+%lang starknet
+%builtins pedersen range_check
+from starkware.cairo.common.math import assert_nn
+from starkware.cairo.common.cairo_builtins import HashBuiltin
+
+@storage_var
+func balance() -> (res : felt):
+end
+
+@external
+func increase_balance{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(
+        amount : felt):
+    with_attr error_message("Amount must be positive. Got: {amount}."):
+        assert_nn(amount)
+    end
+
+    let (res) = balance.read()
+    balance.write(res + amount)
+    return ()
+end
+
+@view
+func get_balance{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}() -> (
+        res : felt):
+    let (res) = balance.read()
+    return (res)
+end
+
+@constructor
+func constructor{syscall_ptr : felt*, pedersen_ptr : HashBuiltin*, range_check_ptr}(
+        initial_balance : felt):
+    balance.write(initial_balance)
+    return ()
+end
