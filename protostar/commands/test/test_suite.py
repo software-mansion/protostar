c793d4f4f8606549808cc9b68ff0ee3ecae9aef1
Arkadiusz <arkadiusz.kasprzyk@swmansion.com>
2022-05-27 13:51:11 +0200

    [`setup_state`::01]  Detect and try executing hook (#307)
    * add integration tests
    
    * find setup_state function
    
    * throw an exception if setup state is defined
    
    * use state dict in tests

--- protostar/commands/test/test_suite.py
+++ protostar/commands/test/test_suite.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from pathlib import Path
-from typing import List
+from typing import List, Optional
+
 from starkware.starknet.compiler.starknet_preprocessor import (
     StarknetPreprocessedProgram,
 )
@@ -11,3 +12,4 @@ class TestSuite:
     test_path: Path
     preprocessed_contract: StarknetPreprocessedProgram
     test_case_names: List[str]
+    setup_state_fn_name: Optional[str] = None
