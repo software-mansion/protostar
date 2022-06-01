c095bbe2eba963fc3bb8fd80d67c5b8e2886554d
Wojciech Szymczyk <wojciech.szymczyk@swmansion.com>
2022-06-01 15:14:36 +0200

    Review changes - move patch to tests, package crypto_cpp_py binary

--- protostar.spec
+++ protostar.spec
@@ -1,6 +1,8 @@
 # -*- mode: python ; coding: utf-8 -*-
 from PyInstaller.utils.hooks import collect_data_files, collect_submodules
 
+import crypto_cpp_py
+
 block_cipher = None
 extra_files = [
     ("cairo", "cairo"),
@@ -12,9 +14,11 @@ extra_imports = [
         "eth_hash.auto",
     ] + collect_submodules('starkware')
 
+binaries = [(f"{crypto_cpp_py.__path__[0]}/../libcrypto_c_exports.*", ".")]
+
 a = Analysis(['binary_entrypoint.py'],
              pathex=[],
-             binaries=[],
+             binaries=binaries,
              datas=extra_files,
              hiddenimports=extra_imports,
              hookspath=[],
--- protostar/commands/test/__init__.py
+++ protostar/commands/test/__init__.py
@@ -1,1 +1,23 @@
+import sys
+
+from starkware.crypto.signature.fast_pedersen_hash import pedersen_hash
+from crypto_cpp_py.cpp_bindings import cpp_hash
+
+
+def patched_pedersen_hash(left: int, right: int) -> int:
+    return cpp_hash(left, right)
+
+
+# This is a monkey-patch to improve the performance of the protostar tests
+# We are using c++ code for calculating the pedersen hashes
+# instead of python implementation from cairo-lang package
+# It is placed before everything else to make sure only the patched function
+# is used
+setattr(
+    sys.modules["starkware.crypto.signature.fast_pedersen_hash"],
+    "pedersen_hash",
+    patched_pedersen_hash,
+)
+
+# pylint: disable=C0413
 from .test_command import TestCommand
