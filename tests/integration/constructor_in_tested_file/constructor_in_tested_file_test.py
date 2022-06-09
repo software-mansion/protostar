ea42b9444e686d6df4a1230ea2a6690372cead61
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-06-09 13:57:38 +0200

    add test case

--- tests/integration/constructor_in_tested_file/constructor_in_tested_file_test.py
+++ tests/integration/constructor_in_tested_file/constructor_in_tested_file_test.py
@@ -11,13 +11,10 @@ from protostar.protostar_exception import ProtostarException
 
 @pytest.mark.asyncio
 async def test_should_pretty_print_constructor_error(mocker: MockerFixture):
-
     test_command = TestCommand(
         project=mocker.MagicMock(),
         protostar_directory=mocker.MagicMock(),
     )
-    test_command._build_include_paths = mocker.MagicMock()
-    test_command._build_include_paths.return_value = [str(Path(__file__).parent)]
 
     testing_summary = await test_command.test(
         targets=[f"{Path(__file__).parent}/basic_contract_test.cairo"],
@@ -28,3 +25,19 @@ async def test_should_pretty_print_constructor_error(mocker: MockerFixture):
         "constructor expecting arguments"
         in cast(ProtostarException, testing_summary.broken[0].exception).message
     )
+
+
+@pytest.mark.asyncio
+async def test_should_not_break_test_suite(
+    mocker: MockerFixture,
+):
+    test_command = TestCommand(
+        project=mocker.MagicMock(),
+        protostar_directory=mocker.MagicMock(),
+    )
+
+    testing_summary = await test_command.test(
+        targets=[f"{Path(__file__).parent}/basic_contract_integration_test.cairo"],
+    )
+
+    assert len(testing_summary.broken) == 0
