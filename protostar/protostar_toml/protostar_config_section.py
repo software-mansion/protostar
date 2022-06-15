fd5a3155a9b0f0192dca765594cc97876b9e6d52
Arkadiusz <arkadiusz.kasprzyk@swmansion.com>
2022-06-15 12:19:45 +0200

    [project_class_refactor::01] Rewrite from scratch (#338)
    * add protostar_toml
    
    * add types
    
    * move sections to separate files
    
    * handle saving
    
    * refactor
    
    * refactor
    
    * refactor
    
    * format
    
    * format
    
    * rename type
    
    * rename type again

---
+++ protostar/protostar_toml/protostar_config_section.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+from typing import Any, Dict
+
+from protostar.protostar_toml.protostar_toml_exceptions import (
+    InvalidProtostarTOMLException,
+)
+from protostar.protostar_toml.protostar_toml_reader import ProtostarTOMLReader
+from protostar.protostar_toml.protostar_toml_section import ProtostarTOMLSection
+from protostar.utils.protostar_directory import VersionManager, VersionType
+
+
+@dataclass
+class ProtostarConfigSection(ProtostarTOMLSection):
+    protostar_version: VersionType
+
+    @staticmethod
+    def get_section_name() -> str:
+        return "config"
+
+    @classmethod
+    def from_protostar_toml(
+        cls, protostar_toml: ProtostarTOMLReader
+    ) -> "ProtostarConfigSection":
+        section_dict = protostar_toml.get_section(cls.get_section_name())
+        if section_dict is None:
+            raise InvalidProtostarTOMLException(cls.get_section_name())
+        return cls.from_dict(section_dict)
+
+    @classmethod
+    def from_dict(cls, raw_dict: Dict[str, Any]) -> "ProtostarConfigSection":
+        return cls(
+            protostar_version=VersionManager.parse(raw_dict["protostar_version"])
+        )
+
+    def to_dict(self) -> "ProtostarTOMLSection.ParsedProtostarTOML":
+        result: ProtostarTOMLSection.ParsedProtostarTOML = {}
+        result["protostar_version"] = str(self.protostar_version)
+        return result
