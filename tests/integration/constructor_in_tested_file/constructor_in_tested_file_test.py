a847fbe8f884144d952e9e9c06c479e5fe733b91
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-06-17 11:21:50 +0200

    add deploy syscall test case

--- tests/integration/constructor_in_tested_file/constructor_in_tested_file_test.py
+++ tests/integration/constructor_in_tested_file/constructor_in_tested_file_test.py
@@ -1,16 +1,21 @@
 # pylint: disable=protected-access
 from pathlib import Path
-from typing import cast
 
 import pytest
 from pytest_mock import MockerFixture
 
 from protostar.commands.test.test_command import TestCommand
-from protostar.protostar_exception import ProtostarException
+
+
+@pytest.fixture(name="pretty_printed_error_message_substring")
+def pretty_printed_error_message_substring_fixture() -> str:
+    return "constructor expecting arguments"
 
 
 @pytest.mark.asyncio
-async def test_should_pretty_print_constructor_error(mocker: MockerFixture):
+async def test_should_pretty_print_constructor_error(
+    mocker: MockerFixture, pretty_printed_error_message_substring: str
+):
     test_command = TestCommand(
         project=mocker.MagicMock(),
         protostar_directory=mocker.MagicMock(),
@@ -21,9 +26,8 @@ async def test_should_pretty_print_constructor_error(mocker: MockerFixture):
     )
 
     assert len(testing_summary.broken) == 1
-    assert (
-        "constructor expecting arguments"
-        in cast(ProtostarException, testing_summary.broken[0].exception).message
+    assert pretty_printed_error_message_substring in str(
+        testing_summary.broken[0].exception
     )
 
 
@@ -41,3 +45,22 @@ async def test_should_not_break_test_suite(
     )
 
     assert len(testing_summary.broken) == 0
+
+
+@pytest.mark.asyncio
+async def test_not_pretty_printing_the_constructor_error(
+    mocker: MockerFixture, pretty_printed_error_message_substring: str
+):
+    test_command = TestCommand(
+        project=mocker.MagicMock(),
+        protostar_directory=mocker.MagicMock(),
+    )
+
+    testing_summary = await test_command.test(
+        targets=[f"{Path(__file__).parent}/error_from_deploy_syscall_test.cairo"],
+    )
+
+    assert len(testing_summary.failed) == 1
+    assert pretty_printed_error_message_substring not in str(
+        testing_summary.failed[0].exception
+    )
