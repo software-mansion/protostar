04fb5c697444502f9347022759ffd6cd32f04727
Maksymilian Demitraszek <maksymilian.demitraszek@protonmail.com>
2022-06-22 15:20:51 +0200

    Extendted to docs

--- scripts/custom_checks.sh
+++ scripts/custom_checks.sh
@@ -1,7 +1,7 @@
 # This scripts performs custom static checks in CI
 
-# Check if all cairo test use @external decorator
-grep -z '@view.*\nfunc test_.*' $(find . -type f -name '*.cairo')
+# Check if all cairo tests (cairo files and docs) use @external decorator
+grep -z '@view.*\nfunc test_.*' $(find . -type f -name '*.cairo' -o -name '*.md')
 if [ $? -eq 0 ]; then
     exit 1
 fi
