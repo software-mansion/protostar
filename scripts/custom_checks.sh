c9a32d41fa0068b186d50b3e405c8fa4dd5a2848
Maksymilian Demitraszek <maksymilian.demitraszek@protonmail.com>
2022-06-22 15:09:22 +0200

    Added check to CI`

--- scripts/custom_checks.sh
+++ scripts/custom_checks.sh
@@ -1,7 +1,7 @@
 # This scripts performs custom static checks in CI
 
 # Check if all cairo test use @external decorator
-find . -type f -name '*.cairo' | grep -z '@view.*\nfunc test_.*'
+grep -z '@view.*\nfunc test_.*' $(find . -type f -name '*.cairo')
 if [ $? -eq 0 ]; then
     exit 1
 fi
