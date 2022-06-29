1a13c89426af80e56ddb33b90f676fea6e4f7325
Arkadiusz <arkadiusz.kasprzyk@swmansion.com>
2022-06-29 09:54:49 +0200

    Disable saving PreprocessedProgram in the memory (#400)
    * add prints
    
    * add log
    
    * disable caching preprocessed contract
    
    * add run 1
    
    * remove print
    
    * remove tmp file
    
    * clean up
    
    * fix tests
    
    * clean up
    
    * remove empty line

--- protostar/commands/test/test_suite.py
+++ protostar/commands/test/test_suite.py
@@ -2,14 +2,9 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import List, Optional
 
-from starkware.starknet.compiler.starknet_preprocessor import (
-    StarknetPreprocessedProgram,
-)
-
 
 @dataclass(frozen=True)
 class TestSuite:
     test_path: Path
-    preprocessed_contract: StarknetPreprocessedProgram
     test_case_names: List[str]
     setup_fn_name: Optional[str] = None
