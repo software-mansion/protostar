f6fb651e95553186cf37d312270996fb8748c61d
Arkadiusz Kasprzyk <arkadiusz.kasprzyk@swmansion.com>
2022-06-30 14:48:03 +0200

    fix test

--- tests/integration/cheatcodes/expect_events/expect_events_test.py
+++ tests/integration/cheatcodes/expect_events/expect_events_test.py
@@ -2,7 +2,6 @@ from pathlib import Path
 from typing import cast
 
 import pytest
-from pytest_mock import MockerFixture
 
 from protostar.commands.test.test_command import TestCommand
 from protostar.commands.test.test_environment_exceptions import (
@@ -32,6 +31,7 @@ async def test_expect_events(mocker):
             "test_expect_event_by_contract_address",
             "test_expect_events_in_declared_order",
             "test_allow_checking_for_events_in_any_order",
+            "test_data_transformation",
         ],
         expected_failed_test_cases_names=[
             "test_fail_on_data_mismatch",
@@ -57,23 +57,3 @@ async def test_event_selector_to_name_mapping(mocker):
     str_ex = str(ex)
     assert '"name": "foobar"' in str_ex
     assert '"name": "balance_increased"' in str_ex
-
-
-@pytest.mark.asyncio
-async def test_data_transformation(mocker: MockerFixture):
-    testing_summary = await TestCommand(
-        project=mocker.MagicMock(),
-        protostar_directory=mocker.MagicMock(),
-    ).test(
-        targets=[
-            f"{Path(__file__).parent}/expect_events_test.cairo::test_data_transformation"
-        ]
-    )
-
-    assert_cairo_test_cases(
-        testing_summary,
-        expected_passed_test_cases_names=[
-            "test_data_transformation",
-        ],
-        expected_failed_test_cases_names=[],
-    )
