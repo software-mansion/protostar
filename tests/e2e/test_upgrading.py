2ba0106ff5018d575491e2d96b0c29b81e002959
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-07-06 18:10:03 +0200

    add e2e test

--- tests/e2e/test_upgrading.py
+++ tests/e2e/test_upgrading.py
@@ -9,3 +9,10 @@ def test_upgrading(protostar: ProtostarFixture):
     assert "0.0.0" in protostar(["--version"])
     assert "ERROR" not in protostar(["upgrade"])
     assert "0.0.0" not in protostar(["--version"])
+
+
+@pytest.mark.parametrize("declared_protostar_version", ["0.2.0"])
+@pytest.mark.usefixtures("init")
+def test_upgrade_notification(protostar: ProtostarFixture):
+    assert "new Protostar version" not in protostar(["test"])
+    assert "new Protostar version" in protostar(["test"])
