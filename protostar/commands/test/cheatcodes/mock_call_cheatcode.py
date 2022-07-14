7bd59fbb6e660be1cb9e101cd2121f298b2cf18b
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-07-14 14:17:53 +0200

    remove unused improt

--- protostar/commands/test/cheatcodes/mock_call_cheatcode.py
+++ protostar/commands/test/cheatcodes/mock_call_cheatcode.py
@@ -4,7 +4,6 @@ from typing import Any, Callable, Dict, List, Optional, Union
 from starkware.starknet.public.abi import AbiType, get_selector_from_name
 
 from protostar.commands.test.test_environment_exceptions import CheatcodeException
-from protostar.starknet.cheatable_state import CheatableCarriedState
 from protostar.starknet.cheatcode import Cheatcode
 from protostar.starknet.types import AddressType
 from protostar.utils.data_transformer_facade import DataTransformerFacade
