83d445941caf38aa44a62cb82d20a8ed57041e69
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-07-14 17:31:41 +0200

    add declare command skeleton

---
+++ protostar/commands/declare/declare_command.py
@@ -0,0 +1,36 @@
+from typing import List, Optional
+
+from protostar.cli.command import Command
+from protostar.commands.deploy.deploy_command import DeployCommand
+from protostar.deployer import Deployer
+
+
+class DeclareCommand(Command):
+    def __init__(self, deployer: Deployer):
+        self._deployer = deployer
+
+    @property
+    def name(self) -> str:
+        return "declare"
+
+    @property
+    def description(self) -> str:
+        return "\n".join(
+            [
+                "Declare contracts.",
+            ]
+        )
+
+    @property
+    def example(self) -> Optional[str]:
+        return None
+
+    @property
+    def arguments(self) -> List[Command.Argument]:
+        return [
+            DeployCommand.gateway_url_arg,
+            DeployCommand.network_arg,
+        ]
+
+    async def run(self, args):
+        raise NotImplementedError()
