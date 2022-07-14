5bb0f54050338fbd521ffc73c15247118588dc43
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-07-14 16:58:51 +0200

    fix test

--- tests/integration/deploying/deploy_command_test.py
+++ tests/integration/deploying/deploy_command_test.py
@@ -1,12 +1,11 @@
 from pathlib import Path
 from types import SimpleNamespace
-from typing import cast
 
 import pytest
 from pytest_mock import MockerFixture
 
 from protostar.commands.deploy.deploy_command import DeployCommand
-from protostar.utils.config.project import Project
+from protostar.deployer import Deployer
 
 
 @pytest.mark.asyncio
@@ -17,10 +16,9 @@ async def test_deploying_contract(
     project_root_path: Path,
     compiled_contract_filepath,
 ):
-    project_mock = mocker.MagicMock()
-    cast(Project, project_mock).project_root = project_root_path
-    deploy_command = DeployCommand(project_mock)
-
+    deploy_command = DeployCommand(
+        deployer=Deployer(project_root_path), logger=mocker.MagicMock()
+    )
     args = SimpleNamespace()
     args.contract = compiled_contract_filepath
     args.gateway_url = devnet_gateway_url
