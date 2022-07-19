e5540bfa09df6992777360f7e149fc112d210b3e
Marek Kaput <marek.kaput@swmansion.com>
2022-07-19 17:13:03 +0200

    add E2E test

---
+++ tests/e2e/test_fuzzing.py
@@ -0,0 +1,37 @@
+import pytest
+
+
+@pytest.mark.usefixtures("init")
+def test_fuzzing(protostar, copy_fixture):
+    copy_fixture("test_fuzz.cairo", "./tests")
+
+    result = protostar(["--no-color", "test", "tests/test_fuzz.cairo"], ignore_exit_code=True)
+    print(result)
+
+    # To keep assertions free from having to exclude trailing \n
+    result += "\n"
+
+    assert "2 failed, 1 passed, 3 total" in result
+
+    assert "[PASS] tests/test_fuzz.cairo" in result
+    assert "mean: 18, δ: 0.0, min: 18, max: 18" in result
+
+    assert "[FAIL] tests/test_fuzz.cairo test_fails_if_big_single_input" in result
+    assert (
+        """
+[inputs]:
+x = 340282366920938463463374607431768211456
+"""
+        in result
+    )
+
+    assert "[FAIL] tests/test_fuzz.cairo test_fails_if_big_many_inputs" in result
+    assert (
+        """
+[inputs]:
+a = 340282366920938463463374607431768211456
+b = 0
+c = 0
+"""
+        in result
+    )
