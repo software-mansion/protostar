d5e55c54d9cbb944ebcb35e89af0a377e5e1c60a
Radinyn <igorurbanik23@gmail.com>
2022-07-21 13:23:42 +0200

    Updated tests

--- tests/integration/cheatcodes/reflect/reflect_test.py
+++ tests/integration/cheatcodes/reflect/reflect_test.py
@@ -2,16 +2,17 @@ from pathlib import Path
 
 import pytest
 
-from protostar.commands.test.test_command import TestCommand
-from tests.integration.conftest import assert_cairo_test_cases
+from tests.integration.conftest import (
+    RunCairoTestRunnerFixture,
+    assert_cairo_test_cases,
+)
 
 
 @pytest.mark.asyncio
-async def test_reflect_cheatcode(mocker):
-    testing_summary = await TestCommand(
-        project=mocker.MagicMock(),
-        protostar_directory=mocker.MagicMock(),
-    ).test(targets=[f"{Path(__file__).parent}/reflect_test.cairo"])
+async def test_prank_cheatcode(run_cairo_test_runner: RunCairoTestRunnerFixture):
+    testing_summary = await run_cairo_test_runner(
+        Path(__file__).parent / "reflect_test.cairo"
+    )
 
     assert_cairo_test_cases(
         testing_summary,
