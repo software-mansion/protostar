6fef08942d1f06b32d16fd67ed932678e6b336dd
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-07-25 10:52:17 +0200

    make fixtures dir shared

--- tests/e2e/test_deploying.py
+++ tests/e2e/test_deploying.py
@@ -6,10 +6,11 @@ import pytest
 
 @pytest.mark.usefixtures("init")
 def test_deploying_contract_with_constructor(
-    protostar, devnet_gateway_url, datadir: Path
+    protostar, devnet_gateway_url, shared_datadir: Path
 ):
     copy_file(
-        src=str(datadir / "contract_with_constructor.cairo"), dst="./src/main.cairo"
+        src=str(shared_datadir / "contract_with_constructor.cairo"),
+        dst="./src/main.cairo",
     )
     protostar(["build"])
 
@@ -30,13 +31,14 @@ def test_deploying_contract_with_constructor(
 
 @pytest.mark.usefixtures("init")
 def test_deploying_contract_with_constructor_and_inputs_defined_in_config_file(
-    protostar, devnet_gateway_url, datadir: Path
+    protostar, devnet_gateway_url, shared_datadir: Path
 ):
     copy_file(
-        src=str(datadir / "contract_with_constructor.cairo"), dst="./src/main.cairo"
+        src=str(shared_datadir / "contract_with_constructor.cairo"),
+        dst="./src/main.cairo",
     )
     copy_file(
-        src=str(datadir / "protostar_toml_with_constructor_args.toml"),
+        src=str(shared_datadir / "protostar_toml_with_constructor_args.toml"),
         dst="./protostar.toml",
     )
     protostar(["build"])
@@ -55,9 +57,10 @@ def test_deploying_contract_with_constructor_and_inputs_defined_in_config_file(
 
 
 @pytest.mark.usefixtures("init")
-def test_declaring_contract(protostar, devnet_gateway_url, datadir: Path):
+def test_declaring_contract(protostar, devnet_gateway_url, shared_datadir: Path):
     copy_file(
-        src=str(datadir / "contract_with_constructor.cairo"), dst="./src/main.cairo"
+        src=str(shared_datadir / "contract_with_constructor.cairo"),
+        dst="./src/main.cairo",
     )
     protostar(["build"])
 
