06e1402e78d713a1207d15ec23c54d6c1f62e8f8
Marek Kaput <marek.kaput@swmansion.com>
2022-07-25 17:39:05 +0200

    isolate state between fuzz test runs

--- tests/integration/fuzzing/fuzzing_test.py
+++ tests/integration/fuzzing/fuzzing_test.py
@@ -42,3 +42,18 @@ async def test_non_felt_parameter(run_cairo_test_runner: RunCairoTestRunnerFixtu
         expected_failed_test_cases_names=[],
         expected_broken_test_cases_names=["test_fails_on_non_felt_parameter"],
     )
+
+
+@pytest.mark.asyncio
+async def test_state_is_isolated(run_cairo_test_runner: RunCairoTestRunnerFixture):
+    testing_summary = await run_cairo_test_runner(
+        Path(__file__).parent / "state_isolation_test.cairo"
+    )
+
+    assert_cairo_test_cases(
+        testing_summary,
+        expected_passed_test_cases_names=[
+            "test_storage_var",
+        ],
+        expected_failed_test_cases_names=[],
+    )
