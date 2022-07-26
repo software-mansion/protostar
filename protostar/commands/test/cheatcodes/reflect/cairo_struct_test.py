560c79d6a72e29ff139ef5347ef30c84a112213e
Radinyn <igorurbanik23@gmail.com>
2022-07-26 09:03:01 +0200

    Improved unit tests

--- protostar/commands/test/cheatcodes/reflect/cairo_struct_test.py
+++ protostar/commands/test/cheatcodes/reflect/cairo_struct_test.py
@@ -1,5 +1,7 @@
 from protostar.commands.test.cheatcodes.reflect.cairo_struct import CairoStruct
 from protostar.commands.test.test_environment_exceptions import CheatcodeException
+import pytest
+
 
 # pylint: disable=C0103,W0612
 
@@ -42,15 +44,13 @@ def test_cairo_struct_equality():
 def test_cairo_struct_immutability():
     x = CairoStruct(a=0b0110, b=0b1001)
 
-    try:
+    with pytest.raises(CheatcodeException) as exc:
         x.b = 0b1000101
-        assert False
-    except CheatcodeException as exc:
-        assert "CairoStruct is immutable." in str(exc)
+    assert "CairoStruct is immutable." in str(exc.value)
 
 
 def test_cairo_struct_type_safety():
-    try:
+    with pytest.raises(CheatcodeException) as exc:
         x = CairoStruct(
             a="""
             I love Cairo
@@ -58,19 +58,15 @@ def test_cairo_struct_type_safety():
             I love Cairo
         """
         )
-        assert False
-    except CheatcodeException as exc:
-        assert "is not a valid CairoType" in str(exc)
+    assert "is not a valid CairoType" in str(exc.value)
 
 
 def test_cairo_struct_no_member():
     x = CairoStruct(a=0xAD0BE_BAD)
 
-    try:
+    with pytest.raises(CheatcodeException) as exc:
         y = x.b
-        assert False
-    except CheatcodeException as exc:
-        assert "is not a member of this CairoStruct" in str(exc)
+    assert "is not a member of this CairoStruct" in str(exc.value)
 
 
 def test_cairo_struct_non_keyword_args():
