7edf3b15230c9fcd3321c011f4d6e29777c02f2c
Igor Urbanik <igorurbanik23@gmail.com>
2022-07-28 14:29:20 +0200

    Added comment

--- protostar/starknet/delayed_builder.py
+++ protostar/starknet/delayed_builder.py
@@ -2,6 +2,13 @@ from typing import Dict, Callable, Any
 
 
 class DelayedBuilder:
+    """
+    DelayedBuilder constructor takes in a callable of form:
+    Callable[[exec_locals], Callable[..., Any]]
+
+    This callable's job is to create a cheatcode with any exec_locals needed.
+    """
+
     def __init__(self, callable_to_delay: Callable[..., Any]) -> None:
         super().__init__()
         self._callable = callable_to_delay
