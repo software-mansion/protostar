5f2ad043314e9eeef3853b322cd61c52a095f52b
Radinyn <igorurbanik23@gmail.com>
2022-07-29 11:10:18 +0200

    Refactor

--- tests/integration/testing_timing/testing_timing_test.py
+++ tests/integration/testing_timing/testing_timing_test.py
@@ -16,7 +16,7 @@ def assert_exec_times_in_desc_order(logs: str) -> None:
     assert exec_times == sorted(exec_times, reverse=True)
 
 
-def get_one_digit_test_indices(logs: str) -> Set[int]:
+def get_test_indices(logs: str) -> Set[int]:
     # "{NUMBER}." -> {NUMBER}
     return set([int(row.split()[0][:-1]) for row in logs.split("\n")])
 
@@ -33,41 +33,36 @@ async def testing_summary_fixture(
 @pytest.fixture(name="no_color_log_color_provider")
 def no_color_log_color_provider_fixture() -> LogColorProvider:
     no_color_log_color_provider = LogColorProvider()
-    no_color_log_color_provider.colorize = MethodType(
-        (lambda self, color, string: string), no_color_log_color_provider  # type: ignore
-    )
-    no_color_log_color_provider.bold = MethodType(
-        (lambda self, string: string), no_color_log_color_provider  # type: ignore
-    )
+    no_color_log_color_provider.is_ci_mode = True
     return no_color_log_color_provider
 
 
-async def test_timing_less_than_tests(
+async def test_output_when_slowest_tests_count_less_than_tests(
     testing_summary: TestingSummary, no_color_log_color_provider: LogColorProvider
 ):
     test3 = testing_summary._format_slow_test_cases_list(3, no_color_log_color_provider)
     assert_exec_times_in_desc_order(test3)
-    assert {1, 2, 3} == get_one_digit_test_indices(test3)
+    assert {1, 2, 3} == get_test_indices(test3)
 
 
-async def test_timing_equal_to_the_number_of_tests(
+async def test_output_when_slowest_tests_count_equal_to_the_number_of_tests(
     testing_summary: TestingSummary, no_color_log_color_provider: LogColorProvider
 ):
     test4 = testing_summary._format_slow_test_cases_list(4, no_color_log_color_provider)
     assert_exec_times_in_desc_order(test4)
-    assert {1, 2, 3, 4} == get_one_digit_test_indices(test4)
+    assert {1, 2, 3, 4} == get_test_indices(test4)
 
 
-async def test_timing_more_than_tests(
+async def test_output_when_slowest_tests_count_more_than_tests(
     testing_summary: TestingSummary, no_color_log_color_provider: LogColorProvider
 ):
     # Specyfing a number too big should have no effect, because of potential skipped tests.
     test5 = testing_summary._format_slow_test_cases_list(5, no_color_log_color_provider)
     assert_exec_times_in_desc_order(test5)
-    assert {1, 2, 3, 4} == get_one_digit_test_indices(test5)
+    assert {1, 2, 3, 4} == get_test_indices(test5)
 
 
-async def test_timing_zero(testing_summary: TestingSummary):
+async def test_output_when_slowest_tests_count_is_zero(testing_summary: TestingSummary):
     # Zero should yield no result
     test0 = testing_summary._format_slow_test_cases_list(0)
     assert test0 == ""
