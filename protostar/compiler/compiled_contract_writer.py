33ad368fc7b6191d2e35ba38c7ee5cacb60aa675
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-08-03 21:01:47 +0200

    refine naming

--- protostar/compiler/compiled_contract_writer.py
+++ protostar/compiler/compiled_contract_writer.py
@@ -17,17 +17,15 @@ class CompiledContractWriter:
     def save_compiled_contract(self, output_dir: Path) -> None:
         self._create_output_dir(output_dir)
         serialized_contract = self._contract.Schema().dump(self._contract)
-        self._save_as_json(
-            data=serialized_contract, path=output_dir / f"{self._contract_name}.json"
-        )
+        file_path = output_dir / f"{self._contract_name}.json"
+        self._save_as_json(data=serialized_contract, path=file_path)
 
     def save_compiled_contract_abi(self, output_dir: Path) -> None:
         if not self._contract.abi:
             return
         self._create_output_dir(output_dir)
-        self._save_as_json(
-            data=self._contract.abi, path=output_dir / f"{self._contract_name}_abi.json"
-        )
+        file_path = output_dir / f"{self._contract_name}_abi.json"
+        self._save_as_json(data=self._contract.abi, path=file_path)
 
     @staticmethod
     def _create_output_dir(output_dir: Path):
@@ -40,6 +38,6 @@ class CompiledContractWriter:
             path,
             mode="w",
             encoding="utf-8",
-        ) as output_abi_file:
-            json.dump(data, output_abi_file, indent=4, sort_keys=True)
-            output_abi_file.write("\n")
+        ) as output_file:
+            json.dump(data, output_file, indent=4, sort_keys=True)
+            output_file.write("\n")
