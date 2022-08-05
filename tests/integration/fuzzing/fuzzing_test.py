e28c6b8af9aaec1c06a4e1dc7cd9cd619caf7eb2
Marek Kaput <marek.kaput@swmansion.com>
2022-08-05 09:50:26 +0200

    clarify

--- tests/integration/fuzzing/fuzzing_test.py
+++ tests/integration/fuzzing/fuzzing_test.py
@@ -98,7 +98,7 @@ async def test_max_fuzz_runs_less_or_equal_than_specified(
 async def test_strategies(
     run_cairo_test_runner: RunCairoTestRunnerFixture,
 ):
-    fuzz_max_examples = 10
+    fuzz_max_examples = 100
 
     testing_summary = await run_cairo_test_runner(
         Path(__file__).parent / "strategies_test.cairo",
