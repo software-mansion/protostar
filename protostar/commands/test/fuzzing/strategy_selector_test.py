dddee788fd46c15a165eb3513590c5d56288e930
Marek Kaput <marek.kaput@swmansion.com>
2022-08-11 14:12:08 +0200

    make strategies selector test independent of existing strategies

--- protostar/commands/test/fuzzing/strategy_selector_test.py
+++ protostar/commands/test/fuzzing/strategy_selector_test.py
@@ -1,7 +1,14 @@
 import re
+from dataclasses import dataclass
+from typing import Any
 
 import pytest
-from starkware.cairo.lang.compiler.ast.cairo_types import TypeFelt, TypePointer
+from hypothesis.strategies import SearchStrategy, integers
+from starkware.cairo.lang.compiler.ast.cairo_types import (
+    TypeFelt,
+    TypePointer,
+    CairoType,
+)
 
 from protostar.commands.test.fuzzing.exceptions import (
     FuzzingError,
@@ -9,12 +16,18 @@ from protostar.commands.test.fuzzing.exceptions import (
 )
 from protostar.commands.test.fuzzing.strategies.felt import (
     UnsignedFeltStrategyDescriptor,
-    SignedFeltStrategyDescriptor,
 )
+from protostar.commands.test.fuzzing.strategy_descriptor import StrategyDescriptor
 from protostar.commands.test.fuzzing.strategy_selector import (
     StrategySelector,
     infer_strategy_from_cairo_type,
 )
+
+
+@dataclass
+class StubStrategyDescriptor(StrategyDescriptor):
+    def build_strategy(self, cairo_type: CairoType) -> SearchStrategy[Any]:
+        return integers()
 
 
 def test_pointer_parameter():
@@ -31,12 +44,12 @@ def test_learn():
     strategy_selector = StrategySelector({"x": TypeFelt()})
     strategy0 = strategy_selector.given_strategies["x"]
 
-    assert strategy_selector.learn("x", SignedFeltStrategyDescriptor())
+    assert strategy_selector.learn("x", StubStrategyDescriptor())
 
     strategy1 = strategy_selector.given_strategies["x"]
     assert strategy1 is not strategy0
 
-    assert not strategy_selector.learn("x", SignedFeltStrategyDescriptor())
+    assert not strategy_selector.learn("x", StubStrategyDescriptor())
 
     strategy2 = strategy_selector.given_strategies["x"]
     assert strategy2 is strategy1
@@ -45,7 +58,7 @@ def test_learn():
 def test_learn_unknown_parameter():
     strategy_selector = StrategySelector({})
     with pytest.raises(FuzzingError, match=re.escape("Unknown fuzzing parameter 'x'.")):
-        strategy_selector.learn("x", SignedFeltStrategyDescriptor())
+        strategy_selector.learn("x", StubStrategyDescriptor())
 
 
 def test_infer_strategy_from_cairo_type_felt():
