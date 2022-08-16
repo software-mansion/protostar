4f8a696c3d12cfee542d4102c25a938f4949a22b
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-08-16 16:42:05 +0200

    fix test and make color provider disabled by default

--- tests/integration/testing_output/testing_output_test.py
+++ tests/integration/testing_output/testing_output_test.py
@@ -2,8 +2,7 @@ from pathlib import Path
 
 import pytest
 
-from protostar.commands.test.test_result_formatter import TestResultFormatter
-from protostar.utils.log_color_provider import LogColorProvider
+from protostar.commands.test.test_result_formatter import format_test_result
 from tests.integration.conftest import (
     RunCairoTestRunnerFixture,
     assert_cairo_test_cases,
@@ -13,7 +12,6 @@ from tests.integration.conftest import (
 @pytest.mark.asyncio
 async def test_testing_output(
     run_cairo_test_runner: RunCairoTestRunnerFixture,
-    log_color_provider: LogColorProvider,
 ):
     testing_summary = await run_cairo_test_runner(
         Path(__file__).parent / "testing_output_test.cairo"
@@ -28,10 +26,8 @@ async def test_testing_output(
         expected_failed_test_cases_names=[],
     )
 
-    test_result_formatter = TestResultFormatter(log_color_provider=log_color_provider)
-
     for passed_test_case in testing_summary.passed:
-        output = test_result_formatter.format(passed_test_case)
+        output = format_test_result(passed_test_case)
         assert "steps=" in output
         assert "pedersen_builtin=" in output
         assert "range_check=" not in output
