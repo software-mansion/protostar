34a1b6cbe08fcc8861a98108c7375a465489668b
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-08-16 17:38:41 +0200

    refine no protostar toml exception

--- protostar/protostar_toml/io/protostar_toml_reader.py
+++ protostar/protostar_toml/io/protostar_toml_reader.py
@@ -72,9 +72,7 @@ class ProtostarTOMLReader:
             return self._cache
 
         if not self.path.is_file():
-            raise NoProtostarProjectFoundException(
-                "No protostar.toml found in the working directory\n" f"{str(self.path)}"
-            )
+            raise NoProtostarProjectFoundException("`protostar.toml` not found")
 
         with open(self.path, "rb") as protostar_toml_file:
             protostar_toml_dict = tomli.load(protostar_toml_file)
