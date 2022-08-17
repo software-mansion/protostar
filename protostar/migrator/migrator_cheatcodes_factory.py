e915e0b3e66dc7937ce009e1a0e8b3d794111c81
Arkadiusz <arkadiusz.kasprzyk@swmansion.com>
2022-08-17 15:04:02 +0200

    Migrator `call` cheatcode (#618)
    * Initial draft
    
    * Removed artifacts
    
    * Cleanup
    
    * Added protocols
    
    * add compiled_project fixture
    
    * fix project testing environemnet
    
    * rename fixtures
    
    * use constants for fixture names
    
    * fix importing fixtures
    
    * use module scope
    
    * add declare test
    
    * add tests
    
    * add more learning tests
    
    * remove invoke and _add call test
    
    * _add test
    
    * refine test
    
    * change fixture desired interface
    
    * _add protostar fixture
    
    * add protostar fixture
    
    * use protostar fixture for declare cheatcode
    
    * use protostar fixture in gateway facade
    
    * remove asyncs
    
    * remove dead code
    
    * use protostar fixture
    
    * make call pass
    
    * move contract to tests.data
    
    * fix creating migration output file
    
    * add more tests
    
    * format
    
    * handle errors
    
    * fix gateway facade tests
    
    * fix types and format
    
    * refine
    
    * fix protostar toml
    
    * fix lint
    
    * remove invoke
    
    * remove call cheatcode
    
    * Revert "remove call cheatcode"
    
    This reverts commit e2871d2fbcbc10f3bd15f9681d331b0077db73d2.
    
    * refine no protostar toml exception
    
    * remove "call" literal type
    
    * Merge branch 'feature/call_and_invoke' of https://github.com/software-mansion/protostar into feature/migrator-call-cheat
    
    * rename type
    
    * remove unnecessary log color provider
    
    * remove cwd
    
    * remove cwd
    
    * fix types
    
    * remove type
    
    * renamed cwd to project root path
    
    * fix e2e tests
    
    Co-authored-by: Igor Urbanik <igorurbanik23@gmail.com>

--- protostar/migrator/migrator_cheatcodes_factory.py
+++ protostar/migrator/migrator_cheatcodes_factory.py
@@ -3,6 +3,7 @@ from typing import List, Optional
 
 from starkware.starknet.business_logic.execution.objects import CallInfo
 
+from protostar.migrator.cheatcodes.migrator_call_cheatcode import MigratorCallCheatcode
 from protostar.migrator.cheatcodes.migrator_declare_cheatcode import (
     MigratorDeclareCheatcode,
 )
@@ -51,4 +52,5 @@ class MigratorCheatcodeFactory(CheatcodeFactory):
                 self.gateway_facade,
                 config=MigratorDeployContractCheatcode.Config(token=self._config.token),
             ),
+            MigratorCallCheatcode(syscall_dependencies, self.gateway_facade),
         ]
