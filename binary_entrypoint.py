416d0a676c29962dd58d19455bc88c0daaf05efc
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-08-17 16:11:58 +0200

    enable activity indicator for tty only

--- binary_entrypoint.py
+++ binary_entrypoint.py
@@ -7,30 +7,36 @@ from threading import Thread
 from time import perf_counter, sleep
 
 import certifi
+from colorama.ansitowin32 import StreamWrapper
 
 SCRIPT_ROOT = Path(__file__).parent
 
 
 def init():
     start_time = perf_counter()
-    fix_ssl_certificate_errors_on_mac_os()
+    fix_ssl_certificate_errors_on_macos()
     main = import_protostar_main()
     main(SCRIPT_ROOT, start_time)
 
 
-def fix_ssl_certificate_errors_on_mac_os():
+def fix_ssl_certificate_errors_on_macos():
     os.environ["SSL_CERT_FILE"] = certifi.where()
 
 
 def import_protostar_main():
     try:
-        # pylint: disable="import-outside-toplevel"
-        from protostar import main
+        with ProtostarInitializingIndicator(disabled=not is_terminal()):
+            # pylint: disable="import-outside-toplevel"
+            from protostar import main
 
-        return main
+            return main
     except ImportError as err:
         handle_git_error(err)
         raise err
+
+
+def is_terminal() -> bool:
+    return StreamWrapper(sys.stdout, sys.stdin).isatty()
 
 
 def handle_git_error(err: ImportError):
@@ -42,17 +48,21 @@ def handle_git_error(err: ImportError):
 
 
 class ProtostarInitializingIndicator:
-    def __init__(self):
+    def __init__(self, disabled: bool):
         self._thread = Thread(target=self._animate, daemon=True)
         self.steps = ["⢿", "⣻", "⣽", "⣾", "⣷", "⣯", "⣟", "⡿"]
         self.interval = 1 / len(self.steps)
         self.done = False
+        self._disabled = disabled
 
     def start(self):
         self._thread.start()
         return self
 
     def _animate(self):
+        if self._disabled:
+            return
+
         for step in cycle(self.steps):
             if self.done:
                 break
