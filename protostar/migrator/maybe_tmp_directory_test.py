b0bfc54020053b9275664f8577af91dea0acea95
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-08-18 18:02:12 +0200

    remove `some`

--- protostar/migrator/maybe_tmp_directory_test.py
+++ protostar/migrator/maybe_tmp_directory_test.py
@@ -7,14 +7,14 @@ def test_directory_exists_when_if_not_empty(tmp_path: Path):
     output_dir = tmp_path / "tmp_dir"
 
     with MaybeTmpDirectory(output_dir):
-        _save_some_file(output_dir / "some_file.txt")
+        _save_file(output_dir / "file.txt")
 
     assert output_dir.exists()
 
 
-def _save_some_file(file_path: Path):
-    with open(file_path, "w", encoding="utf-8") as some_file:
-        some_file.write("")
+def _save_file(file_path: Path):
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write("")
 
 
 def test_directory_not_exists_if_empty(tmp_path: Path):
