86bc7345bddafdc3541f17919a147da436e3eada
Arcticae <tomekgsd@gmail.com>
2022-08-18 19:28:27 +0200

    Remove perf test from test workflow. Add exp backoff, debug info in case of failed devnet start

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,19 @@ from socket import socket as Socket
 from typing import List
 
 import pytest
+import requests
+
+
+def ensure_devnet_alive(port: int, retries=5, base_backoff_time=2) -> bool:
+    for i in range(retries):
+        try:
+            res = requests.get(f"http://localhost:{port}/is_alive")
+            if res.status_code == 200:
+                return True
+        except requests.exceptions.ConnectionError:
+            backoff_time = base_backoff_time * (2 ** (i - 1))  # Use exp backoff
+            time.sleep(backoff_time)
+    return False
 
 
 def run_devnet(devnet: List[str], port: int) -> subprocess.Popen:
@@ -19,9 +32,17 @@ def run_devnet(devnet: List[str], port: int) -> subprocess.Popen:
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
     )
-    time.sleep(5)
+    is_alive = ensure_devnet_alive(port)
+    if is_alive:
+        return proc
 
-    return proc
+    appended_stdout = None
+    if proc.poll():
+        appended_stdout = f"stdout: {proc.stdout.read()}"
+
+    raise RuntimeError(
+        f"Devnet failed to start on port {port}" + (appended_stdout or "")
+    )
 
 
 @pytest.fixture(name="devnet_port")
