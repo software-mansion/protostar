669baefd4a9f0dfb4b6e9c212f66683cac01f7f3
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-08-19 17:51:14 +0200

    make state more abstract

--- protostar/migrator/migrator_datetime_state.py
+++ protostar/migrator/migrator_datetime_state.py
@@ -11,12 +11,8 @@ class MigratorDateTimeState:
     def update_to_now(self):
         self._datetime = DateTime.now()
 
-    def _get_prefix(self) -> Optional[str]:
+    def get_output_stem(self) -> Optional[str]:
         assert self._datetime is not None
-        return DateTime.strftime(self._datetime, "%Y%m%d%H%M%S")
-
-    def get_compilation_output_path(self) -> Path:
-        migrations_dir = self._migration_file_path.parent
-        migration_stem = self._migration_file_path.stem
-        prefix = self._get_prefix()
-        return migrations_dir / f"{prefix}_{migration_stem}"
+        migration_file_stem = self._migration_file_path.stem
+        prefix = DateTime.strftime(self._datetime, "%Y%m%d%H%M%S")
+        return f"{prefix}_{migration_file_stem}"
