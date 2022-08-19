f88129ea1a9f6a4bd6b1ecacbf3f3ca601e3b65b
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-08-19 17:43:25 +0200

    add get_compilation_output_path

--- protostar/migrator/migrator_datetime_state.py
+++ protostar/migrator/migrator_datetime_state.py
@@ -1,15 +1,22 @@
 from datetime import datetime as DateTime
+from pathlib import Path
 from typing import Optional
 
 
 class MigratorDateTimeState:
-    def __init__(self) -> None:
+    def __init__(self, migration_file_path: Path) -> None:
+        self._migration_file_path = migration_file_path
         self._datetime: Optional[DateTime] = None
 
     def update_to_now(self):
         self._datetime = DateTime.now()
 
-    def get_prefix(self) -> Optional[str]:
-        if self._datetime is None:
-            return self._datetime
+    def _get_prefix(self) -> Optional[str]:
+        assert self._datetime is not None
         return DateTime.strftime(self._datetime, "%Y%m%d%H%M%S")
+
+    def get_compilation_output_path(self) -> Path:
+        migrations_dir = self._migration_file_path.parent
+        migration_stem = self._migration_file_path.stem
+        prefix = self._get_prefix()
+        return migrations_dir / f"{prefix}_{migration_stem}"
--- protostar/migrator/migrator_datetime_state_test.py
+++ protostar/migrator/migrator_datetime_state_test.py
@@ -1,21 +1,17 @@
+from pathlib import Path
+
 from freezegun import freeze_time
 
 from .migrator_datetime_state import MigratorDateTimeState
 
 
 @freeze_time("2022-04-02 21:37:42")
-def test_migrator_datetime_state():
-    state = MigratorDateTimeState()
-
+def test_output_directory(tmp_path: Path):
+    migration_file_stem = "migration_01_init"
+    state = MigratorDateTimeState(
+        migration_file_path=tmp_path / f"{migration_file_stem}.cairo"
+    )
     state.update_to_now()
-    prefix = state.get_prefix()
+    output_path = state.get_compilation_output_path()
 
-    assert prefix == "20220402213742"
-
-
-def test_prefix_when_state_is_empty():
-    state = MigratorDateTimeState()
-
-    prefix = state.get_prefix()
-
-    assert prefix is None
+    assert str(output_path) == str(tmp_path / f"20220402213742_{migration_file_stem}")
