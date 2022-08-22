86a317b20c24626c98e00d86fa8d1cf861f04ef3
Igor Urbanik <igorurbanik23@gmail.com>
2022-08-22 12:28:51 +0200

    Added unit test

--- protostar/cli/command_test.py
+++ protostar/cli/command_test.py
@@ -19,3 +19,10 @@ def test_directory_type(tmpdir):
 def test_directory_type_checks_existence():
     with pytest.raises(AssertionError):
         Command.Argument.Type.directory("!@#$")
+
+
+def test_int_type():
+    ints = ["123", "0xf", "0b001", "0o444", "0xDEADC0DE"]
+    for i in ints:
+        result = Command.Argument.Type.int(i)
+        assert isinstance(result, int)
