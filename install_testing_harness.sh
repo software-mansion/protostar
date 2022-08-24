31c8ae7541eb12de4db18aa82930471104d4b4f9
Arkadiusz <arkadiusz.kasprzyk@swmansion.com>
2022-08-24 11:16:44 +0200

    `install.sh` tests (#650)

---
+++ install_testing_harness.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+function exit_if_empty() {
+    arg_name=$1
+    arg_value=$2
+
+    if [[ ! $arg_value ]]; then
+        echo "Missing positional argument: $arg_name"
+        exit 1
+    fi
+}
+
+exit_if_empty "HOME_MOCK" $1
+HOME=$1
+
+exit_if_empty "SHELL_MOCK" $2
+SHELL=$2
+
+function uname() {
+    read -p "[uname$(printf ' %s' "$@")]: " response
+    echo $response
+}
+
+function curl() {
+    read -p "[curl$(printf ' %s' "$@")]: " response
+    echo $response
+}
+
+function tar() {
+    read response
+    echo "[tar $response]"
+}
+
+source ./install.sh
