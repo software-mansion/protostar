9faded5ce17e4ec948331dc7c3e11b9490027139
kasperski95 <arkadiuszkasprzyk95@gmail.com>
2022-08-24 12:10:12 +0200

    test installing specific version

--- tests/e2e/test_install_sh.py
+++ tests/e2e/test_install_sh.py
@@ -5,6 +5,7 @@ from shutil import copytree
 
 import pexpect
 import pytest
+from typing_extensions import Protocol
 
 PROTOSTAR_REPO_URL = "https://github.com/software-mansion/protostar"
 
@@ -19,9 +20,9 @@ class ScriptTestingHarness:
     def expect_kernel_name_uname_prompt(self) -> None:
         self.expect("\\[uname -s]:")
 
-    def expect_latest_release_response_curl_prompt(self) -> None:
+    def expect_release_response_curl_prompt(self, requested_ref: str) -> None:
         self.expect(
-            f"\\[curl -L -s -H Accept: application/json {PROTOSTAR_REPO_URL}/releases/latest]:"
+            f"\\[curl -L -s -H Accept: application/json {PROTOSTAR_REPO_URL}/releases/{requested_ref}]:"
         )
 
     def expect_download_curl_prompt(self, filename: str, version: str) -> None:
@@ -39,15 +40,12 @@ class ScriptTestingHarness:
         self._process.sendline(value)
 
 
-def run_testing_harness(home_path: Path, shell: str) -> ScriptTestingHarness:
-    command = f"bash ./install_testing_harness.sh {home_path} {shell}"
+def run_testing_harness(
+    home_path: Path, shell: str, version: str = ""
+) -> ScriptTestingHarness:
+    command = f"bash ./install_testing_harness.sh {home_path} {shell} {version}"
     process = pexpect.spawn(command, timeout=1)
     return ScriptTestingHarness(process)
-
-
-@pytest.fixture(name="latest_protostar_version")
-def latest_protostar_version_fixture() -> str:
-    return "9.9.9"
 
 
 @pytest.fixture(autouse=True)
@@ -56,6 +54,24 @@ def setup(protostar_repo_root: Path):
     os.chdir(protostar_repo_root)
     yield
     os.chdir(cwd)
+
+
+@pytest.fixture(name="latest_protostar_version")
+def latest_protostar_version_fixture() -> str:
+    return "9.9.9"
+
+
+class SimulateUnwrappingFixture(Protocol):
+    def __call__(self, output_dir: Path) -> None:
+        ...
+
+
+@pytest.fixture(name="simulate_unwrapping")
+def simulate_unwrapping_fixture(datadir: Path) -> SimulateUnwrappingFixture:
+    def simulate_unwrapping(output_dir: Path):
+        copytree(src=datadir / "dist", dst=output_dir / ".protostar" / "dist")
+
+    return simulate_unwrapping
 
 
 @pytest.mark.parametrize(
@@ -68,7 +84,7 @@ def setup(protostar_repo_root: Path):
 def test_installing_latest_version(
     tmp_path: Path,
     latest_protostar_version: str,
-    datadir: Path,
+    simulate_unwrapping: SimulateUnwrappingFixture,
     kernel: str,
     tar_filename: str,
     shell: str,
@@ -78,14 +94,60 @@ def test_installing_latest_version(
     fake_home_path = tmp_path
 
     harness = run_testing_harness(home_path=fake_home_path, shell=shell)
+
     harness.expect_kernel_name_uname_prompt()
     harness.send(kernel)
-    harness.expect_latest_release_response_curl_prompt()
+
+    harness.expect_release_response_curl_prompt(requested_ref="latest")
     harness.send(f'"tag_name":"v{latest_protostar_version}"')
+
     harness.expect_download_curl_prompt(tar_filename, latest_protostar_version)
     harness.send("DATA")
+
     harness.expect_tar_info(data="DATA")
-    copytree(src=datadir / "dist", dst=fake_home_path / ".protostar" / "dist")
+    simulate_unwrapping(output_dir=fake_home_path)
+
+    harness.expect_detected_shell(shell_name=shell_name)
+
+    assert_config_file_includes_path_entry(
+        file_path=fake_home_path / shell_config_path, home_path=fake_home_path
+    )
+
+
+@pytest.mark.parametrize(
+    "kernel, tar_filename, shell, shell_name, shell_config_path",
+    (("Darwin", "protostar-macOS.tar.gz", "/bin/zsh", "zsh", Path("./.zshrc")),),
+)
+def test_installing_specific_version(
+    tmp_path: Path,
+    simulate_unwrapping: SimulateUnwrappingFixture,
+    kernel: str,
+    tar_filename: str,
+    shell: str,
+    shell_name: str,
+    shell_config_path: Path,
+):
+    requested_version = "0.1.0"
+    fake_home_path = tmp_path
+
+    harness = run_testing_harness(
+        home_path=fake_home_path, shell=shell, version=requested_version
+    )
+
+    harness.expect_kernel_name_uname_prompt()
+    harness.send(kernel)
+
+    harness.expect_release_response_curl_prompt(
+        requested_ref=f"tag/v{requested_version}"
+    )
+    harness.send(f'"tag_name":"v{requested_version}"')
+
+    harness.expect_download_curl_prompt(tar_filename, requested_version)
+    harness.send("DATA")
+
+    harness.expect_tar_info(data="DATA")
+    simulate_unwrapping(output_dir=fake_home_path)
+
     harness.expect_detected_shell(shell_name=shell_name)
 
     assert_config_file_includes_path_entry(
