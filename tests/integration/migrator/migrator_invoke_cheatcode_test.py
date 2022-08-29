52918dc6b7623f72ca583b2665d54caf280c6855
Arcticae <tomekgsd@gmail.com>
2022-08-29 11:12:47 +0200

    Correct test

--- tests/integration/migrator/migrator_invoke_cheatcode_test.py
+++ tests/integration/migrator/migrator_invoke_cheatcode_test.py
@@ -31,30 +31,6 @@ assert result.res == 42
     )
 
 
-async def test_waiting_for_pending(
-    migrate: MigrateFixture, signing_credentials, monkeypatch
-):
-    private_key, acc_address = signing_credentials
-    monkeypatch.setenv(PRIVATE_KEY_ENV_VAR_NAME, private_key)
-
-    await migrate(
-        """
-contract_address = deploy_contract("./build/main.json", constructor_args=[0]).contract_address
-
-invocation = invoke(contract_address, "increase_balance", {"amount": 42}, auto_estimate_fee=True)
-
-invocation.wait_for_acceptance()
-
-assert invocation.status == "PENDING"
-
-result = call(contract_address, "get_balance")
-
-assert result.res == 42
-""",
-        account_address=acc_address,
-    )
-
-
 async def test_waiting_for_acceptance(
     migrate: MigrateFixture, signing_credentials, monkeypatch
 ):
@@ -67,9 +43,9 @@ contract_address = deploy_contract("./build/main.json", constructor_args=[0]).co
 
 invocation = invoke(contract_address, "increase_balance", {"amount": 42}, auto_estimate_fee=True)
 
-invocation.wait_for_acceptance()
+invocation = invocation.wait_for_acceptance_sync()
 
-assert invocation.status == "ACCEPTED_ON_L2"
+assert invocation.status.value == "ACCEPTED_ON_L2", f"Status is {invocation.status}"
 
 result = call(contract_address, "get_balance")
 
