eb975bf5c90b4d40419a09ff2291668dfc935c7e
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-09-01 17:15:54 +0200

    test retrieving min version

--- protostar/configuration_file/configuration_file_v2.py
+++ protostar/configuration_file/configuration_file_v2.py
@@ -2,13 +2,13 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import List, Optional, Union
 
-from protostar.protostar_toml.io.protostar_toml_reader import ProtostarTOMLReader
-from protostar.utils.protostar_directory import VersionType
+from protostar.utils.protostar_directory import VersionManager, VersionType
 
 from .configuration_file import (
     ConfigurationFile,
     PrimitiveTypesSupportedByConfigurationFile,
 )
+from .configuration_toml_reader import ConfigurationTOMLReader
 
 
 @dataclass
@@ -17,14 +17,17 @@ class ConfigurationFileV2Model:
 
 
 class ConfigurationFileV2(ConfigurationFile[ConfigurationFileV2Model]):
-    def __init__(self, protostar_toml_reader: ProtostarTOMLReader) -> None:
+    def __init__(self, configuration_toml_reader: ConfigurationTOMLReader) -> None:
         super().__init__()
-        self._protostar_toml_reader = protostar_toml_reader
+        self._configuration_toml_reader = configuration_toml_reader
 
     def get_min_protostar_version(self) -> Optional[VersionType]:
-        self._protostar_toml_reader.get_attribute(
+        version_str = self._configuration_toml_reader.get_attribute(
             section_name="project", attribute_name="min-protostar-version"
         )
+        if not version_str:
+            return None
+        return VersionManager.parse(version_str)
 
     def get_contract_names(self) -> List[str]:
         ...
--- protostar/configuration_file/configuration_file_v2_test.py
+++ protostar/configuration_file/configuration_file_v2_test.py
@@ -2,10 +2,10 @@ from pathlib import Path
 
 import pytest
 
-from protostar.protostar_toml.io.protostar_toml_reader import ProtostarTOMLReader
 from protostar.utils.protostar_directory import VersionManager
 
 from .configuration_file_v2 import ConfigurationFileV2
+from .configuration_toml_reader import ConfigurationTOMLReader
 
 
 @pytest.fixture(name="protostar_toml_content")
@@ -30,7 +30,7 @@ def protostar_toml_content_fixture() -> str:
 def configuration_file_fixture(tmp_path: Path, protostar_toml_content: str):
     protostar_toml_path = tmp_path / "protostar.toml"
     protostar_toml_path.write_text(protostar_toml_content)
-    protostar_toml_reader = ProtostarTOMLReader(protostar_toml_path=protostar_toml_path)
+    protostar_toml_reader = ConfigurationTOMLReader(path=protostar_toml_path)
     return ConfigurationFileV2(protostar_toml_reader)
 
 
