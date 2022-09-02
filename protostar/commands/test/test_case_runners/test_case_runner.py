688c2e623653dc24e19d56782bdad54855686619
Igor Urbanik <39826471+Radinyn@users.noreply.github.com>
2022-09-02 15:44:28 +0200

    remove unused print
    Co-authored-by: Marek Kaput <marek.kaput@outlook.com>

--- protostar/commands/test/test_case_runners/test_case_runner.py
+++ protostar/commands/test/test_case_runners/test_case_runner.py
@@ -52,7 +52,6 @@ class TestCaseRunner(Generic[TExecutionResult]):
                 TestCaseRunner.ExecutionMetadata(self._stopwatch.total_elapsed),
             )
         except ReportedException as ex:
-            print(f"CAUGHT EXCEPTION: {type(ex)}")
             return self._map_reported_exception_to_failed_test_result(
                 ex,
                 TestCaseRunner.ExecutionMetadata(self._stopwatch.total_elapsed),
