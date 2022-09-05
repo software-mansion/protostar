06e4b8decd11bfd493fdcd580236c9924599b936
Radinyn <igorurbanik23@gmail.com>
2022-09-05 14:26:42 +0200

    Added `map`, `filter` to strategies, added `one_of` strategy

---
+++ protostar/commands/test/fuzzing/strategies/one_of.py
@@ -0,0 +1,22 @@
+from hypothesis.strategies import SearchStrategy, one_of
+from starkware.cairo.lang.compiler.ast.cairo_types import CairoType
+
+from protostar.commands.test.fuzzing.exceptions import SearchStrategyBuildError
+from protostar.commands.test.fuzzing.strategy_descriptor import StrategyDescriptor
+
+
+class OneOfStrategyDescriptor(StrategyDescriptor):
+    def __init__(self, *args: StrategyDescriptor):
+        for arg in args:
+            if not isinstance(arg, StrategyDescriptor):  # type: ignore
+                raise SearchStrategyBuildError(
+                    "Strategy 'one_of' takes only other strategies as arguments."
+                )
+        if len(args) == 0:
+            raise SearchStrategyBuildError(
+                "Strategy 'one_of' takes at least one argument."
+            )
+        self.args = args
+
+    def build_strategy(self, cairo_type: CairoType) -> SearchStrategy[int]:
+        return one_of(*(arg.build_strategy(cairo_type) for arg in self.args))
