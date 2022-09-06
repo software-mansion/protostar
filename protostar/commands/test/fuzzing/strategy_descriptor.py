c4131f29cc09f87c1053ed7f2fe20b6d112df2e4
Igor Urbanik <39826471+Radinyn@users.noreply.github.com>
2022-09-06 20:54:00 +0200

    Added `map`, `filter` to strategies, added `one_of` strategy (#752)
    Co-authored-by: Tomasz Rejowski <34059895+Arcticae@users.noreply.github.com>
    Co-authored-by: Marek Kaput <marek.kaput@outlook.com>
    Co-authored-by: Arcticae <tomekgsd@gmail.com>
    Co-authored-by: Maksymilian Demitraszek <32527780+MaksymilianDemitraszek@users.noreply.github.com>

--- protostar/commands/test/fuzzing/strategy_descriptor.py
+++ protostar/commands/test/fuzzing/strategy_descriptor.py
@@ -1,10 +1,38 @@
 from abc import ABC, abstractmethod
+from typing import Callable
 
 from hypothesis.strategies import SearchStrategy
 from starkware.cairo.lang.compiler.ast.cairo_types import CairoType
+
+MappingFunction = Callable[[int], int]
+FilterFunction = Callable[[int], bool]
 
 
 class StrategyDescriptor(ABC):
     @abstractmethod
     def build_strategy(self, cairo_type: CairoType) -> SearchStrategy:
         ...
+
+    def filter(self, filter_function: FilterFunction) -> "FilteredStrategyDescriptor":
+        return FilteredStrategyDescriptor(inner=self, function=filter_function)
+
+    def map(self, mapping_function: MappingFunction) -> "MappedStrategyDescriptor":
+        return MappedStrategyDescriptor(inner=self, function=mapping_function)
+
+
+class MappedStrategyDescriptor(StrategyDescriptor):
+    def __init__(self, function: MappingFunction, inner: StrategyDescriptor):
+        self.inner = inner
+        self.function = function
+
+    def build_strategy(self, cairo_type: CairoType) -> SearchStrategy:
+        return self.inner.build_strategy(cairo_type=cairo_type).map(self.function)
+
+
+class FilteredStrategyDescriptor(StrategyDescriptor):
+    def __init__(self, function: FilterFunction, inner: StrategyDescriptor):
+        self.inner = inner
+        self.function = function
+
+    def build_strategy(self, cairo_type: CairoType) -> SearchStrategy:
+        return self.inner.build_strategy(cairo_type=cairo_type).filter(self.function)
