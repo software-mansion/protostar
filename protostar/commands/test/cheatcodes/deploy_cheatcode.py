91078d110b6c94f7f565f94fb3d1f5e78b56947b
Tomasz Rejowski <34059895+Arcticae@users.noreply.github.com>
2022-09-09 10:41:42 +0200

    Align deploy and declare cheatcodes with new state implementation (#779)
    fixes #766

--- protostar/commands/test/cheatcodes/deploy_cheatcode.py
+++ protostar/commands/test/cheatcodes/deploy_cheatcode.py
@@ -1,8 +1,13 @@
+import asyncio
 from typing import Any, Callable, List
 
+from starkware.python.utils import to_bytes
 from starkware.starknet.business_logic.execution.objects import CallInfo
+from starkware.starknet.business_logic.transaction.objects import InternalDeploy
+from starkware.starknet.core.os.transaction_hash.transaction_hash import calculate_deploy_transaction_hash
 
 from protostar.commands.test.cheatcodes.prepare_cheatcode import PreparedContract
+from protostar.migrator.cheatcodes.migrator_deploy_contract_cheatcode import DeployedContract
 from protostar.starknet.cheatcode import Cheatcode
 
 
@@ -27,5 +32,28 @@ class DeployCheatcode(Cheatcode):
         self,
         prepared: PreparedContract,
     ):
-        # TODO(mkaput): Reimplement this.
-        raise NotImplementedError("Deploy cheatcode is not ported to Cairo 10 yet.")
+        deploy_tx_hash = calculate_deploy_transaction_hash(
+            version=0,
+            contract_address=prepared.contract_address,
+            constructor_calldata=prepared.constructor_calldata,
+            chain_id=self.general_config.chain_id.value,
+        )
+
+        tx = InternalDeploy(
+            contract_address=prepared.contract_address,
+            contract_address_salt=prepared.salt,
+            contract_hash=to_bytes(prepared.class_hash),
+            constructor_calldata=prepared.constructor_calldata,
+            hash_value=deploy_tx_hash,
+            version=0,
+        )
+
+        asyncio.run(self._apply_deploy_tx_updates(tx))
+
+        return DeployedContract(contract_address=prepared.contract_address)
+
+    async def _apply_deploy_tx_updates(self, tx: InternalDeploy):
+        with self.cheatable_state.copy_and_apply() as state_copy:
+            await tx.apply_state_updates(
+                state=state_copy, general_config=self.general_config
+            )
