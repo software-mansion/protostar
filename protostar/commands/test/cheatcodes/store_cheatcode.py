8e520c32b2a26eff8940d6dedd11fa6e62cafac1
Maksymilian Demitraszek <32527780+MaksymilianDemitraszek@users.noreply.github.com>
2022-09-09 12:25:15 +0200

    Fixed load, store (#786)

--- protostar/commands/test/cheatcodes/store_cheatcode.py
+++ protostar/commands/test/cheatcodes/store_cheatcode.py
@@ -3,7 +3,7 @@ from starkware.starknet.storage.starknet_storage import BusinessLogicStarknetSto
 
 from protostar.starknet.cheatcode import Cheatcode
 from protostar.starknet.storage_var import calc_address
-
+from starkware.starknet.business_logic.state.state import ContractStorageState
 
 ADDR_BOUND = 2**251 - 256
 
@@ -29,28 +29,12 @@ class StoreCheatcode(Cheatcode):
             self.store_local(variable_address, value)
             return
 
-        pre_run_contract_carried_state = self.state.contract_states[
-            target_contract_address
-        ]
-        contract_state = pre_run_contract_carried_state.state
-        contract_state.assert_initialized(contract_address=target_contract_address)
-
-        starknet_storage = BusinessLogicStarknetStorage(
-            commitment_tree=contract_state.storage_commitment_tree,
-            ffc=self.state.ffc,
-            # Pass a copy of the carried storage updates (instead of a reference) - note that
-            # pending_modifications may be modified during the run as a result of an internal call.
-            pending_modifications=dict(pre_run_contract_carried_state.storage_updates),
+        starknet_storage = ContractStorageState(
+            state=self.sync_state, contract_address=target_contract_address
         )
 
         self._write_on_remote_storage(
             starknet_storage, target_contract_address, variable_address, value
-        )
-
-        # Apply modifications to the contract storage.
-        self.state.update_contract_storage(
-            contract_address=target_contract_address,
-            modifications=starknet_storage.get_modifications(),
         )
 
     def store_local(self, address: int, value: List[int]):
@@ -63,4 +47,4 @@ class StoreCheatcode(Cheatcode):
         for i, val in enumerate(value):
             storage.read(address=address + i)
             storage.write(address=address + i, value=val)
-            self.state.modified_contracts[contract] = None
+            self.resources_manager.modified_contracts[self.contract_address] = None
