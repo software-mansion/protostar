e426abe20b67616a6a9f537eab6eb67d54868160
Marek Kaput <marek.kaput@swmansion.com>
2022-09-12 10:09:48 +0200

    fix bug in store cheatcode

--- protostar/commands/test/cheatcodes/store_cheatcode.py
+++ protostar/commands/test/cheatcodes/store_cheatcode.py
@@ -1,9 +1,9 @@
 from typing import Any, Callable, List, Optional
-from starkware.starknet.storage.starknet_storage import BusinessLogicStarknetStorage
+
+from starkware.starknet.business_logic.state.state import ContractStorageState
 
 from protostar.starknet.cheatcode import Cheatcode
 from protostar.starknet.storage_var import calc_address
-from starkware.starknet.business_logic.state.state import ContractStorageState
 
 ADDR_BOUND = 2**251 - 256
 
@@ -34,7 +34,10 @@ class StoreCheatcode(Cheatcode):
         )
 
         self._write_on_remote_storage(
-            starknet_storage, target_contract_address, variable_address, value
+            storage=starknet_storage,
+            contract_address=target_contract_address,
+            variable_address=variable_address,
+            value=value,
         )
 
     def store_local(self, address: int, value: List[int]):
@@ -42,9 +45,9 @@ class StoreCheatcode(Cheatcode):
             self._storage_write(address=address + i, value=val)
 
     def _write_on_remote_storage(
-        self, storage, contract, address: int, value: List[int]
+        self, storage, contract_address, variable_address: int, value: List[int]
     ):
         for i, val in enumerate(value):
-            storage.read(address=address + i)
-            storage.write(address=address + i, value=val)
-            self.resources_manager.modified_contracts[self.contract_address] = None
+            storage.read(address=variable_address + i)
+            storage.write(address=variable_address + i, value=val)
+            self.resources_manager.modified_contracts[contract_address] = None
