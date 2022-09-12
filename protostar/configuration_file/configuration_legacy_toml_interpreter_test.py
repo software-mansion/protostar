c4f177c525de897130fb53d57b368b5378b668cc
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-09-12 18:03:03 +0200

    refactor legacy toml interpreter tests

--- protostar/configuration_file/configuration_legacy_toml_interpreter_test.py
+++ protostar/configuration_file/configuration_legacy_toml_interpreter_test.py
@@ -1,48 +1,17 @@
-from pathlib import Path
-
 import pytest
 
 from .configuration_legacy_toml_interpreter import ConfigurationLegacyTOMLInterpreter
 
 
-@pytest.fixture(name="protostar_toml_content")
-def protostar_toml_content_fixture() -> str:
-    return """\
-    ["protostar.config"]
-    protostar_version = "0.2.4"
-
-    ["protostar.project"]
-    libs_path = "lib"
-
-    ["protostar.contracts"]
-    main = [
-    "./src/main.cairo",
-    ]
-
-    ["protostar.shared_command_configs"]
-    no_color = false
-
-    ["profile.ci.protostar.shared_command_configs"]
-    no-color = true
-
-    ["project"]
-    lib_path = "./foo"
-
-    ["profiler.abc"]
-    foo = 123
-
-    ["profile."]
-    foo = 123
-    """
-
-
-@pytest.fixture(name="protostar_toml_path")
-def protostar_toml_path_fixture(tmp_path: Path, protostar_toml_content: str) -> Path:
-    protostar_toml_path = tmp_path / "protostar.toml"
-    protostar_toml_path.write_text(protostar_toml_content)
-    return protostar_toml_path
-
-
+@pytest.mark.parametrize(
+    "protostar_toml_content",
+    (
+        """
+        ["protostar.config"]
+        protostar_version = "0.2.4"
+        """,
+    ),
+)
 def test_loading_attribute(protostar_toml_content: str):
     reader = ConfigurationLegacyTOMLInterpreter(protostar_toml_content)
 
@@ -55,6 +24,15 @@ def test_loading_attribute(protostar_toml_content: str):
     assert result == "0.2.4"
 
 
+@pytest.mark.parametrize(
+    "protostar_toml_content",
+    (
+        """
+        ["protostar.project"]
+        libs_path = "lib"
+        """,
+    ),
+)
 def test_loading_attribute_when_section_namespace_is_not_provided(
     protostar_toml_content: str,
 ):
@@ -68,6 +46,18 @@ def test_loading_attribute_when_section_namespace_is_not_provided(
     assert result == "./foo"
 
 
+@pytest.mark.parametrize(
+    "protostar_toml_content",
+    (
+        """
+        ["protostar.shared_command_configs"]
+        no_color = false
+
+        ["profile.ci.protostar.shared_command_configs"]
+        no-color = true
+        """,
+    ),
+)
 def test_loading_attribute_from_profile(protostar_toml_content: str):
     reader = ConfigurationLegacyTOMLInterpreter(protostar_toml_content)
 
@@ -87,6 +77,18 @@ def test_loading_attribute_from_profile(protostar_toml_content: str):
     assert profiled_attribute is True
 
 
+@pytest.mark.parametrize(
+    "protostar_toml_content",
+    (
+        """
+        ["protostar.shared_command_configs"]
+        no_color = false
+
+        ["profile.ci.protostar.shared_command_configs"]
+        no-color = true
+        """,
+    ),
+)
 def test_ignoring_attribute_casing(protostar_toml_content: str):
     reader = ConfigurationLegacyTOMLInterpreter(
         protostar_toml_content,
@@ -107,14 +109,23 @@ def test_ignoring_attribute_casing(protostar_toml_content: str):
     assert result2 is False
 
 
-def test_returning_none_on_attribute_not_found(protostar_toml_content: str):
-    result = ConfigurationLegacyTOMLInterpreter(protostar_toml_content).get_attribute(
+def test_returning_none_on_attribute_not_found():
+    result = ConfigurationLegacyTOMLInterpreter("").get_attribute(
         "shared_command_configs", "undefined_attribute"
     )
 
     assert result is None
 
 
+@pytest.mark.parametrize(
+    "protostar_toml_content",
+    (
+        """
+        ["protostar.shared_command_configs"]
+        no_color = false
+        """,
+    ),
+)
 def test_retrieving_section(protostar_toml_content: str):
     result = ConfigurationLegacyTOMLInterpreter(protostar_toml_content).get_section(
         "shared_command_configs", section_namespace="protostar"
@@ -123,14 +134,27 @@ def test_retrieving_section(protostar_toml_content: str):
     assert result == {"no_color": False}
 
 
-def test_returning_none_on_section_not_found(protostar_toml_content: str):
-    result = ConfigurationLegacyTOMLInterpreter(protostar_toml_content).get_section(
-        "undefined_section"
-    )
+def test_returning_none_on_section_not_found():
+    result = ConfigurationLegacyTOMLInterpreter("").get_section("undefined_section")
 
     assert result is None
 
 
+@pytest.mark.parametrize(
+    "protostar_toml_content",
+    (
+        """
+        ["profile.ci.protostar.shared_command_configs"]
+        no-color = true
+
+        ["profiler.abc"]
+        foo = 123
+
+        ["profile."]
+        foo = 123
+        """,
+    ),
+)
 def test_extracting_profile_names(protostar_toml_content: str):
     result = ConfigurationLegacyTOMLInterpreter(
         protostar_toml_content
@@ -139,6 +163,15 @@ def test_extracting_profile_names(protostar_toml_content: str):
     assert result == ["ci"]
 
 
+@pytest.mark.parametrize(
+    "protostar_toml_content",
+    (
+        """
+        ["profiler.abc"]
+        foo = 123
+        """,
+    ),
+)
 def test_section_starting_with_profile(
     protostar_toml_content: str,
 ):
