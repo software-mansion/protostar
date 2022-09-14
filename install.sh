467144574a626e96e24e7ddd335fcfa6988b7c29
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-09-14 15:20:25 +0200

    remove underscore from two more functions

--- install.sh
+++ install.sh
@@ -49,51 +49,51 @@ function get_requested_version() {
 
 function download_protostar() {
     RETVAL=""
-    local _version=$1
-    local _platform=$2
-    local _output=$3
+    local version=$1
+    local platform=$2
+    local output=$3
 
-    local _requested_release_url="${PROTOSTAR_REPO}/releases/download/${_version}"
-    local _protostar_tarball_name="protostar-${_platform}.tar.gz"
-    local _tarball_download_url="${_requested_release_url}/${_protostar_tarball_name}"
-    echo "Downloading protostar from ${_tarball_download_url}"
-    curl -L $_tarball_download_url | tar -xvzC $_output
-    local _protostar_binary_dir="${_output}/dist/protostar"
-    local _protostar_binary="${_protostar_binary_dir}/protostar"
-    chmod +x $_protostar_binary
+    local requested_release_url="${PROTOSTAR_REPO}/releases/download/${version}"
+    local protostar_tarball_name="protostar-${platform}.tar.gz"
+    local tarball_download_url="${requested_release_url}/${protostar_tarball_name}"
+    echo "Downloading protostar from ${tarball_download_url}"
+    curl -L $tarball_download_url | tar -xvzC $output
+    local protostar_binary_dir="${output}/dist/protostar"
+    local protostar_binary="${protostar_binary_dir}/protostar"
+    chmod +x $protostar_binary
 
-    RETVAL=$_protostar_binary_dir
+    RETVAL=$protostar_binary_dir
 }
 
 function add_protostar_to_path() {
     RETVAL=""
-    local _protostar_binary_dir=$1
+    local protostar_binary_dir=$1
 
-    local _profile
-    local _pref_shell
+    local profile
+    local pref_shell
     case $SHELL in
     */zsh)
-        _profile=$HOME/.zshrc
-        _pref_shell=zsh
+        profile=$HOME/.zshrc
+        pref_shell=zsh
         ;;
     */bash)
-        _profile=$HOME/.bashrc
-        _pref_shell=bash
+        profile=$HOME/.bashrc
+        pref_shell=bash
         ;;
     */fish)
-        _profile=$HOME/.config/fish/config.fish
-        _pref_shell=fish
+        profile=$HOME/.config/fish/config.fish
+        pref_shell=fish
         ;;
     *)
-        echo "error: could not detect shell, manually add ${_protostar_binary_dir} to your PATH."
+        echo "error: could not detect shell, manually add ${protostar_binary_dir} to your PATH."
         exit 1
         ;;
     esac
 
-    if [[ ":$PATH:" != *":${_protostar_binary_dir}:"* ]]; then
-        echo >>$_profile && echo "export PATH=\"\$PATH:$_protostar_binary_dir\"" >>$_profile
+    if [[ ":$PATH:" != *":${protostar_binary_dir}:"* ]]; then
+        echo >>$profile && echo "export PATH=\"\$PATH:$protostar_binary_dir\"" >>$profile
     fi
-    echo && echo "Detected your preferred shell is ${_pref_shell} and added Protostar to PATH. Run 'source ${_profile}' or start a new terminal session to use Protostar."
+    echo && echo "Detected your preferred shell is ${pref_shell} and added Protostar to PATH. Run 'source ${profile}' or start a new terminal session to use Protostar."
     echo "Then, run 'protostar --help'."
 }
 
