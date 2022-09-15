8ad650b5bbe79b53606d1423a159f977f0c43bfe
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-09-15 16:45:26 +0200

    update build_installer script

--- scripts/build_installer.sh
+++ scripts/build_installer.sh
@@ -15,7 +15,19 @@ hardware_name="$(uname -m)"
 installer_filename="protostar-$platform_name-$hardware_name.tar.gz"
 
 echo "PREPARING $installer_filename"
-echo "INSTALLING DEPS 1/3" && poetry install &>/dev/null &&
-    echo "BUILDING 2/3" && poe build &>/dev/null &&
-    echo "COMPRESSING 3/3" && tar -czf $installer_filename ./dist/protostar
+echo "[1/4] INSTALLING DEPS" && poetry install &>/dev/null &&
+    echo "[2/4] BUILDING" && poe build &>/dev/null &&
+    echo "[3/4] SMOKE TESTING: protostar -v" && ./dist/protostar/protostar -v &&
+    echo "[4/4] COMPRESSING" && tar -czf $installer_filename ./dist/protostar
 ls -lh $installer_filename
+
+if [ $hardware_name == "arm64" ]; then
+    echo ""
+    echo "To publish the installer:"
+    echo "1. Go to: https://github.com/software-mansion/protostar/releases/latest"
+    echo "2. Click the 'Edit release' icon button"
+    echo "3. Drag and drop $installer_filename"
+    echo "4. Press 'Update release' button"
+    echo ""
+    echo "WARNING: Check if you are on the correct branch, branch is synchronized with remote, and tests are passing."
+fi
