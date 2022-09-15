9f9fdaded1d1a91c201d5987ee06c2b21bb0697d
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-09-15 16:07:40 +0200

    update script

--- scripts/build_arm64_installer.sh
+++ scripts/build_arm64_installer.sh
@@ -1,10 +1,21 @@
+#!/usr/bin/env bash
+
+kernel_name="$(uname -s)"
+case $kernel_name in
+Linux)
+    platform_name="Linux"
+    ;;
+Darwin)
+    platform_name="macOS"
+    ;;
+esac
+
 hardware_name="$(uname -m)"
 
-if [ "$hardware_name" != "arm64" ]; then
-    echo "Wrong hardware: $hardware_name. Expected arm64."
-    exit 1
-fi
+installer_filename="protostar-$platform_name-$hardware_name.tar.gz"
 
-poetry install
-poe build
-tar -czvf protostar-macOS-arm64.tar.gz ./dist/protostar
+echo "PREPARING $installer_filename"
+echo "INSTALLING DEPS 1/3" && poetry install &>/dev/null &&
+    echo "BUILDING 2/3" && poe build &>/dev/null &&
+    echo "COMPRESSING 3/3" && tar -czf $installer_filename ./dist/protostar
+ls -lh $installer_filename
