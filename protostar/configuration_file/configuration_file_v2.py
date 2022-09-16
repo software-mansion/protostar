032ac2a533c658960f3056710008dfe3b86fee05
kasperski95 <arkadiuszkasprzyk95@gmail.com>
2022-09-16 13:37:29 +0200

    lint

--- protostar/configuration_file/configuration_file_v2.py
+++ protostar/configuration_file/configuration_file_v2.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Optional, TypeVar, Union
+from typing import Optional, Union
 
 from protostar.utils.protostar_directory import VersionManager, VersionType
 
