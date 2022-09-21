8bbdf4971c59532b2c2ed376150136034be67a6d
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-09-21 15:50:29 +0200

    remove build

--- tests/integration/cheatcodes/expect_revert/expect_revert_test.py
+++ tests/integration/cheatcodes/expect_revert/expect_revert_test.py
@@ -38,7 +38,6 @@ async def test_expect_revert(run_cairo_test_runner: RunCairoTestRunnerFixture):
 @pytest.fixture(autouse=True, scope="module", name="protostar")
 def protostar_fixture(create_protostar_project: CreateProtostarProjectFixture):
     with create_protostar_project() as protostar:
-        protostar.build_sync()
         yield protostar
 
 
