b90cf006954551aa6786bd6aab1b773d5e4297f6
Marek Kaput <marek.kaput@swmansion.com>
2022-09-23 09:27:03 +0200

    Use GatewayClient instead of request in one test (#880)

--- tests/integration/deploying/declare_command_test.py
+++ tests/integration/deploying/declare_command_test.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 import pytest
-import requests
+from starknet_py.net.gateway_client import GatewayClient
 from starknet_py.net.models import StarknetChainId
 
 from protostar.cli.signable_command_util import PRIVATE_KEY_ENV_VAR_NAME
@@ -63,14 +63,9 @@ async def test_deploying_contract_with_signing(
 
     assert response.class_hash is not None
 
-    # TODO: Use GatewayClient when devnet fixes: https://github.com/Shard-Labs/starknet-devnet/issues/225
-    resp = requests.get(
-        f"{devnet_gateway_url}/feeder_gateway/get_transaction?transactionHash={str(hex(response.transaction_hash))}"
-    ).json()
-
-    assert "transaction" in resp
-    assert "signature" in resp["transaction"]
-    assert resp["transaction"]["signature"] == [
+    gateway_client = GatewayClient(devnet_gateway_url)
+    transaction = await gateway_client.get_transaction(response.transaction_hash)
+    assert transaction.signature == [
         "3459263272550625393812584460277149848351409720716906360199187355059506361232",
         "1830633873577487268914590325347030490499699872026627682646826922183589844384",
     ]
