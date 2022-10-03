01900b10e8bb6e524b8a580e24d6a706fdb7eab9
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-10-03 12:08:21 +0200

    use tmp_path fixture

--- protostar/self/cache_io_test.py
+++ protostar/self/cache_io_test.py
@@ -1,26 +1,11 @@
-import tempfile
 import os
-import shutil
-from pathlib import Path
-
-import pytest
 
 from .cache_io import CacheIO
 
 
-@pytest.fixture(name="test_wrapper")
-def test_wrapper_fixture():
-    cache_dir = []
-    yield cache_dir
-    assert len(cache_dir) == 1
-    shutil.rmtree(cache_dir[0])
-
-
-def test_cache_simple(test_wrapper):
+def test_cache_simple(tmp_path):
     cache_name = "test-cache-simple"
-    cache_io = CacheIO(Path(tempfile.gettempdir()))
-    # pylint: disable=protected-access
-    test_wrapper.append(cache_io._cache_path)
+    cache_io = CacheIO(tmp_path)
     # pylint: disable=protected-access
     cache_file_path = cache_io._cache_path / (cache_name + cache_io._EXTENSION)
 
@@ -38,11 +23,9 @@ def test_cache_simple(test_wrapper):
     assert cache_io.read(cache_name) is None
 
 
-def test_cache_override(test_wrapper):
+def test_cache_override(tmp_path):
     cache_name = "test-cache-override"
-    cache_io = CacheIO(Path(tempfile.gettempdir()))
-    # pylint: disable=protected-access
-    test_wrapper.append(cache_io._cache_path)
+    cache_io = CacheIO(tmp_path)
 
     obj = {"a": 1, "b": True}
     obj2 = {"a": 2}
