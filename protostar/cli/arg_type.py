25dfc9a487fc1bdabf6238f61474f8b6b9fab806
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-10-10 16:35:17 +0200

    move more arg types

--- protostar/cli/arg_type.py
+++ protostar/cli/arg_type.py
@@ -1,4 +1,7 @@
+import re
 from abc import abstractmethod
+from pathlib import Path
+from re import Pattern
 from typing import Generic, TypeVar
 
 ParsingResultT = TypeVar("ParsingResultT")
@@ -20,3 +23,29 @@ class StringArgType(ArgType[str]):
 
     def parse(self, arg: str) -> str:
         return arg
+
+
+class DirectoryArgType(ArgType[Path]):
+    def get_name(self):
+        return "directory"
+
+    def parse(self, arg: str) -> Path:
+        path = Path(arg)
+        assert path.is_dir(), f'"{str(path)}" is not a valid directory path'
+        return path
+
+
+class PathArgType(ArgType[Path]):
+    def get_name(self) -> str:
+        return "path"
+
+    def parse(self, arg: str) -> Path:
+        return Path(arg)
+
+
+class RegexArgType(ArgType[Pattern]):
+    def get_name(self) -> str:
+        return "regex"
+
+    def parse(self, arg: str) -> Pattern:
+        return re.compile(arg)
--- protostar/cli/arg_type_test.py
+++ protostar/cli/arg_type_test.py
@@ -1,4 +1,9 @@
-from .arg_type import StringArgType
+from pathlib import Path
+from re import Pattern
+
+import pytest
+
+from .arg_type import DirectoryArgType, PathArgType, RegexArgType, StringArgType
 
 
 def test_string_arg_type():
@@ -8,3 +13,39 @@ def test_string_arg_type():
 
     assert arg_type.get_name() == "str"
     assert isinstance(result, str)
+
+
+def test_directory_arg_type(tmp_path: Path):
+    dir_path = tmp_path / "tmp"
+    dir_path.mkdir()
+    arg_type = DirectoryArgType()
+
+    result = arg_type.parse(str(dir_path))
+
+    assert arg_type.get_name() == "directory"
+    assert isinstance(result, Path)
+
+
+def test_directory_arg_type_fails_when_no_directory(tmp_path: Path):
+    dir_path = tmp_path / "tmp"
+    arg_type = DirectoryArgType()
+
+    with pytest.raises(AssertionError):
+        arg_type.parse(str(dir_path))
+
+
+def test_path_arg_type(tmp_path: Path):
+    arg_type = PathArgType()
+
+    result = arg_type.parse(str(tmp_path))
+
+    assert arg_type.get_name() == "path"
+    assert isinstance(result, Path)
+
+
+def test_regex_arg_type():
+    arg_type = RegexArgType()
+
+    result = arg_type.parse(".*")
+
+    assert isinstance(result, Pattern)
