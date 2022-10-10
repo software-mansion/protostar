02e06ea1e4f90e98bed824434d25742ffd4e0f65
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-10-10 18:11:59 +0200

    move argument to a separate file

--- protostar/cli/command.py
+++ protostar/cli/command.py
@@ -1,53 +1,13 @@
-import re
 from abc import ABC, abstractmethod
-from dataclasses import dataclass, replace
-from pathlib import Path
-from typing import Any, List, Optional, Pattern
+from typing import Any, List, Optional
 
-from starkware.starknet.utils.api_utils import cast_to_felts
+from protostar.cli.arg_type import ArgTypeName
 
-from protostar.starknet_gateway import Fee
+from .argument import Argument
 
 
 class Command(ABC):
-
-    # pylint: disable=too-many-instance-attributes
-    @dataclass(frozen=True)
-    class Argument:
-        class Type:
-            @staticmethod
-            def regexp(arg: str) -> Pattern:
-                return re.compile(arg)
-
-            @staticmethod
-            def directory(arg: str) -> Path:
-                pth = Path(arg)
-                assert pth.is_dir(), f'"{str(pth)}" is not a valid directory path'
-                return pth
-
-            @staticmethod
-            def felt(arg: str) -> int:
-                [output] = cast_to_felts([arg])
-                return output
-
-            @staticmethod
-            def fee(arg: str) -> Fee:
-                if arg == "auto":
-                    return arg
-                return int(arg)
-
-        name: str
-        description: str
-        type: InputAllowedType
-        is_positional: bool = False
-        is_required: bool = False
-        is_array: bool = False
-        default: Any = None
-        example: Optional[str] = None
-        short_name: Optional[str] = None
-
-        def copy_with(self, **changes) -> "Command.Argument":
-            return replace(self, **changes)
+    Argument = Argument[ArgTypeName]
 
     @property
     @abstractmethod
