9dd9bdfe5110ecd08151e88c48225b532420eb05
Marek Kaput <marek.kaput@swmansion.com>
2022-10-18 15:46:20 +0200

    Implement a system for passing static information from Protostar to Scripting API modules
    cc #997
    
    commit-id:4d692c5b

--- tests/integration/scripting/script_runner_test.py
+++ tests/integration/scripting/script_runner_test.py
@@ -1,13 +1,25 @@
 import pytest
+from packaging.version import Version
 from pytest import CaptureFixture
+from pytest_mock import MockerFixture
 
 from protostar.scripting_runtime import ScriptRunner
+from protostar.self.protostar_directory import VersionManager
 from scripting.conftest import CreateScriptFixture
 
 
+@pytest.fixture(name="version_manager")
+def version_manager_fixture(mocker: MockerFixture) -> VersionManager:
+    version_manager = mocker.MagicMock()
+    version_manager.protostar_version = Version("1.2.3")
+    return version_manager
+
+
 @pytest.fixture(name="script_runner")
-def script_runner_fixture() -> ScriptRunner:
-    return ScriptRunner()
+def script_runner_fixture(version_manager: VersionManager) -> ScriptRunner:
+    return ScriptRunner(
+        version_manager=version_manager,
+    )
 
 
 def test_sys_info(
@@ -68,4 +80,27 @@ Traceback (most recent call last):
     lst[1] = 0
 IndexError: list assignment index out of range
 """,
+    )
+
+
+def test_protostar_scripting_version(
+    version_manager: VersionManager,
+    create_script: CreateScriptFixture,
+    script_runner: ScriptRunner,
+    capfd: CaptureFixture[str],
+):
+    # language=python
+    script = create_script(
+        """
+import protostar.scripting
+
+print(protostar.scripting.__version__)
+"""
+    )
+
+    script_runner.run(script)
+
+    assert capfd.readouterr() == (
+        f"{str(version_manager.protostar_version)}\n",
+        "",
     )
