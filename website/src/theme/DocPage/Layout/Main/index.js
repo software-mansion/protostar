8f890ef35c9e7f3df85c28e6b42a6d9c8e295979
Arkadiusz <arkadiusz.kasprzyk@swmansion.com>
2022-10-20 12:54:11 +0200

    Hot-fix stale comments (#1048)
    Fix stale comment box after navigating to a different page and change
    the mapping method to `pathname` (`title` mapping didn't work and `url`
    was too verbose).

--- website/src/theme/DocPage/Layout/Main/index.js
+++ website/src/theme/DocPage/Layout/Main/index.js
@@ -3,10 +3,12 @@ import Main from '@theme-original/DocPage/Layout/Main'
 import Giscus from '@giscus/react'
 import { useColorMode } from '@docusaurus/theme-common'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import { useLocation } from 'react-router-dom'
 
 export default function MainWrapper({ children, ...props }) {
   const { isDarkTheme } = useColorMode()
   const { siteConfig } = useDocusaurusContext()
+  const location = useLocation()
 
   return (
     <Main {...props}>
@@ -14,18 +16,19 @@ export default function MainWrapper({ children, ...props }) {
       <div className='row margin-top--md'>
         <div className='col'>
           <Giscus
+            key={location.pathname}
             repo='software-mansion/protostar'
             repoId='R_kgDOGw_HxA'
             categoryId='DIC_kwDOGw_HxM4CSDJD'
             category='Documentation'
-            mapping='url'
+            mapping='pathname'
             reactionsEnabled='0'
             emitMetadata='0'
             inputPosition='top'
             theme={isDarkTheme ? 'dark' : 'light'}
             lang={siteConfig.i18n.defaultLocale}
             loading='lazy'
-            strict={true}
+            strict
           />
         </div>
         <div className='col col--3' />
