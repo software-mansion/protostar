2ec0872144ca7f6e60404d20f633a62143afbe22
kasperski95 <arkadiuszkasprzyk95@gmail.com>
2022-10-20 12:31:56 +0200

    fix stale comments

--- website/src/theme/DocPage/Layout/Main/index.js
+++ website/src/theme/DocPage/Layout/Main/index.js
@@ -3,10 +3,12 @@ import Main from '@theme-original/DocPage/Layout/Main'
 import Giscus from '@giscus/react'
 import { useColorMode } from '@docusaurus/theme-common'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import { useLocation } from 'react-router-dom'
 
 export default function MainWrapper({ children, ...props }) {
   const { isDarkTheme } = useColorMode()
   const { siteConfig } = useDocusaurusContext()
+  const location = useLocation()
 
   return (
     <Main {...props}>
@@ -14,6 +16,7 @@ export default function MainWrapper({ children, ...props }) {
       <div className='row margin-top--md'>
         <div className='col'>
           <Giscus
+            key={location.pathname}
             repo='software-mansion/protostar'
             repoId='R_kgDOGw_HxA'
             categoryId='DIC_kwDOGw_HxM4CSDJD'
