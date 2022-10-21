434833e11caa5d4b4261edfe4218334259176f60
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-10-21 19:56:32 +0200

    update cli reference

--- protostar/cli/common_arguments.py
+++ protostar/cli/common_arguments.py
@@ -2,6 +2,7 @@ from .protostar_argument import ProtostarArgument
 
 lib_path_arg = ProtostarArgument(
     name="lib-path",
-    description="Directory containing project dependencies",
+    description="Directory containing project dependencies. "
+    "This argument is/will after migrating the configuration file.",
     type="path",
 )
