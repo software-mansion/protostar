62d769690b4ab5d4cb788ae5ed0130d4ca7564c5
Karol Bisztyga <karol.bisztyga@swmansion.com>
2022-11-03 16:13:08 +0100

    use region

--- protostar/starknet/cheatable_syscall_handler.py
+++ protostar/starknet/cheatable_syscall_handler.py
@@ -146,6 +146,7 @@ class CheatableSysCallHandler(BusinessLogicSysCallHandler):
         else:
             raise NotImplementedError(f"Unsupported call type {syscall_name}.")
 
+        # region Modified Starknet code.
         contract_call_data = (int(str(request.function_selector)), calldata)
         if self.cheatable_state.contract_calls.get(contract_address):
             self.cheatable_state.contract_calls[contract_address].append(
@@ -153,6 +154,7 @@ class CheatableSysCallHandler(BusinessLogicSysCallHandler):
             )
         else:
             self.cheatable_state.contract_calls[contract_address] = [contract_call_data]
+        # endregion
 
         call = self.execute_entry_point_cls(
             call_type=call_type,
