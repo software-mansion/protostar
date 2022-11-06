56cd568d9712a9de043bfd0a0c91567e4dac68ba
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-11-06 16:04:15 +0100

    fix type error

--- tests/integration/protostar_fixture.py
+++ tests/integration/protostar_fixture.py
@@ -25,7 +25,7 @@ from protostar.commands import (
     InvokeCommand,
     MigrateCommand,
 )
-from protostar.commands._generated_arg_types import InitCommandArgs
+from protostar.commands._generated_command_input_types import InitCommandInput
 from protostar.commands.deploy_account_command import DeployAccountCommand
 from protostar.commands.deploy_command import DeployCommand
 from protostar.commands.init.project_creator.new_project_creator import (
@@ -200,7 +200,7 @@ class ProtostarFixture:
         return await self._test_command.run(args)
 
     def init_sync(self, project_name: str):
-        result = asyncio.run(self._init_command.run(InitCommandArgs(name=project_name)))
+        result = asyncio.run(self._init_command.run(InitCommandInput(name=project_name)))
         return result
 
     async def build(self):
