e9c3b6c0318d82b4f44620b596aa912f77a801b6
kasperski95 <arkadiuszkasprzyk95@gmail.com>
2022-11-08 12:55:24 +0100

    _add test

--- protostar/starknet_gateway/block_explorer/voyager_block_explorer.py
+++ protostar/starknet_gateway/block_explorer/voyager_block_explorer.py
@@ -12,10 +12,16 @@ from .block_explorer import (
 class VoyagerBlockExplorer(BlockExplorer):
     def __init__(self, network: PredefinedNetwork) -> None:
         super().__init__()
-        self._network = network
+        network_to_domain: dict[PredefinedNetwork, URL] = {
+            "mainnet": "https://voyager.online",
+            "testnet": "https://goerli.voyager.online/",
+            "alpha-mainnet": "https://voyager.online",
+            "alpha-goerli": "https://goerli.voyager.online/",
+        }
+        self._domain = network_to_domain[network]
 
     def create_link_to_transaction(self, tx_hash: TransactionHash) -> URL:
-        ...
+        return f"{self._domain}/0x{tx_hash:064x}"
 
     def create_link_to_contract(self, contract_address: ContractAddress) -> URL:
         ...
--- protostar/starknet_gateway/block_explorer/voyager_block_explorer_test.py
+++ protostar/starknet_gateway/block_explorer/voyager_block_explorer_test.py
@@ -1,9 +1,16 @@
+import pytest
+
+from ..network_config import PredefinedNetwork
 from .voyager_block_explorer import VoyagerBlockExplorer
 
 
-def test_creating_link_to_transaction():
-    explorer = VoyagerBlockExplorer(network="mainnet")
+@pytest.mark.parametrize("network, domain", [("mainnet", "https://voyager.online")])
+def test_creating_link_to_transaction(network: PredefinedNetwork, domain: str):
+    explorer = VoyagerBlockExplorer(network=network)
 
     link = explorer.create_link_to_transaction(123)
 
-    assert link
+    assert (
+        link
+        == f"{domain}/tx/0x00000000000000000000000000000000000000000000000000000000000003e7"
+    )
