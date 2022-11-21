720928bbbce94fbb141708130b5d2af21b55caa2
kasperski95 <arkadiuszkasprzyk95@gmail.com>
2022-11-21 10:54:29 +0100

    add copy_with_description

--- protostar/cli/protostar_argument.py
+++ protostar/cli/protostar_argument.py
@@ -1,5 +1,12 @@
+from dataclasses import replace
+
+from typing_extensions import Self
+
 from protostar.argument_parser import Argument
 
 from .protostar_arg_type import ProtostarArgTypeName
 
-ProtostarArgument = Argument[ProtostarArgTypeName]
+
+class ProtostarArgument(Argument[ProtostarArgTypeName]):
+    def copy_with_description(self, description: str) -> Self:
+        return replace(self, description=description)
