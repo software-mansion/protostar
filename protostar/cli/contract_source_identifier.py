1d5dba312604a4392c01849f79196b0073e06fe3
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-11-21 18:19:19 +0100

    add contract source identifier factory

--- protostar/cli/contract_source_identifier.py
+++ protostar/cli/contract_source_identifier.py
@@ -1,10 +1,61 @@
 from dataclasses import dataclass
 from pathlib import Path
 
+from protostar.protostar_exception import ProtostarException
 from protostar.self import ContractName
+from protostar.configuration_file import ConfigurationFile
 
 
 @dataclass(eq=True, frozen=True, unsafe_hash=True)
 class ContractSourceIdentifier:
-    contract_name: ContractName
-    contract_paths: list[Path]
+    @classmethod
+    def from_contract_path(cls, path: Path):
+        check_contract_path_existence(path)
+
+        return cls(
+            name=path.stem,
+            paths=[path],
+        )
+
+    @classmethod
+    def create(cls, name: ContractName, paths: list[Path]):
+        for contract_path in paths:
+            check_contract_path_existence(contract_path)
+
+        return cls(
+            name=name,
+            paths=paths,
+        )
+
+    name: ContractName
+    paths: list[Path]
+
+
+def create_contract_source_identifier_factory(configuration_file: ConfigurationFile):
+    def create_contract_source_identifier(input_data: str):
+        if input_data.endswith(".cairo"):
+            return ContractSourceIdentifier.from_contract_path(Path(input_data))
+        contract_name = input_data
+        contract_names = configuration_file.get_contract_names()
+        if contract_name not in contract_names:
+            raise InvalidContractSourceIdentifierException(
+                f"Unknown contract: {contract_name}"
+            )
+
+        return ContractSourceIdentifier.create(
+            name=contract_name,
+            paths=configuration_file.get_contract_source_paths(contract_name),
+        )
+
+    return create_contract_source_identifier
+
+
+def check_contract_path_existence(contract_path: Path):
+    if not contract_path.exists():
+        raise InvalidContractSourceIdentifierException(
+            f"The following contract doesn't exist: {contract_path}"
+        )
+
+
+class InvalidContractSourceIdentifierException(ProtostarException):
+    pass
--- protostar/cli/contract_source_identifier_test.py
+++ protostar/cli/contract_source_identifier_test.py
@@ -1,20 +1,30 @@
 from pathlib import Path
 
+import pytest
+
 from protostar.configuration_file import FakeConfigurationFile
 
-from .contract_source_identifier import ContractSourceIdentifier
+from .contract_source_identifier import create_contract_source_identifier_factory
 
 
-def test_creating_contract_source_identifier_from_contract_name():
-    main_contract_source_path = Path("src/main.cairo")
-    create_contract_source = create_contract_source_identifier_factory(
+@pytest.fixture(name="fake_contract_path")
+def fake_contract_path_fixture(tmp_path: Path) -> Path:
+    contract_path = tmp_path / "main.cairo"
+    contract_path.touch()
+    return contract_path
+
+
+def test_creating_contract_source_identifier_from_contract_name(
+    fake_contract_path: Path,
+):
+    create_contract_source_identifier = create_contract_source_identifier_factory(
         configuration_file=FakeConfigurationFile(
-            contract_name_to_source_paths={"main": [main_contract_source_path]}
+            contract_name_to_source_paths={"main": [fake_contract_path]}
         )
     )
 
     contract_source_identifier = create_contract_source_identifier("main")
 
     assert contract_source_identifier.name == "main"
-    assert main_contract_source_path in contract_source_identifier.paths
+    assert fake_contract_path in contract_source_identifier.paths
     assert len(contract_source_identifier.paths) == 1
