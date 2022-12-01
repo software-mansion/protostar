6a3535a5b91ce560199452eeb18b61404d7e7fd8
kasperski95 <arkadiuszkasprzyk95@gmail.com>
2022-12-01 13:30:59 +0100

    _add file interpreter

--- protostar/starknet_gateway/multicall/multicall_file_interpreter.py
+++ protostar/starknet_gateway/multicall/multicall_file_interpreter.py
@@ -1,9 +1,43 @@
-import tomlkit as toml
+from dataclasses import dataclass
+from typing import Literal, cast
 
-from .multicall_structs import CallBase
+import tomlkit as toml
+from typing_extensions import TypedDict
+from tomlkit.items import AoT
+
+from .multicall_structs import CallBase, InvokeCall, DeployCall
+
+
+@dataclass
+class RawCall(TypedDict):
+    type: Literal["invoke", "deploy"]
+
+
+@dataclass
+class DeployRawCall(RawCall):
+    id: str
+
+
+@dataclass
+class InvokeRawCall(RawCall):
+    pass
 
 
 def interpret_multicall_file_content(toml_content: str) -> list[CallBase]:
-    doc = toml.loads(toml_content)
-    calls = doc.get("calls")
+    raw_calls = parse_toml_multicall(toml_content)
     return []
+
+
+def parse_toml_multicall(toml_content: str) -> list[RawCall]:
+    doc = toml.loads(toml_content)
+    call_aot = doc.get("call")
+    assert isinstance(call_aot, AoT)
+    return cast(list[RawCall], call_aot.value)
+
+
+def map_raw_call_to_call_base(raw_call: RawCall) -> CallBase:
+    if raw_call["type"] == "invoke":
+        return InvokeCall(address=Address())
+    if raw_call["type"] == "deploy":
+        return DeployCall()
+    assert False, "Unknown call type"
