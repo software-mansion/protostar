4636398153af7cad281363f862bdbafa1a8a5fc2
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-12-07 14:00:01 +0100

    fix

--- tests/integration/commands/test_command_test.py
+++ tests/integration/commands/test_command_test.py
@@ -1,14 +1,13 @@
 import pytest
 from pytest import CaptureFixture
 
+from tests.data.contracts import EMPTY_TEST
 from tests.integration.conftest import CreateProtostarProjectFixture, ProtostarFixture
-from tests.conftest import DevnetFixture
 
 
 @pytest.fixture(autouse=True, name="protostar")
 def protostar_fixture(create_protostar_project: CreateProtostarProjectFixture):
     with create_protostar_project() as protostar:
-        protostar.build_sync()
         yield protostar
 
 
@@ -18,4 +17,14 @@ async def test_gas_estimation(
 ):
     await protostar.test(targets=["::test_increase_balance"], estimate_gas=True)
 
-    assert "gas=4059" in capsys.readouterr().out
+    assert "gas=4356" in capsys.readouterr().out
+
+
+async def test_gas_estimation_for_identity_function(
+    protostar: ProtostarFixture,
+    capsys: CaptureFixture[str],
+):
+    protostar.create_files({"tests/test_main.cairo": EMPTY_TEST})
+    await protostar.test(targets=["::test_nothing"], estimate_gas=True)
+
+    assert "gas=4068" in capsys.readouterr().out
