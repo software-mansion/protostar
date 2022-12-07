5c9b31ec99ef431db60be402cc5ed34a2977d878
Igor Urbanik <igorurbanik23@gmail.com>
2022-12-07 13:32:22 +0100

    Added uint256 as input test

---
+++ tests/integration/gateway/uint256_as_input_test.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+
+import pytest
+
+from tests.conftest import DevnetAccount, SetPrivateKeyEnvVarFixture
+from tests.data.contracts import CONTRACT_WITH_UINT256_CONSTRUCTOR
+from tests.integration.conftest import CreateProtostarProjectFixture
+from tests.integration.protostar_fixture import ProtostarFixture
+
+from protostar.starknet.data_transformer import CairoOrPythonData
+
+
+@pytest.fixture(name="protostar", scope="module")
+def protostar_fixture(create_protostar_project: CreateProtostarProjectFixture):
+    with create_protostar_project() as protostar:
+        protostar.create_files({"./src/main.cairo": CONTRACT_WITH_UINT256_CONSTRUCTOR})
+        protostar.build_sync()
+        yield protostar
+
+
+@pytest.fixture(name="compiled_contract_filepath")
+def compiled_contract_filepath_fixture() -> Path:
+    return Path("./build/main.json")
+
+
+@pytest.mark.parametrize(
+    "constructor_input",
+    [[42, 24], {"amount": {"high": 24, "low": 42}}, {"amount": 24 << 128 + 24}],
+)
+async def test_uint256_as_input(
+    protostar: ProtostarFixture,
+    devnet_gateway_url: str,
+    compiled_contract_filepath: Path,
+    devnet_account: DevnetAccount,
+    set_private_key_env_var: SetPrivateKeyEnvVarFixture,
+    constructor_input: CairoOrPythonData,
+):
+    with set_private_key_env_var(devnet_account.private_key):
+        declare_response = await protostar.declare(
+            contract=compiled_contract_filepath,
+            gateway_url=devnet_gateway_url,
+            account_address=devnet_account.address,
+            max_fee="auto",
+        )
+        response = await protostar.deploy(
+            class_hash=declare_response.class_hash,
+            gateway_url=devnet_gateway_url,
+            account_address=devnet_account.address,
+            max_fee="auto",
+            inputs=constructor_input,
+        )
+    assert response.address is not None
