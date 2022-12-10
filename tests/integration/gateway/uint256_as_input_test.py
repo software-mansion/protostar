5042519ebdbc75a864289979e977d81559a41524
Igor Urbanik <igorurbanik23@gmail.com>
2022-12-10 15:05:16 +0100

    wip

--- tests/integration/gateway/uint256_as_input_test.py
+++ tests/integration/gateway/uint256_as_input_test.py
@@ -8,7 +8,7 @@ from tests.integration.conftest import CreateProtostarProjectFixture
 from tests.integration.protostar_fixture import ProtostarFixture
 
 from protostar.starknet.data_transformer import CairoOrPythonData
-
+from protostar.starknet_gateway import InputValidationException
 
 @pytest.fixture(name="protostar", scope="module")
 def protostar_fixture(create_protostar_project: CreateProtostarProjectFixture):
@@ -25,7 +25,7 @@ def compiled_contract_filepath_fixture() -> Path:
 
 @pytest.mark.parametrize(
     "constructor_input",
-    [[42, 24], {"amount": {"high": 24, "low": 42}}, {"amount": 24 << 128 + 24}],
+    [[42, 24], {"amount": {"high": 24, "low": 42}}, {"amount": (24 << 128) + 42}],
 )
 async def test_uint256_as_input(
     protostar: ProtostarFixture,
@@ -49,4 +49,41 @@ async def test_uint256_as_input(
             max_fee="auto",
             inputs=constructor_input,
         )
-    assert response.address is not None
+        assert response.address is not None
+
+        response = await protostar.call(
+            contract_address=response.address,
+            function_name="get_balance",
+            inputs=None,
+            gateway_url=devnet_gateway_url
+        )
+
+        assert response.response.res == (24 << 128) + 42
+
+
+
+
+
+async def test_uint256_as_input_fail(
+    protostar: ProtostarFixture,
+    devnet_gateway_url: str,
+    compiled_contract_filepath: Path,
+    devnet_account: DevnetAccount,
+    set_private_key_env_var: SetPrivateKeyEnvVarFixture,
+):
+    with set_private_key_env_var(devnet_account.private_key):
+        declare_response = await protostar.declare(
+            contract=compiled_contract_filepath,
+            gateway_url=devnet_gateway_url,
+            account_address=devnet_account.address,
+            max_fee="auto",
+        )
+
+        with pytest.raises(InputValidationException):
+            response = await protostar.deploy(
+                class_hash=declare_response.class_hash,
+                gateway_url=devnet_gateway_url,
+                account_address=devnet_account.address,
+                max_fee="auto",
+                inputs=[(24 << 128) + 42],
+            )
