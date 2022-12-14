a845d753fbb16d988ed646f6b33dd5ef18eef31a
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2022-12-14 15:53:30 +0100

    add AbiResolver

--- protostar/starknet_gateway/call/__init__.py
+++ protostar/starknet_gateway/call/__init__.py
@@ -1,1 +1,2 @@
 from .call_use_case import CallUseCase
+from .call_protocols import AbiResolverProtocol, DataTransformerProtocol, ClientProtocol
--- protostar/starknet_gateway/call/call_use_case.py
+++ protostar/starknet_gateway/call/call_use_case.py
@@ -1,35 +1,26 @@
-from protostar.starknet import Address
+from protostar.starknet import from_python_transformer
 
 from .call_structs import (
     CallInput,
     CallOutput,
     CallClientPayload,
-    Calldata,
     PythonCalldata,
     CairoCalldata,
 )
-from .call_protocols import (
-    CallClientProtocol,
-    DataTransformerProtocol,
-    ContractAddressToAbiConverterProtocol,
-)
+from .call_protocols import ClientProtocol, AbiResolverProtocol
 
 
 class CallUseCase:
     def __init__(
         self,
-        client: CallClientProtocol,
-        contract_address_to_abi_converter: ContractAddressToAbiConverterProtocol,
-        data_transformer: DataTransformerProtocol,
+        contract_address_to_abi_converter: AbiResolverProtocol,
+        client: ClientProtocol,
     ) -> None:
         self._client = client
         self._contract_address_to_abi_converter = contract_address_to_abi_converter
-        self._data_transformer = data_transformer
 
     async def execute(self, data: CallInput) -> CallOutput:
-        cairo_calldata = await self._transform_calldata_if_necessary(
-            address=data.address, calldata=data.inputs
-        )
+        cairo_calldata = await self._transform_calldata_if_necessary(data)
         payload = CallClientPayload(
             address=data.address,
             selector=data.selector,
@@ -38,12 +29,12 @@ class CallUseCase:
         response = await self._client.call(payload)
         return CallOutput(data=response.data)
 
-    async def _transform_calldata_if_necessary(
-        self, address: Address, calldata: Calldata
-    ) -> CairoCalldata:
+    async def _transform_calldata_if_necessary(self, data: CallInput) -> CairoCalldata:
+        calldata = data.inputs
         if isinstance(calldata, PythonCalldata):
-            abi = await self._contract_address_to_abi_converter.convert(address)
-            return await self._data_transformer.convert_to_cairo_calldata(
-                abi=abi, python_calldata=calldata
+            abi = await self._contract_address_to_abi_converter.resolve(data.address)
+            transform = from_python_transformer(
+                contract_abi=abi, fn_name=str(data.selector), mode="inputs"
             )
+            return transform(calldata)
         return calldata
