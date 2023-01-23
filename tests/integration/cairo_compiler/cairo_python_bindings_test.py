883c8935cb0b755cecf4524f0694b013d3db3475
Karol Bisztyga <karol.bisztyga@swmansion.com>
2023-01-23 10:34:44 +0100

    improve test, add cairo to casm test

--- tests/integration/cairo_compiler/cairo_python_bindings_test.py
+++ tests/integration/cairo_compiler/cairo_python_bindings_test.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import pytest
 
@@ -5,7 +6,7 @@ import cairo_python_bindings
 
 from tests.data.contracts import CAIRO_BINDINGS_CONTRACT
 from tests.integration.conftest import CreateProtostarProjectFixture
-from tests.integration.protostar_fixture import ProtostarFixture
+from tests.integration._conftest import ProtostarFixture
 
 
 @pytest.fixture(name="protostar")
@@ -15,43 +16,68 @@ def protostar_fixture(create_protostar_project: CreateProtostarProjectFixture):
         yield protostar
 
 
-def test_basic(protostar: ProtostarFixture):
+@pytest.fixture(name="prepared_files")
+def prepared_files_fixture(protostar: ProtostarFixture):
     cairo_path = Path("./src/example.cairo")
     sierra_path = Path("./src/example.sierra")
     casm_path = Path("./src/example.casm")
 
-    for path in [cairo_path, sierra_path, casm_path]:
+    paths = {"cairo": cairo_path, "sierra": sierra_path, "casm": casm_path}
+
+    for _, path in paths.items():
         protostar.create_files({str(path): ""})
         assert path.exists()
 
     with open(cairo_path, "w") as file:
         file.write(CAIRO_BINDINGS_CONTRACT)
 
+    return paths
+
+
+def test_cairo_to_casm(protostar: ProtostarFixture, prepared_files: dict[str, Path]):
+    cairo_python_bindings.call_cairo_to_casm_compiler(  # pyright: ignore
+        str(prepared_files["cairo"]), str(prepared_files["casm"])
+    )
+    assert prepared_files["casm"].exists() and prepared_files["casm"].stat().st_size
+    casm_contents = (
+        cairo_python_bindings.call_cairo_to_casm_compiler(  # pyright: ignore
+            str(prepared_files["cairo"])
+        )
+    )
+    assert len(casm_contents)
+    with open(prepared_files["casm"], "r") as file:
+        file_contents = file.readlines()
+        assert len(casm_contents.split("\n")) >= len(file_contents)
+
+
+def test_cairo_to_sierra_to_casm(
+    protostar: ProtostarFixture, prepared_files: dict[str, Path]
+):
     # cairo => sierra
     cairo_python_bindings.call_cairo_to_sierra_compiler(  # pyright: ignore
-        str(cairo_path), str(sierra_path)
+        str(prepared_files["cairo"]), str(prepared_files["sierra"])
     )
-    assert sierra_path.exists() and sierra_path.stat().st_size
+    assert prepared_files["sierra"].exists() and prepared_files["sierra"].stat().st_size
     sierra_contents = (
         cairo_python_bindings.call_cairo_to_sierra_compiler(  # pyright: ignore
-            str(cairo_path)
+            str(prepared_files["cairo"])
         )
     )
     assert len(sierra_contents)
-    with open(sierra_path, "r") as file:
+    with open(prepared_files["sierra"], "r") as file:
         file_contents = file.readlines()
         assert len(sierra_contents.split("\n")) >= len(file_contents)
     # sierra => casm
     cairo_python_bindings.call_sierra_to_casm_compiler(  # pyright: ignore
-        str(sierra_path), str(casm_path)
+        str(prepared_files["sierra"]), str(prepared_files["casm"])
     )
-    assert casm_path.exists() and casm_path.stat().st_size
+    assert prepared_files["casm"].exists() and prepared_files["casm"].stat().st_size
     casm_contents = (
         cairo_python_bindings.call_sierra_to_casm_compiler(  # pyright: ignore
-            str(sierra_path)
+            str(prepared_files["sierra"])
         )
     )
     assert len(casm_contents)
-    with open(casm_path, "r") as file:
+    with open(prepared_files["casm"], "r") as file:
         file_contents = file.readlines()
         assert len(casm_contents.split("\n")) >= len(file_contents)
