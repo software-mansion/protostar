8e1816d3f07520a099bb3db2f872e549448c2c18
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2023-01-24 13:05:13 +0100

    refactor assertion

--- tests/integration/_conftest/cairo_test_results_assertion.py
+++ tests/integration/_conftest/cairo_test_results_assertion.py
@@ -11,13 +11,30 @@ def assert_cairo_test_cases(
     expected_passed_test_cases_names: Optional[list[str]] = None,
     expected_failed_test_cases_names: Optional[list[str]] = None,
     expected_broken_test_cases_names: Optional[list[str]] = None,
-    expected_skipped_test_cases_names: Optional[list[str]] = None,  # Explicitly skipped
+    expected_skipped_test_cases_names: Optional[list[str]] = None,
 ):
-    expected_passed_test_cases_names = expected_passed_test_cases_names or []
-    expected_failed_test_cases_names = expected_failed_test_cases_names or []
-    expected_broken_test_cases_names = expected_broken_test_cases_names or []
-    expected_skipped_test_cases_names = expected_skipped_test_cases_names or []
+    actual_test_results_data = convert_testing_summary_to_test_results_data(
+        testing_summary
+    )
+    expected_test_results_data = CairoTestResultsData(
+        passed=set(expected_passed_test_cases_names or []),
+        failed=set(expected_failed_test_cases_names or []),
+        broken=set(expected_broken_test_cases_names or []),
+        skipped=set(expected_skipped_test_cases_names or []),
+    )
+    diff = CairoTestCasesDiffGenerator.from_testing_summary(testing_summary).execute(
+        actual_test_results_data=actual_test_results_data,
+        expected_test_results_data=expected_test_results_data,
+    )
+    if diff:
+        # diff is not included in the condition to improve errors' readability
+        assert False, diff
+    assert actual_test_results_data == expected_test_results_data  # safety net
 
+
+def convert_testing_summary_to_test_results_data(
+    testing_summary: TestingSummary,
+) -> CairoTestResultsData:
     passed_test_cases_names = set(
         passed_test_case.test_case_name for passed_test_case in testing_summary.passed
     )
@@ -31,29 +48,12 @@ def assert_cairo_test_cases(
         skipped_test_case.test_case_name
         for skipped_test_case in testing_summary.explicitly_skipped
     )
-
     for broken_test_case in testing_summary.broken_suites:
         for test_case_name in broken_test_case.test_case_names:
             broken_test_cases_names.add(test_case_name)
-
-    actual = CairoTestResultsData(
+    return CairoTestResultsData(
         passed=passed_test_cases_names,
         failed=failed_test_cases_names,
         broken=broken_test_cases_names,
         skipped=skipped_test_cases_names,
     )
-
-    expected = CairoTestResultsData(
-        passed=set(expected_passed_test_cases_names),
-        failed=set(expected_failed_test_cases_names),
-        broken=set(expected_broken_test_cases_names),
-        skipped=set(expected_skipped_test_cases_names),
-    )
-
-    diff = CairoTestCasesDiffGenerator.from_testing_summary(testing_summary).execute(
-        actual, expected
-    )
-    if diff:
-        # diff is not included in the condition to improve errors' readability
-        assert False, diff
-    assert actual == expected
