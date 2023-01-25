ab2ff54d0f0d206c2d64294dfc3cc05cad04e9ce
Arcticae <tomekgsd@gmail.com>
2023-01-25 20:04:36 +0100

    Remove redundant contract storage class

--- protostar/cheatable_starknet/cheaters/storage.py
+++ protostar/cheatable_starknet/cheaters/storage.py
@@ -1,40 +1,9 @@
-from typing import List, Optional, TYPE_CHECKING, Set
-
-from starkware.starknet.business_logic.state.state_api import State
+from typing import List, Optional, TYPE_CHECKING
 
 from protostar.starknet import calc_address, BreakingReportedException
 
 if TYPE_CHECKING:
     from protostar.cheatable_starknet.cheatable_cached_state import CheatableCachedState
-
-
-class AsyncContractStorageState:
-    """
-    Defines the API for accessing StarkNet single contract storage state.
-    """
-
-    def __init__(self, state: State, contract_address: int):
-        self.state = state
-        self.contract_address = contract_address
-
-        # Maintain all read request values in chronological order.
-        self.read_values: List[int] = []
-        self.accessed_keys: Set[int] = set()
-
-    async def read(self, address: int) -> int:
-        self.accessed_keys.add(address)
-        value = await self.state.get_storage_at(
-            contract_address=self.contract_address, key=address
-        )
-        self.read_values.append(value)
-
-        return value
-
-    async def write(self, address: int, value: int):
-        self.accessed_keys.add(address)
-        await self.state.set_storage_at(
-            contract_address=self.contract_address, key=address, value=value
-        )
 
 
 class StorageCairoCheater:
@@ -48,16 +17,13 @@ class StorageCairoCheater:
         value: List[int],
         key: Optional[List[int]] = None,
     ):
-        key = key or []
         variable_address = calc_address(variable_name, key)
-        contract_storage = AsyncContractStorageState(
-            state=self.cheatable_state,
-            contract_address=target_contract_address,
-        )
-
         for i, val in enumerate(value):
-            await contract_storage.read(address=variable_address + i)
-            await contract_storage.write(address=variable_address + i, value=val)
+            await self.cheatable_state.set_storage_at(
+                contract_address=target_contract_address,
+                key=variable_address + i,
+                value=val,
+            )
 
     async def load(
         self,
@@ -70,13 +36,10 @@ class StorageCairoCheater:
         variable_address = calc_address(variable_name, key)
         variable_size = self._get_variable_size(target_contract_address, variable_type)
 
-        starknet_storage = AsyncContractStorageState(
-            state=self.cheatable_state,
-            contract_address=target_contract_address,
-        )
-
         return [
-            await starknet_storage.read(address=variable_address + i)
+            await self.cheatable_state.get_storage_at(
+                contract_address=target_contract_address, key=variable_address + i
+            )
             for i in range(variable_size)
         ]
 
