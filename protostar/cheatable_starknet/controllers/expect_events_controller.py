4b29d7257b3235d3c6dc1ef9943d35230208f526
Arkadiusz Kasprzyk <arkadiuszkasprzyk95@gmail.com>
2023-01-31 13:07:41 +0100

    remove functions

--- protostar/cheatable_starknet/controllers/expect_events_controller.py
+++ protostar/cheatable_starknet/controllers/expect_events_controller.py
@@ -138,23 +138,26 @@ class ExpectEventsMismatchReportedException(ReportedException):
         lines: list[str] = []
         for event_matching in self.event_matching_result.event_matchings:
             if isinstance(event_matching, AcceptedEventMatching):
-                lines.append(self._format_accepted_event_matching(event_matching))
+                lines.append(
+                    self._format_event(
+                        event=event_matching.emitted_event, prefix="[pass]"
+                    )
+                )
             elif isinstance(event_matching, SkippedEventMatching):
-                lines.append(self._format_skipped_event_matching(event_matching))
+                lines.append(
+                    self._format_event(
+                        event=event_matching.emitted_event, prefix="[skip]"
+                    )
+                )
             elif isinstance(event_matching, FailedEventMatching):
-                lines.append(self._format_failed_event_matching(event_matching))
+                lines.append(
+                    self._format_event(
+                        event=event_matching.expected_event, prefix="[fail]"
+                    )
+                )
             else:
                 assert False, f"Unexpected event_matching: {event_matching}"
         return lines
-
-    def _format_accepted_event_matching(self, event_matching: AcceptedEventMatching):
-        return self._format_event(event=event_matching.emitted_event, prefix="[pass]")
-
-    def _format_failed_event_matching(self, event_matching: FailedEventMatching):
-        return self._format_event(event=event_matching.expected_event, prefix="[fail]")
-
-    def _format_skipped_event_matching(self, event_matching: SkippedEventMatching):
-        return self._format_event(event=event_matching.emitted_event, prefix="[skip]")
 
     def _format_event(self, event: Event, prefix: str):
         segments: list[str] = []
