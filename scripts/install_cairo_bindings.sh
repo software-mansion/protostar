48aa4c0cf994e5c7ee9a39356195c7b7b9291140
Karol Bisztyga <karol.bisztyga@swmansion.com>
2023-02-03 15:37:32 +0100

    fix ci

--- scripts/install_cairo_bindings.sh
+++ scripts/install_cairo_bindings.sh
@@ -12,20 +12,12 @@ if [ "$1" == "--cleanup" ]; then
   fi
 fi
 
-# local
-pushd ../cairo-fork
-pushd crates/cairo-lang-python-bindings
-rustup override set nightly
-maturin develop --release
-
-exit 0;
-
 function install() {
   pushd "${1}"
   git clone https://github.com/software-mansion-labs/cairo.git
   pushd cairo
   # currrent master works ok, in case it doesn't, uncomment the line below
-  # git checkout 5608ce7e052df79da11485689cb5f1459d3e5d18 # working commit
+  git checkout 6db822a167b0109c773ed8ee75118f975b58bec3 # working commit
   pushd crates/cairo-lang-python-bindings
   rustup override set nightly
   maturin develop --release || return 1;
