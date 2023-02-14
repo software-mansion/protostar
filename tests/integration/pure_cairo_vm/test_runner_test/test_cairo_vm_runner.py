2fd3cf59d33569dd02ee337ce1ad1fc3e7515eca
Arcticae <tomekgsd@gmail.com>
2023-02-14 13:05:06 +0100

    Move & rename the test

--- tests/integration/pure_cairo_vm/test_runner_test/test_cairo_vm_runner.py
+++ tests/integration/pure_cairo_vm/test_runner_test/test_cairo_vm_runner.py
@@ -107,24 +107,3 @@ async def test_setup_case_with_satellite_contract(
         testing_summary,
         expected_passed_test_cases_names=["test_a", "test_b_doesnt_leak_from_a_setup"],
     )
-
-
-async def test_syscall_deployment_abi_storing(
-    protostar: ProtostarFixture,
-):
-    protostar.create_contracts(
-        {
-            "block_number_contract": CONTRACTS_PATH / "block_number_contract.cairo",
-            "deployer": CONTRACTS_PATH / "deployer.cairo",
-        }
-    )
-
-    testing_summary = await protostar.run_test_runner(
-        Path(__file__).parent / "cheatable_syscall_handler_test.cairo",
-        cairo_test_runner=True,
-    )
-
-    assert_cairo_test_cases(
-        testing_summary,
-        expected_passed_test_cases_names=["test_syscall_deployment_abi_storing"],
-    )
