af333802ba380532472b73b118efd012df1ad0c4
Karol Bisztyga <karol.bisztyga@swmansion.com>
2023-02-14 08:25:07 +0100

    remove specific commit from the install script

--- scripts/install_cairo_bindings.sh
+++ scripts/install_cairo_bindings.sh
@@ -17,7 +17,7 @@ function install() {
   git clone https://github.com/software-mansion-labs/cairo.git
   pushd cairo
   # currrent master works ok, in case it doesn't, uncomment the line below
-  git checkout 6db822a167b0109c773ed8ee75118f975b58bec3 # working commit
+  # git checkout 6db822a167b0109c773ed8ee75118f975b58bec3 # working commit
   pushd crates/cairo-lang-python-bindings
   rustup override set nightly
   maturin develop --release || return 1;
