a2d8854cdd558bdb2320db8f60cfaf5d887226c3
Maksymilian Demitraszek <maksymilian.demitraszek@protonmail.com>
2023-02-16 14:06:56 +0100

    Updated bindings

--- protostar/cairo/cairo_bindings.py
+++ protostar/cairo/cairo_bindings.py
@@ -68,9 +68,9 @@ def call_test_collector(
     return TestCollectorOutput(sierra_output=output[0], test_names=output[1])
 
 
-def call_protostar_sierra_to_casm(
+def call_protostar_sierra_to_casm_from_path(
     named_tests: list[str], input_path: Path, output_path: Optional[Path] = None
 ) -> Optional[str]:
-    return cairo_python_bindings.call_protostar_sierra_to_casm(  # pyright: ignore
+    return cairo_python_bindings.call_protostar_sierra_to_casm_from_path(  # pyright: ignore
         named_tests, str(input_path), str(output_path) if output_path else None
     )
