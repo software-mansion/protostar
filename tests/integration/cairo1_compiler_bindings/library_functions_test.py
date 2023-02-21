c2fd5e8cef08dfd0b6dd590320487fd019e31d2b
Karol Bisztyga <karol.bisztyga@swmansion.com>
2023-02-21 09:52:59 +0100

    split into sep tests

--- tests/integration/cairo1_compiler_bindings/library_functions_test.py
+++ tests/integration/cairo1_compiler_bindings/library_functions_test.py
@@ -21,40 +21,51 @@ def get_mock_for_lib_func(mocker: MockerFixture, lib_func_name: str, err_code: i
     return cheat_mock
 
 
-def test_library_functions(mocker: MockerFixture, datadir: Path):
-    for lib_func_name in ["declare", "roll", "start_prank"]:
-        test_collector_output = cairo1.collect_tests(
-            input_path=datadir / f"{ lib_func_name }_test.cairo"
+def check_library_function(
+    mocker: MockerFixture, lib_func_name: str, cairo_test_path: Path
+):
+    test_collector_output = cairo1.collect_tests(input_path=cairo_test_path)
+    assert test_collector_output.sierra_output
+    protostar_casm_json = cairo1.compile_protostar_sierra_to_casm(
+        named_tests=test_collector_output.test_names,
+        input_data=test_collector_output.sierra_output,
+    )
+    assert protostar_casm_json
+    for mocked_error_code in [0, 1, 50]:
+        test_suite = parse_test_suite(
+            path=Path("test_source.cairo"), json_raw=protostar_casm_json
         )
-        assert test_collector_output.sierra_output
-        protostar_casm_json = cairo1.compile_protostar_sierra_to_casm(
-            named_tests=test_collector_output.test_names,
-            input_data=test_collector_output.sierra_output,
-        )
-        assert protostar_casm_json
-        for mocked_error_code in [0, 1, 50]:
-            test_suite = parse_test_suite(
-                path=Path("test_source.cairo"), json_raw=protostar_casm_json
-            )
 
-            for case in test_suite.test_cases:
-                runner = CairoFunctionRunner(program=test_suite.program, layout="all")
-                runner.run_from_entrypoint(
-                    case.offset,
-                    *[],
-                    hint_locals={
-                        lib_func_name: get_mock_for_lib_func(
-                            mocker, lib_func_name, mocked_error_code
-                        ),
-                    },
-                    static_locals={
-                        "__find_element_max_size": 2**20,
-                        "__squash_dict_max_size": 2**20,
-                        "__keccak_max_size": 2**20,
-                        "__usort_max_size": 2**20,
-                        "__chained_ec_op_max_len": 1000,
-                    },
-                    run_resources=RunResources(n_steps=100000000000000000),
-                    verify_secure=False,
-                )
-                assert runner.get_return_values(3)[0] == int(bool(mocked_error_code))
+        for case in test_suite.test_cases:
+            runner = CairoFunctionRunner(program=test_suite.program, layout="all")
+            runner.run_from_entrypoint(
+                case.offset,
+                *[],
+                hint_locals={
+                    lib_func_name: get_mock_for_lib_func(
+                        mocker, lib_func_name, mocked_error_code
+                    ),
+                },
+                static_locals={
+                    "__find_element_max_size": 2**20,
+                    "__squash_dict_max_size": 2**20,
+                    "__keccak_max_size": 2**20,
+                    "__usort_max_size": 2**20,
+                    "__chained_ec_op_max_len": 1000,
+                },
+                run_resources=RunResources(n_steps=100000000000000000),
+                verify_secure=False,
+            )
+            assert runner.get_return_values(3)[0] == int(bool(mocked_error_code))
+
+
+def test_roll(mocker: MockerFixture, datadir: Path):
+    check_library_function(mocker, "roll", datadir / "roll_test.cairo")
+
+
+def test_declare(mocker: MockerFixture, datadir: Path):
+    check_library_function(mocker, "declare", datadir / "declare_test.cairo")
+
+
+def test_start_prank(mocker: MockerFixture, datadir: Path):
+    check_library_function(mocker, "start_prank", datadir / "start_prank_test.cairo")
