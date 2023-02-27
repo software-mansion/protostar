a129018ca163ac5fd55e2ceb69dcc411c565d42b
Arcticae <tomekgsd@gmail.com>
2023-02-27 18:49:11 +0100

    Add empty case + combined case with 2 suites

--- tests/integration/pure_cairo_vm/cairo1_integration/cairo1_runner_test.py
+++ tests/integration/pure_cairo_vm/cairo1_integration/cairo1_runner_test.py
@@ -59,3 +59,45 @@ async def test_cairo_1_runner_with_external_lib(
             "test_cairo1_ext_lib::test_cairo1_ext_lib::passing_test_using_foo",
         ],
     )
+
+
+async def test_cairo_1_runner_empty_suite(
+    protostar: ProtostarFixture, shared_datadir: Path, datadir: Path
+):
+    protostar.create_files(
+        {
+            "cairo_project.toml": shared_datadir / "cairo_project.toml",
+            "lib.cairo": shared_datadir / "lib.cairo",
+        }
+    )
+
+    testing_summary = await protostar.run_test_runner(
+        datadir / "test_cairo1_empty_suite.cairo",
+        cairo1_test_runner=True,
+    )
+    assert testing_summary.test_collector_result.total_test_suites_count == 0
+
+
+async def test_cairo_1_runner_multiple_suites(
+    protostar: ProtostarFixture, shared_datadir: Path, datadir: Path
+):
+    protostar.create_files(
+        {
+            "cairo_project.toml": shared_datadir / "cairo_project.toml",
+            "lib.cairo": shared_datadir / "lib.cairo",
+        }
+    )
+
+    testing_summary = await protostar.run_test_runner(
+        datadir,
+        cairo1_test_runner=True,
+        cairo_path=[datadir / "external_lib_foo"],
+    )
+
+    assert_cairo_test_cases(
+        testing_summary,
+        expected_passed_test_cases_names=[
+            "test_cairo1::test_cairo1::passing_test",
+            "test_cairo1_ext_lib::test_cairo1_ext_lib::passing_test_using_foo",
+        ],
+    )
