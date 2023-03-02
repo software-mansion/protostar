bcda4553ea5633ce365a3a12468192ef59177d92
Tomasz Rejowski <34059895+Arcticae@users.noreply.github.com>
2023-03-02 16:09:51 +0100

    surroud variable with parentheses
    Co-authored-by: karol-bisztyga <59833830+karol-bisztyga@users.noreply.github.com>

--- scripts/bump_cairo_bindings.sh
+++ scripts/bump_cairo_bindings.sh
@@ -8,7 +8,7 @@ fi
 
 tracking_branch="$(git for-each-ref --format='%(upstream:short)' "$(git symbolic-ref -q HEAD)")"
 
-if [ $tracking_branch == "origin/master" ] ; then
+if [ "$tracking_branch" == "origin/master" ] ; then
   >&2 echo "You are currently tracking master branch. Checkout to another branch before bumping submodule version"
   exit 1
 fi
