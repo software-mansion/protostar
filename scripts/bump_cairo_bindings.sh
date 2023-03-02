781cab22016f38b255d12dd19a813a9607dd4d30
Arcticae <tomekgsd@gmail.com>
2023-03-02 15:29:24 +0100

    Add some checks for bumping procedure + add info to README.md

--- scripts/bump_cairo_bindings.sh
+++ scripts/bump_cairo_bindings.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
+if [ -n "$(git status --porcelain)" ]; then
+  >&2 echo "Working tree has uncommited changes, please stash/commit them before bumping submodule version"
+  exit 1
+fi
+
+tracking_branch = "$(git for-each-ref --format='%(upstream:short)' "$(git symbolic-ref -q HEAD)")"
+
+if [ tracking_branch == "origin/master" ] ; then
+  >&2 echo "You are currently tracking master branch. Checkout to another branch before bumping submodule version"
+  exit 1
+fi
+
+
 git submodule update --recursive --remote --init cairo
 git add ./cairo
 git commit -m "Bump cairo bindings" --no-verify
