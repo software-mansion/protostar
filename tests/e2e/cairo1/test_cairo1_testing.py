bd6e1be10120a800172541444487cb1181ee6915
Arcticae <tomekgsd@gmail.com>
2023-03-06 17:27:14 +0100

    Implement some e2e tests for cairo1 test collecting and ignoring

--- tests/e2e/cairo1/test_cairo1_testing.py
+++ tests/e2e/cairo1/test_cairo1_testing.py
@@ -16,3 +16,44 @@ def test_no_tests_found(protostar: ProtostarFixture):
     result = protostar(["test-cairo1", "tests"])
 
     assert "No test cases found" in result
+
+
+def test_targeted_collecting(protostar: ProtostarFixture, copy_fixture: CopyFixture):
+    copy_fixture("cairo1_project", "./cairo1_project")
+    os.chdir("./cairo1_project")
+
+    result = protostar(["test-cairo1", "::test_B"])
+
+    assert "Collected 1 suite, and 1 test case" in result
+    assert "test_B" in result
+
+
+def test_glob_collecting(protostar: ProtostarFixture, copy_fixture: CopyFixture):
+    copy_fixture("cairo1_project", "./cairo1_project")
+    os.chdir("./cairo1_project")
+
+    result = protostar(["test-cairo1", "./tests/**/*nested*::nested*"])
+
+    assert "Collected 1 suite, and 2 test cases" in result
+    assert "nested_1" in result
+    assert "nested_2" in result
+
+
+def test_ignoring_dir(protostar: ProtostarFixture, copy_fixture: CopyFixture):
+    copy_fixture("cairo1_project", "./cairo1_project")
+    os.chdir("./cairo1_project")
+
+    result = protostar(["test-cairo1", "./tests", "--ignore", "**/nested"])
+
+    assert "nested_1" not in result
+    assert "nested_2" not in result
+
+
+def test_ignoring_cases(protostar: ProtostarFixture, copy_fixture: CopyFixture):
+    copy_fixture("cairo1_project", "./cairo1_project")
+    os.chdir("./cairo1_project")
+
+    result = protostar(["test-cairo1", "./tests", "--ignore", "**/*::*nested*"])
+
+    assert "nested_1" not in result
+    assert "nested_2" not in result
---
+++ tests/e2e/fixtures/cairo1_project/tests/nested/test_nested.cairo
@@ -0,0 +1,9 @@
+#[test]
+fn nested_1() {
+    let x = 1;
+}
+
+#[test]
+fn nested_2() {
+    let x = 1;
+}
