d7d84c72124f9ad60ab6adb97fda64ccc77c8c75
Piotr Magiera <piotrmagiera150@gmail.com>
2023-03-06 13:13:36 +0100

    added integration tests for both init commands

--- tests/integration/commands/init_command_test.py
+++ tests/integration/commands/init_command_test.py
@@ -1,3 +1,43 @@
-# import pytest
+from pathlib import Path
+from filecmp import dircmp, cmpfiles
 
-# @pytest.mark.parametrize("create_protostar_project", [CairoVersion.cairo1], indirect=True)
+import pytest
+
+from protostar.cairo import CairoVersion
+from tests.integration.conftest import CreateProtostarProjectFixture
+from tests.conftest import PROJECT_ROOT
+
+
+@pytest.mark.parametrize(
+    "create_protostar_project, cairo_version",
+    [
+        (CairoVersion.cairo0, CairoVersion.cairo0),
+        (CairoVersion.cairo1, CairoVersion.cairo1),
+    ],
+    indirect=["create_protostar_project"],
+)
+def test_init(
+    create_protostar_project: CreateProtostarProjectFixture, cairo_version: CairoVersion
+):
+    def compare_directories(dcmp: dircmp):
+        if dcmp.left_only or dcmp.right_only or dcmp.diff_files:
+            return False
+
+        _, diff, errors = cmpfiles(
+            dcmp.left, dcmp.right, dcmp.same_files, shallow=False
+        )
+        if diff or errors:
+            return False
+
+        for sub_dcmp in dcmp.subdirs.values():
+            if not compare_directories(sub_dcmp):
+                return False
+
+        return True
+
+    with create_protostar_project():
+        mocker_project_path = Path(".")
+        template_project_path = PROJECT_ROOT.joinpath("templates", cairo_version.value)
+
+        dir_compare_object = dircmp(mocker_project_path, template_project_path)
+        assert compare_directories(dir_compare_object)
