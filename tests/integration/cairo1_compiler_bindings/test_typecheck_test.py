71c345971733152b10b97f4159a06bac7226fbcb
Karol Bisztyga <karol.bisztyga@swmansion.com>
2023-03-07 08:48:14 +0100

    validate tests when collecting

--- tests/integration/cairo1_compiler_bindings/test_typecheck_test.py
+++ tests/integration/cairo1_compiler_bindings/test_typecheck_test.py
@@ -9,43 +9,30 @@ def test_typecheck_basic(datadir: Path):
     test_collector_output = cairo1.collect_tests(datadir / "test_basic.cairo")
     assert test_collector_output.sierra_output
     assert test_collector_output.test_names
-    cairo1.typecheck_sierra_tests(
-        test_collector_output.test_names, test_collector_output.sierra_output
-    )
 
 
 def test_typecheck_with_args(datadir: Path):
-    test_collector_output = cairo1.collect_tests(datadir / "test_with_args.cairo")
-    assert test_collector_output.sierra_output
-    assert test_collector_output.test_names
-
     with pytest.raises(RuntimeError, match=r"Invalid number of parameters for test"):
-        cairo1.typecheck_sierra_tests(
-            test_collector_output.test_names, test_collector_output.sierra_output
-        )
+        test_collector_output = cairo1.collect_tests(datadir / "test_with_args.cairo")
+        assert test_collector_output.sierra_output
+        assert test_collector_output.test_names
 
 
 def test_typecheck_with_return_values(datadir: Path):
-    test_collector_output = cairo1.collect_tests(datadir / "test_with_ret_vals.cairo")
-    assert test_collector_output.sierra_output
-    assert test_collector_output.test_names
-
     with pytest.raises(
         RuntimeError,
         match=r"returns a value, it is required that test functions do not return values",
     ):
-        cairo1.typecheck_sierra_tests(
-            test_collector_output.test_names, test_collector_output.sierra_output
+        test_collector_output = cairo1.collect_tests(
+            datadir / "test_with_ret_vals.cairo"
         )
+        assert test_collector_output.sierra_output
+        assert test_collector_output.test_names
 
 
 def test_typecheck_with_no_panic(datadir: Path):
+    # TODO this should raise an error!!
+    # with pytest.raises(RuntimeError, match=r"..."):
     test_collector_output = cairo1.collect_tests(datadir / "test_with_no_panic.cairo")
     assert test_collector_output.sierra_output
     assert test_collector_output.test_names
-
-    # TODO this should raise an error!!
-    # with pytest.raises(RuntimeError, match=r"..."):
-    cairo1.typecheck_sierra_tests(
-        test_collector_output.test_names, test_collector_output.sierra_output
-    )
