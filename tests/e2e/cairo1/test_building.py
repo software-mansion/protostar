63c53ee9ca551a0ba1c22a378612e81e04e1fd2f
Piotr Magiera <piotrmagiera150@gmail.com>
2023-03-08 13:40:05 +0100

    fixed test_default_build and renamed to test_cairo1_build

--- tests/e2e/cairo1/test_building.py
+++ tests/e2e/cairo1/test_building.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 
-import pytest
+import os
 
-from tests.e2e.conftest import ProtostarFixture
+from tests.e2e.conftest import ProtostarFixture, CopyFixture
 
 
-@pytest.mark.usefixtures("init_cairo1")
-def test_default_build(protostar: ProtostarFixture):
+def test_cairo1_build(protostar: ProtostarFixture, copy_fixture: CopyFixture):
+    copy_fixture("cairo1_build", "./cairo1_build")
+    os.chdir("./cairo1_build")
     protostar(["build-cairo1"])
     compiled_path = Path("build/main.json")
     assert compiled_path.exists()
