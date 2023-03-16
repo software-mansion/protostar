30ef953ea6af45ee55a4e1e69ce0ef1422164557
Artur Michałek <52135326+cptartur@users.noreply.github.com>
2023-03-16 14:45:00 +0100

    Add `stop_prank` hint local (#1611)
    Closes #1484 
    
    Related to https://github.com/software-mansion-labs/cairo/pull/35
    
    ---------
    
    Co-authored-by: karol-bisztyga <59833830+karol-bisztyga@users.noreply.github.com>

---
+++ protostar/cheatable_starknet/callable_hint_locals/stop_prank_hint_local.py
@@ -0,0 +1,23 @@
+from typing import Callable
+
+from protostar.cheatable_starknet.callable_hint_locals.callable_hint_local import (
+    CallableHintLocal,
+)
+from protostar.cheatable_starknet.controllers import ContractsController
+from protostar.starknet import Address
+
+
+class StopPrankHintLocal(CallableHintLocal):
+    def __init__(self, contracts_controller: ContractsController):
+        self._contracts_controller = contracts_controller
+
+    @property
+    def name(self) -> str:
+        return "stop_prank"
+
+    def _build(self) -> Callable:
+        return self.stop_prank
+
+    def stop_prank(self, target_address: int):
+        address = Address.from_user_input(target_address)
+        self._contracts_controller.cancel_prank(target_address=address)
