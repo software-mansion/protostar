3a1dfbded87a37bea2fdd53a83c7311deecc2df5
Arcticae <tomekgsd@gmail.com>
2023-03-24 16:10:09 +0100

    Revert "Improved error messages"
    This reverts commit 032f32bf1fe9d481cde40849e33dbe973d679716.

--- protostar/cheatable_starknet/callable_hint_locals/declare_cairo0_hint_local.py
+++ protostar/cheatable_starknet/callable_hint_locals/declare_cairo0_hint_local.py
@@ -7,11 +7,9 @@ from protostar.cheatable_starknet.controllers.contracts import (
 )
 from protostar.compiler import ProjectCompiler
 from protostar.cairo.short_string import CairoShortString, short_string_to_str
-from protostar.configuration_file.configuration_file import ContractNameNotFoundException
-from protostar.starknet.cheatable_starknet_exceptions import CheatcodeException
+
 from .callable_hint_local import CallableHintLocal
 
-from starkware.cairo.lang.compiler.parser_transformer import ParserError
 
 class DeclareCheatcodeProtocol(Protocol):
     def __call__(
@@ -40,17 +38,11 @@ class DeclareCairo0HintLocal(CallableHintLocal):
 
     def declare_cairo0(self, contract: CairoShortString) -> DeclaredContract:
         contract_str = short_string_to_str(contract)
-        try:
-            compiled_contract = (
-                self._project_compiler.compile_contract_from_contract_identifier(
-                    contract_str
-                )
+        compiled_contract = (
+            self._project_compiler.compile_contract_from_contract_identifier(
+                contract_str
             )
-        except ContractNameNotFoundException as err:
-            raise CheatcodeException(self, err.message)
-        except ParserError as err:
-            raise CheatcodeException(self, err.message)
-
+        )
         declared_class = asyncio.run(
             self._contracts_controller.declare_cairo0_contract(compiled_contract)
         )
