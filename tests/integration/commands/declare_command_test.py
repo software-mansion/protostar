b8fbf1889610f43c91e66555b1aa5db4294b82cf
Artur Michalek <artur.michalek@swmansion.com>
2023-03-28 09:50:50 +0200

    Add DeclareCairo1Command integration test

--- tests/integration/commands/declare_command_test.py
+++ tests/integration/commands/declare_command_test.py
@@ -1,12 +1,17 @@
+from argparse import Namespace
 from pathlib import Path
+from unittest.mock import MagicMock
 
 import pytest
 from starknet_py.net.gateway_client import GatewayClient
 from starknet_py.net.models import StarknetChainId
 
+from protostar.cli import MessengerFactory
 from protostar.cli.signable_command_util import PRIVATE_KEY_ENV_VAR_NAME
+from protostar.commands import DeclareCairo1Command
+from protostar.io import log_color_provider
 from protostar.starknet import Address
-from protostar.starknet_gateway import FeeExceededMaxFeeException
+from protostar.starknet_gateway import FeeExceededMaxFeeException, GatewayFacadeFactory
 from tests.conftest import DevnetAccount, SetPrivateKeyEnvVarFixture
 from tests.data.contracts import CONTRACT_WITH_CONSTRUCTOR
 from tests.integration.conftest import CreateProtostarProjectFixture
@@ -24,6 +29,50 @@ def protostar_fixture(create_protostar_project: CreateProtostarProjectFixture):
 @pytest.fixture(name="compiled_contract_path")
 def compiled_contract_path_fixture() -> Path:
     return Path("./build/main.json")
+
+
+async def test_declaring_cairo1_contract(
+    devnet_gateway_url: str,
+    devnet_account: DevnetAccount,
+    set_private_key_env_var: SetPrivateKeyEnvVarFixture,
+    datadir: Path,
+):
+    # FIXME current compiler outputs don't work with declare
+    # contract_path = tmp_path / "balance.json"
+    # cairo_bindings.compile_starknet_contract_to_sierra_from_path(
+    #     input_path=datadir / "balance.cairo",
+    #     output_path=contract_path
+    # )
+    contract_path = datadir / "minimal_contract_compiled.json"
+
+    declare = DeclareCairo1Command(
+        gateway_facade_factory=GatewayFacadeFactory(Path("")),
+        messenger_factory=MessengerFactory(
+            log_color_provider=log_color_provider,
+            activity_indicator=MagicMock(),
+        ),
+    )
+
+    args = Namespace(
+        chain_id=StarknetChainId.TESTNET,
+        account_address=devnet_account.address,
+        contract=contract_path,
+        compiled_class_hash=0x56B935FA8AF97EC603A96A3E1870CA193DA4CF08B8DA3F12E023E67A82B5A7E,
+        gateway_url=devnet_gateway_url,
+        max_fee=int(1e16),
+        wait_for_acceptance=False,
+        json=False,
+        signer_class=None,
+        private_key_path=None,
+        network=None,
+        token=None,
+        block_explorer=None,
+    )
+
+    with set_private_key_env_var(devnet_account.private_key):
+        response = await declare.run(args)
+
+        assert response.class_hash is not None
 
 
 async def test_declaring_contract(
