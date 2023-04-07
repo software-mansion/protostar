deb212e2aad674bc871b8f32a4477c5a577f5066
Karol Bisztyga <karol.bisztyga@swmansion.com>
2023-04-07 09:37:42 +0200

    remove redundant keccak use

--- protostar/cheatable_starknet/callable_hint_locals/call_hint_local.py
+++ protostar/cheatable_starknet/callable_hint_locals/call_hint_local.py
@@ -1,8 +1,6 @@
 import asyncio
 from typing import Callable, Optional
 
-from starkware.starknet.public.abi import starknet_keccak
-
 from protostar.starknet import CheatcodeException, RawAddress, Address, Selector
 from protostar.cheatable_starknet.controllers.contracts import (
     ContractsCheaterException,
@@ -12,7 +10,7 @@ from protostar.cheatable_starknet.controllers.contracts import (
 from protostar.cheatable_starknet.controllers import ExpectCallController
 from protostar.cheatable_starknet.controllers.expect_call_controller import ExpectedCall
 from protostar.starknet.data_transformer import CairoOrPythonData
-from protostar.cairo.short_string import short_string_to_str
+from protostar.cairo.short_string import short_string_to_str, CairoShortString
 
 from .callable_hint_local import CallableHintLocal
 
@@ -32,21 +30,21 @@ class CallHintLocal(CallableHintLocal):
 
     def _build(
         self,
-    ) -> Callable[[RawAddress, int, Optional[CairoOrPythonData]], CallResult]:
+    ) -> Callable[
+        [RawAddress, CairoShortString, Optional[CairoOrPythonData]], CallResult
+    ]:
         return self.call
 
     def call(
         self,
         contract_address: RawAddress,
-        function_selector: int,
+        function_name: CairoShortString,
         calldata: Optional[CairoOrPythonData] = None,
     ) -> CallResult:
         return asyncio.run(
             self._call(
                 contract_address=Address.from_user_input(contract_address),
-                entry_point_selector=Selector(
-                    starknet_keccak(str.encode(short_string_to_str(function_selector)))
-                ),
+                entry_point_selector=Selector(short_string_to_str(function_name)),
                 calldata=calldata,
             )
         )
