340fb342c1d9ec1787507dcd433abc540575cb8a
Oleksii Bulenok <oleksii.bulenok@swmansion.com>
2023-04-07 17:34:13 +0200

    Use protostar_fixture_factory in ProtostarProjectFixture to create ProtostarFixture

--- tests/integration/_conftest/protostar_project_fixture.py
+++ tests/integration/_conftest/protostar_project_fixture.py
@@ -1,9 +1,8 @@
 import os
 from pathlib import Path
-from typing import Dict, List, Union, Generator, cast, Any
+from typing import Dict, Union, Generator, Any
 from contextlib import contextmanager
 
-import pytest
 from pytest_mock import MockerFixture
 
 from tests.conftest import ProtostarTmpPathFactory
@@ -16,50 +15,11 @@ from protostar.configuration_file.configuration_toml_interpreter import (
     ConfigurationTOMLInterpreter,
 )
 from protostar.cairo import CairoVersion
-from protostar.argument_parser import ArgumentParserFacade, CLIApp
-from protostar.cli import map_protostar_type_name_to_parser, MessengerFactory
-from protostar.commands import (
-    BuildCommand,
-    BuildCairo1Command,
-    CalculateAccountAddressCommand,
-    CallCommand,
-    DeclareCommand,
-    FormatCommand,
-    InitCommand,
-    InitCairo1Command,
-    InvokeCommand,
-    MulticallCommand,
-)
-from protostar.commands.deploy_account_command import DeployAccountCommand
-from protostar.commands.deploy_command import DeployCommand
-from protostar.commands.init.project_creator.new_project_creator import (
-    NewProjectCreator,
-)
-from protostar.commands.test import TestCommand
-from protostar.compiler import (
-    ProjectCairoPathBuilder,
-    LinkedLibrariesBuilder,
-    Cairo0ProjectCompiler,
-)
 from protostar.configuration_file import (
-    ConfigurationFileFactory,
     ConfigurationFileV2ContentFactory,
     ConfigurationTOMLContentBuilder,
 )
-from protostar.io import log_color_provider
-from protostar.io.input_requester import InputRequester
-from protostar.self.protostar_compatibility_with_project_checker import (
-    parse_protostar_version,
-)
-from protostar.self.protostar_directory import ProtostarDirectory
-
-from .protostar_fixture import ProtostarFixture
-from .spying_gateway_facade_factory import SpyingGatewayFacadeFactory
-from .transaction_registry import TransactionRegistry
-
-ContractMap = Dict[str, List[str]]
-
-REPOSITORY_ROOT = Path(__file__).parent.parent.parent.parent.resolve()
+from .protostar_fixture_factory import create_protostar_fixture
 
 
 @contextmanager
@@ -82,7 +42,9 @@ class ProtostarProjectFixture:
         self._project_root_path = tmp_path
 
         self.cwd = None
-        self.protostar = self.create_protostar_fixture()
+        self.protostar = self.create_protostar_fixture(
+            self._mocker, self._project_root_path
+        )
         self._create_protostar_project(cairo_version)
 
     def _create_protostar_project(self, cairo_version: CairoVersion):
@@ -97,7 +59,9 @@ class ProtostarProjectFixture:
         self._project_root_path = self._project_root_path / project_name
         os.chdir(self._project_root_path)
         # rebuilding protostar fixture to reload configuration file
-        self.protostar = self.create_protostar_fixture()
+        self.protostar = self.create_protostar_fixture(
+            self._mocker, self._project_root_path
+        )
 
     def __enter__(self):
         return self
@@ -106,157 +70,8 @@ class ProtostarProjectFixture:
         assert self.cwd
         os.chdir(self.cwd)
 
-    def create_protostar_fixture(self):
-        version_manager = self._mocker.MagicMock()
-        version_manager.protostar_version = self._mocker.MagicMock()
-        version_manager.protostar_version = "99.9.9"
-
-        configuration_file = ConfigurationFileFactory(
-            active_profile_name=None, cwd=self._project_root_path
-        ).create()
-        project_cairo_path_builder = ProjectCairoPathBuilder(
-            project_root_path=self._project_root_path,
-        )
-
-        project_compiler = Cairo0ProjectCompiler(
-            project_root_path=self._project_root_path,
-            project_cairo_path_builder=project_cairo_path_builder,
-            configuration_file=configuration_file,
-        )
-
-        input_requester = cast(InputRequester, self._mocker.MagicMock())
-
-        def request_input(message: str) -> str:
-            if message.startswith("project directory name"):
-                return self._project_root_path.name
-            if message.startswith("libraries directory name"):
-                return "lib"
-            return ""
-
-        input_requester.request_input = request_input
-
-        configuration_file_content_factory = ConfigurationFileV2ContentFactory(
-            content_builder=ConfigurationTOMLContentBuilder()
-        )
-
-        new_project_creator = NewProjectCreator(
-            script_root=REPOSITORY_ROOT,
-            requester=input_requester,
-            configuration_file_content_factory=configuration_file_content_factory,
-            protostar_version=parse_protostar_version("0.0.0"),
-            output_dir_path=self._project_root_path,
-        )
-
-        init_command = InitCommand(
-            input_requester,
-            new_project_creator=new_project_creator,
-            adapted_project_creator=self._mocker.MagicMock(),
-        )
-
-        init_cairo1_command = InitCairo1Command(
-            new_project_creator=new_project_creator,
-        )
-
-        messenger_factory = MessengerFactory(
-            log_color_provider=log_color_provider,
-            activity_indicator=fake_activity_indicator,
-        )
-
-        build_command = BuildCommand(
-            project_compiler=project_compiler,
-            messenger_factory=messenger_factory,
-        )
-
-        build_cairo1_command = BuildCairo1Command(
-            configuration_file=project_compiler.configuration_file,
-            project_root_path=self._project_root_path,
-        )
-
-        transaction_registry = TransactionRegistry()
-
-        gateway_facade_factory = SpyingGatewayFacadeFactory(
-            project_root_path=self._project_root_path,
-            transaction_registry=transaction_registry,
-        )
-
-        deploy_account_command = DeployAccountCommand(
-            gateway_facade_factory=gateway_facade_factory,
-            messenger_factory=messenger_factory,
-        )
-
-        format_command = FormatCommand(
-            project_root_path=self._project_root_path,
-            messenger_factory=messenger_factory,
-        )
-        declare_command = DeclareCommand(
-            gateway_facade_factory=gateway_facade_factory,
-            messenger_factory=messenger_factory,
-        )
-
-        deploy_command = DeployCommand(
-            gateway_facade_factory=gateway_facade_factory,
-            messenger_factory=messenger_factory,
-        )
-
-        test_command = TestCommand(
-            project_root_path=self._project_root_path,
-            protostar_directory=ProtostarDirectory(REPOSITORY_ROOT),
-            project_cairo_path_builder=LinkedLibrariesBuilder(),
-            log_color_provider=log_color_provider,
-            cwd=self._project_root_path,
-            active_profile_name=None,
-            messenger_factory=messenger_factory,
-        )
-
-        invoke_command = InvokeCommand(
-            gateway_facade_factory=gateway_facade_factory,
-            messenger_factory=messenger_factory,
-        )
-        call_command = CallCommand(
-            project_root_path=self._project_root_path,
-            gateway_facade_factory=gateway_facade_factory,
-            messenger_factory=messenger_factory,
-        )
-        calculate_account_address_command = CalculateAccountAddressCommand(
-            messenger_factory=messenger_factory
-        )
-        multicall_command = MulticallCommand(
-            gateway_facade_factory=gateway_facade_factory,
-            messenger_factory=messenger_factory,
-        )
-
-        cli_app = CLIApp(
-            commands=[
-                deploy_account_command,
-                calculate_account_address_command,
-                multicall_command,
-            ]
-        )
-        parser = ArgumentParserFacade(
-            cli_app, parser_resolver=map_protostar_type_name_to_parser
-        )
-
-        protostar_fixture = ProtostarFixture(
-            project_root_path=self._project_root_path,
-            init_command=init_command,
-            init_cairo1_command=init_cairo1_command,
-            call_command=call_command,
-            build_command=build_command,
-            build_cairo1_command=build_cairo1_command,
-            format_command=format_command,
-            declare_command=declare_command,
-            deploy_command=deploy_command,
-            test_command=test_command,
-            invoke_command=invoke_command,
-            deploy_account_command=deploy_account_command,
-            cli_app=cli_app,
-            parser=parser,
-            transaction_registry=transaction_registry,
-            calculate_account_address_command=calculate_account_address_command,
-            multicall_command=multicall_command,
-            monkeypatch=pytest.MonkeyPatch(),
-        )
-        return protostar_fixture
+    def create_protostar_fixture(self, mocker: MockerFixture, project_root_path: Path):
+        return create_protostar_fixture(mocker, project_root_path)
 
     @property
     def project_root_path(self) -> Path:
@@ -279,7 +94,9 @@ class ProtostarProjectFixture:
         }
         self.create_files(relative_path_str_to_file)
         self.add_contracts_to_protostar_toml(contract_name_to_file)
-        self.protostar = self.create_protostar_fixture()
+        self.protostar = self.create_protostar_fixture(
+            self._mocker, self._project_root_path
+        )
 
     def add_contracts_to_protostar_toml(
         self, contract_name_to_file: Dict[str, Union[str, Path]]
