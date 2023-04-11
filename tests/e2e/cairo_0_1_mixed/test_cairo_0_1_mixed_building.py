3e8a74aa4bfbc7b9a8aaa7c93edb358e6a65383d
Oleksii Bulenok <oleksii.bulenok@swmansion.com>
2023-04-11 15:02:08 +0200

    Iterate over contracts once in test_cairo1_build_with_contract_names_build_together's assert

--- tests/e2e/cairo_0_1_mixed/test_cairo_0_1_mixed_building.py
+++ tests/e2e/cairo_0_1_mixed/test_cairo_0_1_mixed_building.py
@@ -39,10 +39,12 @@ def test_cairo1_build_with_contract_names_build_together(
     protostar(["build-cairo1"])
     build_path = Path("build")
     assert build_path.exists()
-    built_files = list(build_path.glob("*"))
-    assert set(built_files) == set(
-        Path("build") / (contract + ".casm.json") for contract in contracts
-    ).union({Path("build") / (contract + ".sierra.json") for contract in contracts})
+    actual_built_files = set(list(build_path.glob("*")))
+    expected_built_files = set()
+    for contract in contracts:
+        expected_built_files.add(Path("build") / (contract + ".casm.json"))
+        expected_built_files.add(Path("build") / (contract + ".sierra.json"))
+    assert actual_built_files == expected_built_files
 
 
 def test_cairo0_build_with_contract_names_separate_builds(
