b91caf7efab536e095322d1539ea07ab1ff585f5
Karol Bisztyga <karol.bisztyga@swmansion.com>
2023-04-12 13:07:25 +0200

    fix pyright

--- tests/integration/cairo1_hint_locals/call/test_call.py
+++ tests/integration/cairo1_hint_locals/call/test_call.py
@@ -2,21 +2,23 @@ from pathlib import Path
 
 import pytest
 
-from tests.integration._conftest import ProtostarFixture
+from tests.integration._conftest import ProtostarProjectFixture
 from tests.integration.conftest import (
     CreateProtostarProjectFixture,
     assert_cairo_test_cases,
 )
 
 
-@pytest.fixture(name="protostar", scope="function")
+@pytest.fixture(name="protostar_project", scope="function")
 def protostar_fixture(create_protostar_project: CreateProtostarProjectFixture):
-    with create_protostar_project() as protostar:
-        yield protostar
+    with create_protostar_project() as protostar_project:
+        yield protostar_project
 
 
-async def test_call_hint_local(protostar: ProtostarFixture, shared_datadir: Path):
-    protostar.create_contracts(
+async def test_call_hint_local(
+    protostar_project: ProtostarProjectFixture, shared_datadir: Path
+):
+    protostar_project.create_contracts(
         {
             "simple": shared_datadir / "simple.cairo",
             "with_ctor": shared_datadir / "with_ctor.cairo",
@@ -25,7 +27,7 @@ async def test_call_hint_local(protostar: ProtostarFixture, shared_datadir: Path
         }
     )
 
-    testing_summary = await protostar.run_test_runner(
+    testing_summary = await protostar_project.protostar.run_test_runner(
         Path(__file__).parent / "call_test.cairo",
         cairo1_test_runner=True,
     )
