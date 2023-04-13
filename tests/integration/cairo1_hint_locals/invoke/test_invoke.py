6df03cbcaf81cc57117c5d9110cb9884971b20ab
Arcticae <tomekgsd@gmail.com>
2023-04-13 13:35:33 +0200

    Fix invoke hintlocal test

--- tests/integration/cairo1_hint_locals/invoke/test_invoke.py
+++ tests/integration/cairo1_hint_locals/invoke/test_invoke.py
@@ -2,21 +2,23 @@ from pathlib import Path
 
 import pytest
 
-from tests.integration._conftest import ProtostarFixture
+from tests.integration._conftest import ProtostarProjectFixture
 from tests.integration.conftest import (
     CreateProtostarProjectFixture,
     assert_cairo_test_cases,
 )
 
 
-@pytest.fixture(name="protostar", scope="function")
-def protostar_fixture(create_protostar_project: CreateProtostarProjectFixture):
+@pytest.fixture(name="protostar_project", scope="function")
+def protostar_project_fixture(create_protostar_project: CreateProtostarProjectFixture):
     with create_protostar_project() as protostar:
         yield protostar
 
 
-async def test_invoke_hint_local(protostar: ProtostarFixture, shared_datadir: Path):
-    protostar.create_contracts(
+async def test_invoke_hint_local(
+    protostar_project: ProtostarProjectFixture, shared_datadir: Path
+):
+    protostar_project.create_contracts(
         {
             "get_set": shared_datadir / "get_set.cairo",
             "get_set_with_ctor": shared_datadir / "get_set_with_ctor.cairo",
@@ -24,7 +26,7 @@ async def test_invoke_hint_local(protostar: ProtostarFixture, shared_datadir: Pa
         }
     )
 
-    testing_summary = await protostar.run_test_runner(
+    testing_summary = await protostar_project.protostar.run_test_runner(
         Path(__file__).parent / "invoke_test.cairo",
         cairo1_test_runner=True,
     )
