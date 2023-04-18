ebdb490b3069f8483a6d518ff23d1a52be4e0fbf
Artur Michalek <artur.michalek@swmansion.com>
2023-04-18 10:50:25 +0200

    Test init-cairo1 with existing name

--- tests/e2e/cairo1/test_cairo1_init.py
+++ tests/e2e/cairo1/test_cairo1_init.py
@@ -39,3 +39,15 @@ def test_init_without_name(protostar: ProtostarFixture):
     output = protostar(["init-cairo1"], expect_exit_code=1)
 
     assert "project directory name: " in output
+
+
+def test_init_fails_with_existing_folder(protostar: ProtostarFixture):
+    project_name = "my_project"
+    os.mkdir(project_name)
+
+    output = protostar(["init", project_name], expect_exit_code=1)
+
+    assert (
+        f"Folder or file named {project_name} already exists. Choose different project name."
+        in output
+    )
