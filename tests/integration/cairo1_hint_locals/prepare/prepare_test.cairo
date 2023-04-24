10b2ae60eb1343de1a7e19a73d9a96f2cb895cf4
Arcticae <tomekgsd@gmail.com>
2023-04-24 16:24:38 +0200

    Add cairo0 tests. Add test with a custom structure

--- tests/integration/cairo1_hint_locals/prepare/prepare_test.cairo
+++ tests/integration/cairo1_hint_locals/prepare/prepare_test.cairo
@@ -23,3 +23,43 @@ fn test_prepare_with_args() {
     calldata.append('name');
     drop(prepare(class_hash, calldata).unwrap());
 }
+
+#[test]
+fn test_prepare_cairo0() {
+    let class_hash = declare_cairo0('cairo0').unwrap();
+
+    drop(prepare(class_hash, ArrayTrait::new()).unwrap());
+
+}
+
+#[test]
+fn test_prepare_cairo0_w_ctor() {
+    let class_hash = declare_cairo0('cairo0_w_ctor').unwrap();
+
+    let mut calldata = ArrayTrait::new();
+    calldata.append(200);
+    drop(prepare(class_hash, calldata).unwrap());
+
+}
+
+#[test]
+fn test_prepare_cairo0_w_ctor_no_args() {
+    let class_hash = declare_cairo0('cairo0_w_ctor_no_args').unwrap();
+
+    drop(prepare(class_hash, ArrayTrait::new()).unwrap());
+}
+
+use serde::Serde;
+use minimal_custom_structures::MinimalContract::CustomStruct;
+use minimal_custom_structures::MinimalContract::CustomStructSerde;
+
+
+#[test]
+fn test_custom_structures_usage() {
+    let class_hash = declare('minimal_custom_structures').unwrap();
+    let mut args = ArrayTrait::new();
+    let cs = CustomStruct { a: 1, b: 2 };
+    CustomStructSerde::serialize(ref args, cs);
+
+    drop(prepare(class_hash, args).unwrap());
+}
--- tests/integration/cairo1_hint_locals/prepare/test_prepare.py
+++ tests/integration/cairo1_hint_locals/prepare/test_prepare.py
@@ -23,12 +23,21 @@ async def test_prepare_hint_local(
             "minimal_no_ctor": shared_datadir / "minimal_no_ctor",
             "minimal_no_args": shared_datadir / "minimal_no_args",
             "minimal_with_args": shared_datadir / "minimal_with_args",
+            "minimal_custom_structures": shared_datadir / "minimal_custom_structures",
+        }
+    )
+    protostar_project.create_contracts(
+        {
+            "cairo0": shared_datadir / "cairo0.cairo",
+            "cairo0_w_ctor": shared_datadir / "cairo0_w_ctor.cairo",
+            "cairo0_w_ctor_no_args": shared_datadir / "cairo0_w_ctor_no_args.cairo",
         }
     )
 
     testing_summary = await protostar_project.protostar.run_test_runner(
         Path(__file__).parent / "prepare_test.cairo",
         cairo1_test_runner=True,
+        cairo_path=[shared_datadir / "minimal_custom_structures"],
     )
 
     assert_cairo_test_cases(
@@ -37,5 +46,9 @@ async def test_prepare_hint_local(
             "test_prepare_no_ctor",
             "test_prepare_no_args",
             "test_prepare_with_args",
+            "test_prepare_cairo0",
+            "test_prepare_cairo0_w_ctor",
+            "test_prepare_cairo0_w_ctor_no_args",
+            "test_custom_structures_usage",
         ],
     )
