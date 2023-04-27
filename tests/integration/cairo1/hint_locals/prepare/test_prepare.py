ace7af01d7901c09557f150ef3d75d79ac5c0c22
Tomasz Rejowski <34059895+Arcticae@users.noreply.github.com>
2023-04-27 15:08:50 +0200

    Prepare extensive tests (#1821)
    Closes #1715

--- tests/integration/cairo1/hint_locals/prepare/test_prepare.py
+++ tests/integration/cairo1/hint_locals/prepare/test_prepare.py
@@ -25,6 +25,13 @@ async def test_prepare_hint_local(
             "minimal_with_args": shared_datadir / "minimal_with_args",
         }
     )
+    protostar_project.create_contracts(
+        {
+            "cairo0": shared_datadir / "cairo0.cairo",
+            "cairo0_w_ctor": shared_datadir / "cairo0_w_ctor.cairo",
+            "cairo0_w_ctor_no_args": shared_datadir / "cairo0_w_ctor_no_args.cairo",
+        }
+    )
 
     testing_summary = await protostar_project.protostar.test_cairo1(
         Path(__file__).parent / "prepare_test.cairo",
@@ -36,5 +43,8 @@ async def test_prepare_hint_local(
             "test_prepare_no_ctor",
             "test_prepare_no_args",
             "test_prepare_with_args",
+            "test_prepare_cairo0",
+            "test_prepare_cairo0_w_ctor",
+            "test_prepare_cairo0_w_ctor_no_args",
         ],
     )
