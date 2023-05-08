f02d4d1f57d4e6aeebf9a43d7198ffe91c958e82
Arcticae <tomekgsd@gmail.com>
2023-05-08 12:54:42 +0200

    Add warp cheatcode implementation + test

--- protostar/cheatable_starknet/cheatables/cheatable_execute_entry_point.py
+++ protostar/cheatable_starknet/cheatables/cheatable_execute_entry_point.py
@@ -1,10 +1,14 @@
 # pylint: disable=duplicate-code,protected-access
+import dataclasses
 import logging
 import re
 from dataclasses import dataclass
-from typing import Optional, cast
+from typing import Optional, cast, List
 from copy import deepcopy
 
+from starkware.starknet.builtins.segment_arena.segment_arena_builtin_runner import (
+    SegmentArenaBuiltinRunner,
+)
 from starkware.starknet.business_logic.execution.objects import (
     CallType,
     CallInfo,
@@ -12,8 +16,8 @@ from starkware.starknet.business_logic.execution.objects import (
     ExecutionResourcesManager,
 )
 from starkware.cairo.common.cairo_function_runner import CairoFunctionRunner
-from starkware.cairo.lang.vm.relocatable import RelocatableValue
-from starkware.python.utils import to_bytes
+from starkware.cairo.lang.vm.relocatable import RelocatableValue, MaybeRelocatable
+from starkware.python.utils import to_bytes, as_non_optional
 from starkware.starknet.business_logic.execution.execute_entry_point import (
     ExecuteEntryPoint,
     EntryPointArgs,
@@ -22,6 +26,7 @@ from starkware.starknet.business_logic.state.state import StateSyncifier
 from starkware.starknet.business_logic.state.state_api import State, SyncState
 from starkware.starknet.business_logic.utils import (
     get_call_result_for_version0_class,
+    get_call_result,
 )
 from starkware.starknet.core.os import os_utils
 from starkware.starknet.definitions.error_codes import StarknetErrorCode
@@ -37,6 +42,7 @@ from starkware.starkware_utils.error_handling import (
 from starkware.starknet.services.api.contract_class.contract_class import (
     EntryPointType,
     DeprecatedCompiledClass,
+    CompiledClass,
 )
 
 from protostar.starknet import Address
@@ -45,7 +51,8 @@ from protostar.cheatable_starknet.controllers.transaction_revert_exception impor
 )
 from protostar.starknet.selector import Selector
 
-from .cheatable_syscall_handler import CheatableSysCallHandler
+from .cheatable_deprecated_syscall_handler import CheatableDeprecatedSysCallHandler
+from .cheatable_syscall_handler import CheatableSyscallHandler
 
 FAULTY_CLASS_HASH = to_bytes(
     0x1A7820094FEAF82D53F53F214B81292D717E7BB9A92BB2488092CD306F3993F
@@ -113,7 +120,7 @@ class CheatableExecuteEntryPoint(ExecuteEntryPoint):
         assert isinstance(
             state, StateSyncifier
         ), "Sync state is not a state syncifier!"  # This should always be true
-        syscall_handler = CheatableSysCallHandler(
+        syscall_handler = CheatableDeprecatedSysCallHandler(
             execute_entry_point_cls=CheatableExecuteEntryPoint,
             tx_execution_context=tx_execution_context,
             state=state,
@@ -207,6 +214,124 @@ class CheatableExecuteEntryPoint(ExecuteEntryPoint):
             )
         except StarkException as ex:
             raise self._wrap_stark_exception(ex)
+
+    def _execute(
+        self,
+        state: SyncState,
+        compiled_class: CompiledClass,
+        class_hash: int,
+        resources_manager: ExecutionResourcesManager,
+        general_config: StarknetGeneralConfig,
+        tx_execution_context: TransactionExecutionContext,
+        support_reverted: bool,
+    ) -> CallInfo:
+        # Fix the current resources usage, in order to calculate the usage of this run at the end.
+        previous_cairo_usage = resources_manager.cairo_usage
+
+        # Create a dummy layout.
+        layout = dataclasses.replace(
+            STARKNET_LAYOUT_INSTANCE,
+            builtins={**STARKNET_LAYOUT_INSTANCE.builtins, "segment_arena": {}},
+        )
+
+        # Prepare runner.
+        entry_point = self._get_selected_entry_point(
+            compiled_class=compiled_class, class_hash=class_hash
+        )
+        program = compiled_class.get_runnable_program(
+            entrypoint_builtins=as_non_optional(entry_point.builtins)
+        )
+        with wrap_with_stark_exception(code=StarknetErrorCode.SECURITY_ERROR):
+            runner = CairoFunctionRunner(
+                program=program,
+                layout=layout,
+                additional_builtin_factories=dict(
+                    segment_arena=lambda name, included: SegmentArenaBuiltinRunner(
+                        included=included
+                    )
+                ),
+            )
+
+        # Prepare implicit arguments.
+        implicit_args = os_utils.prepare_os_implicit_args(
+            runner=runner, gas=self.initial_gas
+        )
+
+        # Prepare syscall handler.
+        initial_syscall_ptr = cast(RelocatableValue, implicit_args[-1])
+        # region: Modified Starknet Code
+        syscall_handler = CheatableSyscallHandler(
+            state=state,
+            resources_manager=resources_manager,
+            segments=runner.segments,
+            tx_execution_context=tx_execution_context,
+            initial_syscall_ptr=initial_syscall_ptr,
+            general_config=general_config,
+            entry_point=self,
+            support_reverted=support_reverted,
+        )
+        # endregion
+
+        # Load the builtin costs; Cairo 1.0 programs are expected to end with a `ret` opcode
+        # followed by a pointer to the builtin costs.
+        core_program_end_ptr = runner.program_base + len(runner.program.data)
+        builtin_costs = [0, 0, 0, 0, 0]
+        # Use allocate_segment to mark it as read-only.
+        builtin_cost_ptr = syscall_handler.allocate_segment(data=builtin_costs)
+        program_extra_data: List[MaybeRelocatable] = [
+            0x208B7FFF7FFF7FFE,
+            builtin_cost_ptr,
+        ]
+        runner.load_data(ptr=core_program_end_ptr, data=program_extra_data)
+
+        # Arrange all arguments.
+
+        # Allocate and mark the segment as read-only (to mark every input array as read-only).
+        calldata_start = syscall_handler.allocate_segment(data=self.calldata)
+        calldata_end = calldata_start + len(self.calldata)
+        entry_point_args: EntryPointArgs = [
+            # Note that unlike old classes, implicit arguments appear flat in the stack.
+            *implicit_args,
+            calldata_start,
+            calldata_end,
+        ]
+
+        # Run.
+        self._run(
+            runner=runner,
+            entry_point_offset=entry_point.offset,
+            entry_point_args=entry_point_args,
+            hint_locals={"syscall_handler": syscall_handler},
+            run_resources=tx_execution_context.run_resources,
+            program_segment_size=len(runner.program.data) + len(program_extra_data),
+            allow_tmp_segments=True,
+        )
+
+        # We should not count (possibly) unsued code as holes.
+        runner.mark_as_accessed(
+            address=core_program_end_ptr, size=len(program_extra_data)
+        )
+
+        # Complete validations.
+        os_utils.validate_and_process_os_implicit_args(
+            runner=runner,
+            syscall_handler=syscall_handler,
+            initial_implicit_args=implicit_args,
+        )
+
+        # Update resources usage (for the bouncer and fee calculation).
+        resources_manager.cairo_usage += runner.get_execution_resources()
+
+        # Build and return the call info.
+        return self._build_call_info(
+            class_hash=class_hash,
+            execution_resources=resources_manager.cairo_usage - previous_cairo_usage,
+            storage=syscall_handler.storage,
+            result=get_call_result(runner=runner, initial_gas=self.initial_gas),
+            events=syscall_handler.events,
+            l2_to_l1_messages=syscall_handler.l2_to_l1_messages,
+            internal_calls=syscall_handler.internal_calls,
+        )
 
     def _change_max_steps_in_general_config(
         self, general_config: StarknetGeneralConfig
--- protostar/cheatable_starknet/cheatables/cheatable_syscall_handler.py
+++ protostar/cheatable_starknet/cheatables/cheatable_syscall_handler.py
@@ -1,201 +1,49 @@
-# pylint: disable=duplicate-code
-from typing import cast, Optional, Any, TYPE_CHECKING
+from typing import cast
 
-from starkware.cairo.lang.compiler.preprocessor.flow import ReferenceManager
-from starkware.cairo.lang.compiler.program import CairoHint
 from starkware.cairo.lang.vm.relocatable import RelocatableValue
-from starkware.starknet.business_logic.execution.objects import CallType, CallResult
+from starkware.python.utils import as_non_optional
 from starkware.starknet.business_logic.state.state import StateSyncifier
-from starkware.starknet.business_logic.state.state_api_objects import BlockInfo
-from starkware.starknet.core.os.contract_address.contract_address import (
-    calculate_contract_address_from_hash,
-)
-from starkware.starknet.core.os.syscall_handler import (
-    DeprecatedBlSyscallHandler,
-)
-from starkware.starknet.core.os.syscall_utils import cast_to_int
-from starkware.starknet.definitions.constants import GasCost
-from starkware.starknet.security.secure_hints import HintsWhitelist
-from starkware.starknet.services.api.contract_class.contract_class import EntryPointType
+from starkware.starknet.core.os.syscall_handler import BusinessLogicSyscallHandler
 
-from protostar.starknet.address import Address
-from protostar.starknet.selector import Selector
-
-from protostar.cheatable_starknet.controllers.expect_call_controller import (
-    ExpectedCall,
-    ExpectCallController,
+from protostar.cheatable_starknet.cheatables.cheatable_cached_state import (
+    CheatableCachedState,
 )
 
-if TYPE_CHECKING:
-    from protostar.cheatable_starknet.cheatables.cheatable_cached_state import (
-        CheatableCachedState,
-    )
 
-
-class CheatableSysCallHandlerException(Exception):
-    def __init__(self, message: str):
-        self.message = message
-        super().__init__(message)
-
-
-class CheatableSysCallHandler(DeprecatedBlSyscallHandler):
-    def __init__(self, state: StateSyncifier, **kwargs: Any):
-        self.cheatable_state: "CheatableCachedState" = cast(
-            "CheatableCachedState", state.async_state
+class CheatableSyscallHandler(BusinessLogicSyscallHandler):
+    def _get_execution_info_ptr(self) -> RelocatableValue:
+        # region: Modified starknet code
+        # Prepare block info.
+        state_syncifier = cast(StateSyncifier, self.storage.state)
+        cheatable_state = cast(CheatableCachedState, state_syncifier.async_state)
+        python_block_info = cheatable_state.get_block_info(
+            self.entry_point.contract_address
         )
-
-        super().__init__(state=state, **kwargs)
-
-    @property
-    def block_info(self) -> BlockInfo:
-        return self.cheatable_state.get_block_info(self.contract_address)
-
-    @block_info.setter
-    def block_info(self, block_info: BlockInfo):
-        # Only called in constructor.
-        assert block_info == self.cheatable_state.block_info
-
-    def _get_caller_address(
-        self,
-        syscall_ptr: RelocatableValue,
-    ) -> int:
-        caller_address = super()._get_caller_address(syscall_ptr=syscall_ptr)
-        pranked_address = self.cheatable_state.get_pranked_address(
-            Address(self.contract_address)
-        )
-        return int(pranked_address) if pranked_address is not None else caller_address
-
-    def _call_contract(
-        self, syscall_ptr: RelocatableValue, syscall_name: str
-    ) -> CallResult:
-        # Parse request and prepare the call.
-        request = self._read_and_validate_syscall_request(
-            syscall_name=syscall_name, syscall_ptr=syscall_ptr
-        )
-        calldata = self.segments.memory.get_range_as_ints(
-            addr=request.calldata, size=request.calldata_size
-        )
-
-        code_address: Optional[int] = None
-        class_hash: Optional[int] = None
-        function_selector = Selector(cast(int, request.function_selector))
-        if syscall_name == "call_contract":
-            code_address = cast_to_int(request.contract_address)
-            contract_address = code_address
-            # region Modified Starknet code.
-            if (
-                mocked_response := self.cheatable_state.get_mocked_response(
-                    target_address=Address(contract_address),
-                    entrypoint=function_selector,
-                )
-            ) is not None:
-                return CallResult(
-                    retdata=mocked_response, gas_consumed=0, failure_flag=0
-                )
-            # endregion
-
-            caller_address = self.contract_address
-            entry_point_type = EntryPointType.EXTERNAL
-            call_type = CallType.CALL
-        elif syscall_name == "delegate_call":
-            code_address = cast_to_int(request.contract_address)
-            contract_address = self.contract_address
-            caller_address = self.caller_address
-            entry_point_type = EntryPointType.EXTERNAL
-            call_type = CallType.DELEGATE
-        elif syscall_name == "delegate_l1_handler":
-            code_address = cast_to_int(request.contract_address)
-            contract_address = self.contract_address
-            caller_address = self.caller_address
-            entry_point_type = EntryPointType.L1_HANDLER
-            call_type = CallType.DELEGATE
-        elif syscall_name == "library_call":
-            class_hash = cast_to_int(request.class_hash)
-            contract_address = self.contract_address
-            caller_address = self.caller_address
-            entry_point_type = EntryPointType.EXTERNAL
-            call_type = CallType.DELEGATE
-        elif syscall_name == "library_call_l1_handler":
-            class_hash = cast_to_int(request.class_hash)
-            contract_address = self.contract_address
-            caller_address = self.caller_address
-            entry_point_type = EntryPointType.L1_HANDLER
-            call_type = CallType.DELEGATE
-        else:
-            raise NotImplementedError(f"Unsupported call type {syscall_name}.")
-
-        # region Modified Starknet code.
-        ExpectCallController.remove_expected_call_static(
-            cheatable_state=self.cheatable_state,
-            expected_call_to_remove=ExpectedCall(
-                address=Address(contract_address),
-                fn_selector=function_selector,
-                calldata=calldata,
-            ),
-        )
-
         # endregion
-
-        call = self.execute_entry_point_cls(
-            call_type=call_type,
-            class_hash=class_hash,
-            contract_address=contract_address,
-            code_address=code_address,
-            entry_point_selector=cast_to_int(request.function_selector),
-            initial_gas=GasCost.INITIAL.value,
-            entry_point_type=entry_point_type,
-            calldata=calldata,
-            caller_address=caller_address,
+        block_info = self.structs.BlockInfo(
+            block_number=python_block_info.block_number,
+            block_timestamp=python_block_info.block_timestamp,
+            sequencer_address=as_non_optional(python_block_info.sequencer_address),
         )
-
-        return self.execute_entry_point(call=call)
-
-    def _deploy(self, syscall_ptr: RelocatableValue) -> int:
-        """
-        Initializes and runs the constructor of the new contract.
-        Returns the address of the newly deployed contract.
-        """
-        request = self._read_and_validate_syscall_request(
-            syscall_name="deploy", syscall_ptr=syscall_ptr
+        # Prepare transaction info.
+        signature = self.tx_execution_context.signature
+        signature_start = self.allocate_segment(data=signature)
+        tx_info = self.structs.TxInfo(
+            version=self.tx_execution_context.version,
+            account_contract_address=self.tx_execution_context.account_contract_address,
+            max_fee=self.tx_execution_context.max_fee,
+            signature_start=signature_start,
+            signature_end=signature_start + len(signature),
+            transaction_hash=self.tx_execution_context.transaction_hash,
+            chain_id=self.general_config.chain_id.value,
+            nonce=self.tx_execution_context.nonce,
         )
-        assert request.deploy_from_zero in [
-            0,
-            1,
-        ], "The deploy_from_zero field in the deploy system call must be 0 or 1."
-        constructor_calldata = self.segments.memory.get_range_as_ints(
-            addr=cast(RelocatableValue, request.constructor_calldata),
-            size=cast_to_int(request.constructor_calldata_size),
+        # Gather all info.
+        execution_info = self.structs.ExecutionInfo(
+            block_info=self.allocate_segment(data=block_info),
+            tx_info=self.allocate_segment(data=tx_info),
+            caller_address=self.entry_point.caller_address,
+            contract_address=self.entry_point.contract_address,
+            selector=self.entry_point.entry_point_selector,
         )
-        class_hash = cast_to_int(request.class_hash)
-
-        deployer_address = self.contract_address if request.deploy_from_zero == 0 else 0
-        contract_address = calculate_contract_address_from_hash(
-            salt=cast_to_int(request.contract_address_salt),
-            class_hash=class_hash,
-            constructor_calldata=constructor_calldata,
-            deployer_address=deployer_address,
-        )
-
-        # region Modified Starknet code.
-        self.cheatable_state.contract_address_to_class_hash_map[
-            Address(contract_address)
-        ] = class_hash
-        # endregion
-
-        # Instantiate the contract.
-        self.sync_state.deploy_contract(
-            contract_address=contract_address, class_hash=class_hash
-        )
-
-        self.execute_constructor_entry_point(
-            contract_address=contract_address,
-            class_hash=class_hash,
-            constructor_calldata=constructor_calldata,
-        )
-
-        return contract_address
-
-
-class CheatableHintsWhitelist(HintsWhitelist):
-    def verify_hint_secure(self, hint: CairoHint, reference_manager: ReferenceManager):
-        pass
+        return self.allocate_segment(data=execution_info)
