9e9bcc37a0f8c3385587434dbfc30172bffb3f03
Artur Michalek <artur.michalek@swmansion.com>
2023-05-26 15:07:45 +0200

    Run test per scarb package wip

--- protostar-rust/src/lib.rs
+++ protostar-rust/src/lib.rs
@@ -1,5 +1,6 @@
-use anyhow::{Context, Result};
-use std::collections::HashMap;
+use anyhow::{anyhow, Context, Result};
+use itertools::Itertools;
+use std::path::PathBuf;
 
 use cairo_lang_protostar::test_collector::collect_tests;
 use cairo_lang_runner::{RunResultValue, SierraCasmRunner};
@@ -16,28 +17,26 @@ fn run_result_value_to_string(run_result: RunResultValue) -> String {
     };
 }
 
-pub fn run_tests(input_path: &str) -> Result<()> {
-    let builtins = vec![
-        "GasBuiltin".to_string(),
-        "Pedersen".to_string(),
-        "RangeCheck".to_string(),
-        "bitwise".to_string(),
-        "ec_op".to_string(),
-    ];
-    let builtins = builtins.iter().collect();
+pub fn run_tests(input_path: &str, linked_libraries: Option<&Vec<PathBuf>>) -> Result<()> {
+    let builtins = vec!["GasBuiltin", "Pedersen", "RangeCheck", "bitwise", "ec_op"];
 
     let (sierra_program, test_configs) = collect_tests(
-        &input_path.to_owned(),
+        &input_path.to_string(),
         None,
-        Some(
-            vec!["/Users/arturmichalek/Coding/protostar/protostar-rust/pkg".to_string()]
+        linked_libraries.map(|linked_libraries| {
+            linked_libraries
                 .iter()
-                .collect(),
-        ),
+                .map(|path| {
+                    path.to_str()
+                        .with_context(|| format!("Failed to convert path to str: {:?}", path))
+                        .unwrap()
+                })
+                .collect()
+        }),
         Some(builtins),
     )?;
 
-    println!("Test configs = {:?}", test_configs);
+    dbg!(&test_configs);
 
     let runner =
         SierraCasmRunner::new(sierra_program, Some(Default::default()), Default::default())
--- protostar-rust/src/main.rs
+++ protostar-rust/src/main.rs
@@ -1,16 +1,19 @@
-use anyhow::Result;
+use anyhow::{anyhow, Context, Result};
 use clap::Parser;
 use rust_test_runner_bindings::run_tests;
-use scarb_metadata::MetadataCommand;
+use scarb_metadata::{Metadata, MetadataCommand, PackageId, PackageMetadata};
 use serde::Deserialize;
 use std::env::set_var;
 use std::path::PathBuf;
-use cairo_lang_sierra_to_casm::invocations::InvocationError;
+
+const CONTRACT_TARGET: &str = "starknet-contract";
 
 #[derive(Parser, Debug)]
 struct Args {
     // #[clap(required = true)]
-    paths: Vec<PathBuf>,
+    // paths: Vec<PathBuf>,
+    #[arg(short, long)]
+    linked_libraries: Option<Vec<PathBuf>>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -19,29 +22,60 @@ struct ProtostarConfig {
     ignore: Option<Vec<String>>,
     json: Option<bool>,
     last_failed: Option<bool>,
-    linked_libraries: Option<Vec<PathBuf>>,
     report_slowest_tests: Option<bool>,
 }
 
-fn fetch_protostar_config(cwd: &PathBuf, package_name: &str) -> Result<ProtostarConfig> {
-    let metadata = MetadataCommand::new()
-        .current_dir(cwd.parent().unwrap())
-        .inherit_stderr()
-        .exec()?;
-    let raw_protostar_metadata = metadata
+fn protostar_config_for_package(
+    metadata: &Metadata,
+    package: &PackageId,
+) -> Result<ProtostarConfig> {
+    let raw_metadata = metadata
         .packages
         .iter()
-        .find(|&x| x.name == package_name)
-        .unwrap()
+        .find(|pk| pk.id == *package)
+        .ok_or_else(|| anyhow!("Failed to find metadata for package = {package}"))?
         .tool_metadata("protostar")
-        .unwrap();
-    let protostar_metadata: ProtostarConfig =
-        serde_json::from_value(raw_protostar_metadata.clone())?;
+        .ok_or_else(|| anyhow!("Failed to find protostar config for package = {package}"))?
+        .clone();
+    let protostar_config: ProtostarConfig = serde_json::from_value(raw_metadata)?;
 
-    Ok(protostar_metadata)
+    Ok(protostar_config)
 }
 
-fn main() {
+fn dependencies_for_package(
+    metadata: &Metadata,
+    package: &PackageId,
+) -> Result<(PathBuf, Vec<PathBuf>)> {
+    let compilation_unit = metadata
+        .compilation_units
+        .iter()
+        .filter(|unit| unit.package == *package)
+        .min_by_key(|unit| match unit.target.name.as_str() {
+            name @ "starknet-contract" => (0, name),
+            name @ "lib" => (1, name),
+            name => (2, name),
+        })
+        .ok_or_else(|| anyhow!("Failed to find metadata for package = {package}"))?;
+
+    let base_path = metadata
+        .packages
+        .iter()
+        .find(|p| p.id == *package)
+        .ok_or_else(|| anyhow!("Failed to find metadata for package = {package}"))?
+        .root
+        .clone()
+        .into_std_path_buf();
+
+    let dependencies = compilation_unit
+        .components
+        .iter()
+        .map(|cu| cu.source_root().to_owned().into_std_path_buf())
+        .collect();
+
+    Ok((base_path, dependencies))
+}
+
+fn main() -> Result<()> {
     let args = Args::parse();
     // TODO resolve this path somehow
     set_var(
@@ -49,12 +83,32 @@ fn main() {
         "/Users/arturmichalek/Coding/protostar/cairo/Cargo.toml",
     );
 
-    let paths = &args.paths;
-    println!("{:?}", paths);
-    for path in paths {
-        let protostar_config = fetch_protostar_config(path, "pkg");
-        println!("{:?}", protostar_config);
+    let scarb_metadata = MetadataCommand::new()
+        .current_dir("/Users/arturmichalek/Coding/protostar/protostar-rust/pkg")
+        .inherit_stderr()
+        .exec()?;
 
-        run_tests(path.to_str().unwrap()).unwrap();
+    for package in &scarb_metadata.workspace.members {
+        let protostar_config = protostar_config_for_package(&scarb_metadata, package)?;
+        dbg!(&protostar_config);
+
+        let (base_path, dependencies) = dependencies_for_package(&scarb_metadata, package)?;
+        let dependencies = dependencies
+            .iter()
+            .filter(|d| d.to_str().map_or(true, |s| !s.contains("core/src")))
+            .cloned()
+            .collect::<Vec<_>>();
+        dbg!(&dependencies);
+        dbg!(&base_path);
+
+        run_tests(
+            "/Users/arturmichalek/Coding/protostar/protostar-rust/pkg",
+            Some(&dependencies),
+            // Some(&vec![PathBuf::from(
+            //     "/Users/arturmichalek/Coding/protostar/protostar-rust/pkg/src",
+            // )]),
+        )?;
     }
+
+    Ok(())
 }
