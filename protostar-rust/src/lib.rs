c8becd00659977171006dd2f7b64cfeb12998437
Artur Michalek <artur.michalek@swmansion.com>
2023-05-31 13:56:09 +0200

    Move methods to lib.rs

--- protostar-rust/src/lib.rs
+++ protostar-rust/src/lib.rs
@@ -5,6 +5,7 @@ use anyhow::{Context, Result};
 use camino::{Utf8Path, Utf8PathBuf};
 use serde::Deserialize;
 use walkdir::WalkDir;
+use scarb_metadata::{Metadata, PackageId};
 
 use cairo_lang_protostar::casm_generator::TestConfig;
 use cairo_lang_protostar::test_collector::{collect_tests, LinkedLibrary};
@@ -127,4 +128,58 @@ fn run_tests(tests: TestsFromFile, tests_stats: &mut TestsStats) -> Result<()> {
         pretty_printing::print_test_result(&config.name.clone(), &result.value);
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    
+}
+
+pub fn protostar_config_for_package(
+    metadata: &Metadata,
+    package: &PackageId,
+) -> Result<ProtostarTestConfig> {
+    let raw_metadata = metadata
+        .get_package(package)
+        .ok_or_else(|| anyhow!("Failed to find metadata for package = {package}"))?
+        .tool_metadata("protostar")
+        .ok_or_else(|| anyhow!("Failed to find protostar config for package = {package}"))?
+        .clone();
+    let protostar_config: ProtostarTestConfig = serde_json::from_value(raw_metadata)?;
+
+    Ok(protostar_config)
+}
+
+pub fn dependencies_for_package(
+    metadata: &Metadata,
+    package: &PackageId,
+) -> Result<(Utf8PathBuf, Vec<LinkedLibrary>)> {
+    let compilation_unit = metadata
+        .compilation_units
+        .iter()
+        .filter(|unit| unit.package == *package)
+        .min_by_key(|unit| match unit.target.name.as_str() {
+            name @ "starknet-contract" => (0, name),
+            name @ "lib" => (1, name),
+            name => (2, name),
+        })
+        .ok_or_else(|| anyhow!("Failed to find metadata for package = {package}"))?;
+
+    let base_path = metadata
+        .get_package(package)
+        .ok_or_else(|| anyhow!("Failed to find metadata for package = {package}"))?
+        .root
+        .clone();
+
+    let dependencies = compilation_unit
+        .components
+        .iter()
+        .filter(|du| !du.source_path.to_string().contains("core/src"))
+        .map(|cu| LinkedLibrary {
+            name: cu.name.clone(),
+            path: cu.source_root().to_owned().into_std_path_buf(),
+        })
+        .collect();
+
+    Ok((base_path, dependencies))
 }
