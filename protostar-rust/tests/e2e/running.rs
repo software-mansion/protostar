88d141a820a25a04f85022b5e54ba64653975b52
Artur Michalek <artur.michalek@swmansion.com>
2023-06-12 17:31:47 +0200

    Fix paths

--- protostar-rust/tests/e2e/running.rs
+++ protostar-rust/tests/e2e/running.rs
@@ -5,7 +5,7 @@ use indoc::indoc;
 #[test]
 fn running_tests() {
     let temp = assert_fs::TempDir::new().unwrap();
-    temp.copy_from("tests/resources/example_package", &["**/*"])
+    temp.copy_from("tests/data/example_package", &["**/*"])
         .unwrap();
 
     let snapbox = runner();
