9a5d3cd5da2875621dbf3f0876725dcde554f801
Oleksii Bulenok <63914185+abulenok@users.noreply.github.com>
2023-06-14 18:54:16 +0200

    Check if int is decodable to ascii before doing `short_string_to_str` (#2070)
    ### Checklist
    - [x] Relevant issue is linked
    - [ ] Docs updated/issue for docs created
    - [x] Added relevant tests
    
    ---------
    
    Co-authored-by: Piotr Magiera <56825108+piotmag769@users.noreply.github.com>

--- protostar/cairo/short_string.py
+++ protostar/cairo/short_string.py
@@ -1,8 +1,24 @@
 CairoShortString = int
 
+LAST_ASCII_CHAR_DEC_VAL = 127
+
 
 def short_string_to_str(value: CairoShortString) -> str:
     """
     Reverse of starkware.cairo.lang.compiler.test_utils.short_string_to_felt
     """
-    return value.to_bytes(length=32, byteorder="big").decode("ascii").lstrip("\x00")
+    return _to_bytes(value).decode("ascii").lstrip("\x00")
+
+
+def is_short_string(value: int) -> bool:
+    if value > int.from_bytes(b"\xff" * 31, "big"):
+        return False
+
+    for byte in _to_bytes(value):
+        if byte > LAST_ASCII_CHAR_DEC_VAL:
+            return False
+    return True
+
+
+def _to_bytes(value: int) -> bytes:
+    return value.to_bytes(length=32, byteorder="big")
