82245f688ee67c598027011722ffc91c35568a9c
Karol Bisztyga <karol.bisztyga@swmansion.com>
2023-06-20 14:43:55 +0200

    restore tests

--- protostar-rust/tests/e2e/running.rs
+++ protostar-rust/tests/e2e/running.rs
@@ -3,30 +3,30 @@ use indoc::indoc;
 
 use crate::common::runner::runner;
 
-// #[test]
-// fn run_simple_test() {
-//     let temp = assert_fs::TempDir::new().unwrap();
-//     temp.copy_from("tests/data/simple_test", &["**/*"]).unwrap();
+#[test]
+fn run_simple_test() {
+    let temp = assert_fs::TempDir::new().unwrap();
+    temp.copy_from("tests/data/simple_test", &["**/*"]).unwrap();
 
-//     let snapbox = runner();
+    let snapbox = runner();
 
-//     snapbox
-//         .current_dir(&temp)
-//         .assert()
-//         .success()
-//         .stdout_matches(indoc! {r#"Collected 6 test(s) and 3 test file(s)
-//             Running 1 test(s) from src/lib.cairo
-//             [PASS] [..]::test_fib
-//             Running 2 test(s) from tests/ext_function_test.cairo
-//             [PASS] ext_function_test::ext_function_test::test_my_test
-//             [PASS] ext_function_test::ext_function_test::test_simple
-//             Running 3 test(s) from tests/test_simple.cairo
-//             [PASS] test_simple::test_simple::test_simple
-//             [PASS] test_simple::test_simple::test_simple2
-//             [FAIL] test_simple::test_simple::test_failing failing check
-//             Tests: 5 passed, 1 failed
-//         "#});
-// }
+    snapbox
+        .current_dir(&temp)
+        .assert()
+        .success()
+        .stdout_matches(indoc! {r#"Collected 6 test(s) and 3 test file(s)
+            Running 1 test(s) from src/lib.cairo
+            [PASS] [..]::test_fib
+            Running 2 test(s) from tests/ext_function_test.cairo
+            [PASS] ext_function_test::ext_function_test::test_my_test
+            [PASS] ext_function_test::ext_function_test::test_simple
+            Running 3 test(s) from tests/test_simple.cairo
+            [PASS] test_simple::test_simple::test_simple
+            [PASS] test_simple::test_simple::test_simple2
+            [FAIL] test_simple::test_simple::test_failing failing check
+            Tests: 5 passed, 1 failed
+        "#});
+}
 
 #[test]
 fn run_declare_test() {
@@ -48,28 +48,28 @@ fn run_declare_test() {
         "#});
 }
 
-// #[test]
-// fn run_print_test() {
-//     let temp = assert_fs::TempDir::new().unwrap();
-//     temp.copy_from("tests/data/print_test", &["**/*"]).unwrap();
+#[test]
+fn run_print_test() {
+    let temp = assert_fs::TempDir::new().unwrap();
+    temp.copy_from("tests/data/print_test", &["**/*"]).unwrap();
 
-//     let snapbox = runner();
+    let snapbox = runner();
 
-//     snapbox
-//         .current_dir(&temp)
-//         .assert()
-//         .success()
-//         .stdout_matches(indoc! {r#"Collected 1 test(s) and 2 test file(s)
-//             Running 0 test(s) from src/lib.cairo
-//             Running 1 test(s) from tests/test_print.cairo
-//             original value: [123], converted to a string: [{]
-//             original value: [6381921], converted to a string: [aaa]
-//             original value: [3618502788666131213697322783095070105623107215331596699973092056135872020480]
-//             original value: [152]
-//             original value: [124], converted to a string: [|]
-//             original value: [149]
-//             original value: [439721161573], converted to a string: [false]
-//             [PASS] test_print::test_print::test_print
-//             Tests: 1 passed, 0 failed
-//         "#});
-// }
+    snapbox
+        .current_dir(&temp)
+        .assert()
+        .success()
+        .stdout_matches(indoc! {r#"Collected 1 test(s) and 2 test file(s)
+            Running 0 test(s) from src/lib.cairo
+            Running 1 test(s) from tests/test_print.cairo
+            original value: [123], converted to a string: [{]
+            original value: [6381921], converted to a string: [aaa]
+            original value: [3618502788666131213697322783095070105623107215331596699973092056135872020480]
+            original value: [152]
+            original value: [124], converted to a string: [|]
+            original value: [149]
+            original value: [439721161573], converted to a string: [false]
+            [PASS] test_print::test_print::test_print
+            Tests: 1 passed, 0 failed
+        "#});
+}
