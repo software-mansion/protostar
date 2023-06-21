153ef5421b889e3ee235feb39908d09e7215a70e
Radinyn <igorurbanik23@gmail.com>
2023-06-21 11:45:58 +0200

    Added `compiler.py` from starknet.py

--- tests/_conftest/compiled_account/compiled_account.py
+++ tests/_conftest/compiled_account/compiled_account.py
@@ -1,24 +1,12 @@
 from pathlib import Path
-import os
 
-from starkware.cairo.lang.compiler.constants import LIBS_DIR_ENVVAR
-
-from protostar.starknet import (
-    StarknetPassManagerFactory,
-    StarknetCompiler,
-    StarknetCompilerConfig,
-)
+from .compiler import Compiler
 
 
 def compile_account_contract_with_validate_deploy() -> str:
-    contract = StarknetCompiler(
-        config=StarknetCompilerConfig(
-            include_paths=list(filter(None, os.getenv(LIBS_DIR_ENVVAR, "").split(":"))),
-            disable_hint_validation=True,
-        ),
-        pass_manager_factory=StarknetPassManagerFactory,
-    ).compile_contract(
-        Path(__file__).parent / "account_contract_with_validate_deploy.cairo"
-    )
-
-    return contract.Schema().dumps(contract)
+    return Compiler(
+        contract_source=(
+            Path(__file__).parent / "account_contract_with_validate_deploy.cairo"
+        ).read_text("utf-8"),
+        is_account_contract=True,
+    ).compile_contract()
