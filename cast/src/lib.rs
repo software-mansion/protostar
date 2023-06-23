b1c063dba7d2c5768bd11d2187dba3ad4b6255c7
Wojciech Szymczyk <wojciech.szymczyk@swmansion.com>
2023-06-23 09:53:07 +0200

    Add MVP for cli (#2058)
    ### Checklist
    - [x] Relevant issue is linked
    - [ ] Docs updated/issue for docs created
    - [ ] Added relevant tests
    
    Closes https://github.com/software-mansion/protostar/issues/1948
    Closes https://github.com/software-mansion/protostar/issues/2104
    Closes https://github.com/software-mansion/protostar/issues/2118
    
    ---------
    
    Co-authored-by: Marcin Warchoł <61014013+war-in@users.noreply.github.com>
    Co-authored-by: Artur Michałek <52135326+cptartur@users.noreply.github.com>

---
+++ cast/src/lib.rs
@@ -0,0 +1,108 @@
+use std::collections::HashMap;
+use anyhow::{anyhow, Context, Result};
+use camino::Utf8PathBuf;
+use serde::{Deserialize, Serialize};
+use starknet::core::types::BlockId;
+use starknet::core::types::BlockTag::{Latest, Pending};
+use starknet::{
+    accounts::SingleOwnerAccount,
+    core::{chain_id, types::FieldElement},
+    providers::jsonrpc::{HttpTransport, JsonRpcClient},
+    signers::{LocalWallet, SigningKey},
+};
+use std::fs;
+use url::Url;
+
+#[derive(Deserialize, Serialize, Clone)]
+struct Account {
+    private_key: String,
+    public_key: String,
+    address: String,
+    salt: Option<String>,
+    deployed: Option<bool>,
+}
+
+pub enum Network {
+    Testnet,
+    Testnet2,
+    Mainnet,
+}
+
+impl Network {
+    #[must_use]
+    pub fn get_chain_id(&self) -> FieldElement {
+        match self {
+            Network::Testnet => chain_id::TESTNET,
+            Network::Testnet2 => chain_id::TESTNET2,
+            Network::Mainnet => chain_id::MAINNET,
+        }
+    }
+    #[must_use]
+    pub fn get_value(&self) -> &'static str {
+        match self {
+            Network::Testnet => "alpha-goerli",
+            Network::Testnet2 => "alpha-goerli2",
+            Network::Mainnet => "alpha-mainnet,",
+        }
+    }
+}
+
+pub fn get_provider(url: &str) -> Result<JsonRpcClient<HttpTransport>> {
+    let parsed_url = Url::parse(url)?;
+    let provider = JsonRpcClient::new(HttpTransport::new(parsed_url));
+    Ok(provider)
+}
+
+fn get_account_info(name: &str, chain_id: &str, path: &Utf8PathBuf) -> Result<Account> {
+    let accounts: HashMap<String, HashMap<String, Account>> =
+        serde_json::from_str(&fs::read_to_string(path)?)?;
+    let user = accounts
+        .get(chain_id)
+        .and_then(|accounts_map| accounts_map.get(name))
+        .cloned();
+
+    user.ok_or_else(|| anyhow!("Account {} not found under chain id {}", name, chain_id))
+}
+
+pub fn get_account<'a>(
+    name: &str,
+    accounts_file_path: &Utf8PathBuf,
+    provider: &'a JsonRpcClient<HttpTransport>,
+    network: &Network,
+) -> Result<SingleOwnerAccount<&'a JsonRpcClient<HttpTransport>, LocalWallet>> {
+    // todo: #2113 verify network with provider
+    let account_info = get_account_info(name, network.get_value(), accounts_file_path)?;
+    let signer = LocalWallet::from(SigningKey::from_secret_scalar(
+        FieldElement::from_hex_be(&account_info.private_key)
+            .with_context(|| format!("Failed to convert private key {} to FieldElement", &account_info.private_key))?,
+    ));
+    let address = FieldElement::from_hex_be(&account_info.address)
+        .with_context(|| format!("Failed to convert account address {} to FieldElement", &account_info.private_key))?;
+    let mut account = SingleOwnerAccount::new(provider, signer, address, network.get_chain_id());
+
+    Ok(account)
+}
+
+pub fn get_block_id(value: &str) -> Result<BlockId> {
+    // todo: add more block ids (hash, number)
+    match value {
+        "pending" => Ok(BlockId::Tag(Pending)),
+        "latest" => Ok(BlockId::Tag(Latest)),
+        _ => Err(anyhow::anyhow!(
+            "No such block id {}! Possible values are pending and latest for now.",
+            value
+        )),
+    }
+}
+
+pub fn get_network(name: &str) -> Result<Network> {
+    match name {
+        "testnet" => Ok(Network::Testnet),
+        "testnet2" => Ok(Network::Testnet2),
+        "mainnet" => Ok(Network::Mainnet),
+        _ => Err(anyhow::anyhow!(
+            "No such network {}! Possible values are testnet, testnet2, mainnet.",
+            name
+        )),
+    }
+}
---
+++ cast/src/main.rs
@@ -0,0 +1,109 @@
+use crate::starknet_commands::{call::Call, declare::Declare, invoke::Invoke};
+use anyhow::Result;
+use camino::Utf8PathBuf;
+use clap::{Parser, Subcommand};
+use console::style;
+use cast::{get_account, get_block_id, get_provider, get_network};
+
+mod starknet_commands;
+
+#[derive(Parser)]
+#[command(version)]
+#[command(about = "cast - a starknet-foundry CLI", long_about = None)]
+struct Cli {
+    /// RPC provider url address
+    #[clap(short = 'u', long = "url")]
+    rpc_url: String,
+
+    /// Network name, one of: testnet, testnet2, mainnet
+    #[clap(short = 'n', long = "network")]
+    network: Option<String>,
+
+    /// Account name to be used for contract declaration, defaults to __default__
+    #[clap(short = 'a', long = "account", default_value = "__default__")]
+    account: String,
+
+    /// Path to the file holding accounts info, defaults to ~/.starknet_accounts/starknet_open_zeppelin_accounts.json
+    #[clap(
+        short = 'f',
+        long = "accounts-file",
+        default_value = "~/.starknet_accounts/starknet_open_zeppelin_accounts.json"
+    )]
+    accounts_file_path: Utf8PathBuf,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Declare a contract
+    Declare(Declare),
+
+    /// Call a contract
+    Call(Call),
+
+    /// Invoke a contract
+    Invoke(Invoke),
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    // todo: #2052 take network from scarb config if flag not provided
+    let network_name = cli.network.unwrap_or_else(|| {
+        // todo: #2107
+        eprintln!("{}", style("No --network flag passed!").red());
+        std::process::exit(1);
+    });
+    let network = get_network(&network_name)?;
+    let provider = get_provider(&cli.rpc_url)?;
+
+    match cli.command {
+        Commands::Declare(declare) => {
+            let mut account =
+                get_account(&cli.account, &cli.accounts_file_path, &provider, &network)?;
+
+            let declared_contract = starknet_commands::declare::declare(
+                &declare.sierra_contract_path,
+                &declare.casm_contract_path,
+                &mut account,
+            )
+            .await?;
+            // todo: #2107
+            eprintln!("Class hash: {}", declared_contract.class_hash);
+            eprintln!("Transaction hash: {}", declared_contract.transaction_hash);
+            Ok(())
+        }
+        Commands::Call(args) => {
+            let block_id = get_block_id(&args.block_id).unwrap();
+
+            let result = starknet_commands::call::call(
+                args.contract_address.as_ref(),
+                args.function_name.as_ref(),
+                args.calldata.as_ref(),
+                &provider,
+                block_id.as_ref(),
+            )
+            .await?;
+
+            // todo (#2107): Normalize outputs in CLI
+            eprintln!("Call response: {result:?}");
+            Ok(())
+        }
+        Commands::Invoke(invoke) => {
+            let mut account =
+                get_account(&cli.account, &cli.accounts_file_path, &provider, &network)?;
+            starknet_commands::invoke::invoke(
+                &invoke.contract_address,
+                &invoke.entry_point_name,
+                invoke.calldata.iter().map(AsRef::as_ref).collect(),
+                invoke.max_fee.as_deref(),
+                &mut account,
+            )
+            .await?;
+            Ok(())
+        }
+    }
+}
