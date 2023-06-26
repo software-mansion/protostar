c88951972356af0825c1dbf40acdcf9abd42b67e
Artur Michalek <artur.michalek@swmansion.com>
2023-06-26 16:20:28 +0200

    Hack class_hash

--- protostar-rust/src/cheatcodes_hint_processor.rs
+++ protostar-rust/src/cheatcodes_hint_processor.rs
@@ -1,5 +1,7 @@
 use std::any::Any;
+use std::collections::hash_map::DefaultHasher;
 use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
 use std::path::PathBuf;
 use std::{fs, i64};
 
@@ -9,7 +11,8 @@ use blockifier::execution::contract_class::{
     ContractClass as BlockifierContractClass, ContractClassV1,
 };
 use blockifier::state::cached_state::CachedState;
-use blockifier::test_utils::DictStateReader;
+use blockifier::state::state_api::StateReader;
+use blockifier::test_utils::{DictStateReader, TEST_ACCOUNT_CONTRACT_ADDRESS};
 use blockifier::transaction::account_transaction::AccountTransaction;
 use blockifier::transaction::transaction_utils_for_protostar::{
     declare_tx_default, deploy_account_tx,
@@ -23,7 +26,7 @@ use cairo_vm::types::exec_scope::ExecutionScopes;
 use cairo_vm::vm::errors::hint_errors::HintError;
 use cairo_vm::vm::vm_core::VirtualMachine;
 use num_traits::cast::ToPrimitive;
-use starknet_api::transaction::Fee;
+use starknet_api::transaction::{DeclareTransactionV0V1, Fee};
 
 use cairo_lang_casm::hints::Hint;
 use cairo_lang_casm::hints::ProtostarHint;
@@ -36,6 +39,9 @@ use cairo_vm::hint_processor::hint_processor_definition::HintReference;
 use cairo_vm::serde::deserialize_program::ApTracking;
 use cairo_vm::vm::errors::vm_errors::VirtualMachineError;
 use serde::Deserialize;
+use starknet_api::core::{ClassHash, ContractAddress, PatriciaKey};
+use starknet_api::hash::{StarkFelt, StarkHash};
+use starknet_api::{patricia_key, stark_felt};
 
 pub struct CairoHintProcessor<'a> {
     pub original_cairo_hint_processor: OriginalCairoHintProcessor<'a>,
@@ -150,8 +156,6 @@ fn execute_cheatcode_hint(
             }).unwrap_or_else(|| panic!("Failed to find contract {contract_value_as_short_str} in starknet_artifacts.json"));
             let sierra_path = current_dir.join(sierra_path);
 
-            dbg!(&sierra_path);
-
             let file = std::fs::File::open(&sierra_path)
                 .unwrap_or_else(|_| panic!("Failed to open file at path = {:?}", &sierra_path));
             let sierra_contract_class: ContractClass = serde_json::from_reader(&file)
@@ -167,7 +171,24 @@ fn execute_cheatcode_hint(
                 .expect("Failed to read contract class from json");
             let contract_class = BlockifierContractClass::V1(contract_class);
 
+            // TODO replace this. Hash should be calculated by the blockifier in the correct manner. This is just a workaround.
+            let mut hasher = DefaultHasher::new();
+            casm_serialized.hash(&mut hasher);
+            let class_hash = hasher.finish();
+            let class_hash = ClassHash(stark_felt!(class_hash));
+
+            let nonce = blockifier_state
+                .get_nonce_at(ContractAddress(patricia_key!(
+                    TEST_ACCOUNT_CONTRACT_ADDRESS
+                )))
+                .expect("Failed to get nonce");
+
             let declare_tx = declare_tx_default();
+            let declare_tx = DeclareTransactionV0V1 {
+                nonce,
+                class_hash,
+                ..declare_tx
+            };
             let tx = DeclareTransaction {
                 tx: starknet_api::transaction::DeclareTransaction::V1(declare_tx),
                 contract_class,
@@ -179,13 +200,6 @@ fn execute_cheatcode_hint(
                 .execute(blockifier_state, block_context)
                 .expect("Failed to execute declare transaction");
 
-            let class_hash = actual_execution_info
-                .validate_call_info
-                .as_ref()
-                .expect("Failed to read validate call info of transaction")
-                .call
-                .class_hash
-                .expect("Failed to read class hash of transaction");
             let class_hash_int =
                 i64::from_str_radix(&class_hash.to_string().replace("0x", "")[..], 16)
                     .expect("Failed to convert hex string to int");
