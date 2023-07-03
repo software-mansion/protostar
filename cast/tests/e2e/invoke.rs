663f9bec63d4dba2ac1173241f29e870ec39e132
war-in <marcin.warchol20@gmail.com>
2023-07-03 10:59:23 +0200

    one more test

--- cast/tests/e2e/invoke.rs
+++ cast/tests/e2e/invoke.rs
@@ -77,6 +77,40 @@ fn test_wrong_function_name() {
 }
 
 #[test]
+fn test_wrong_calldata() {
+    declare_simple_balance_contract();
+    deploy_simple_balance_contract();
+
+    let mut args = common_cli_args();
+    args.append(&mut vec![
+        "invoke",
+        "--contract-address",
+        "0x2bd89651521ec94a7c497c53f6b4555eeecef8b2221350dc5a04aa14ba41d68",
+        "--entry-point-name",
+        "increase_balance",
+        "--calldata",
+        "0x1ab93",
+        "--calldata",
+        "0x1"
+    ]);
+
+    let snapbox = runner(&args);
+
+    snapbox.assert().success().stderr_matches(indoc! {r#"
+        error: Error at pc=0:81:
+        Got an exception while executing a hint.
+        Cairo traceback (most recent call last):
+        Unknown location (pc=0:731)
+        Unknown location (pc=0:677)
+        Unknown location (pc=0:291)
+        Unknown location (pc=0:314)
+
+        Error in the called contract (0x2bd89651521ec94a7c497c53f6b4555eeecef8b2221350dc5a04aa14ba41d68):
+        Execution was reverted; failure reason: [0x496e70757420746f6f206c6f6e6720666f7220617267756d656e7473].
+    "#});
+}
+
+#[test]
 fn test_too_low_max_fee() {
     declare_simple_balance_contract();
     deploy_simple_balance_contract();
