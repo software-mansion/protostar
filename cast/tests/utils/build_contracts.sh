3a71b040c3f44a5684a8c81b0100408e951c4a82
war-in <marcin.warchol20@gmail.com>
2023-07-04 10:00:11 +0200

    build all contracts with scarb

---
+++ cast/tests/utils/build_contracts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CONTRACTS_DIRECTORY="$(git rev-parse --show-toplevel)/cast/tests/data/contracts"
+
+if command -v scarb &> /dev/null; then
+  for contract_dir in "$CONTRACTS_DIRECTORY"/*; do
+    if ! test -d "$contract_dir"/target; then
+      pushd $contract_dir
+      scarb build
+      popd
+    fi
+  done
+
+else
+  printf "Please run prepare_for_tests.sh\n"
+fi
--- cast/tests/utils/prepare_for_tests.sh
+++ cast/tests/utils/prepare_for_tests.sh
@@ -5,7 +5,7 @@ if ! which starknet-devnet > /dev/null 2>&1; then
   exit 1
 fi
 
-DIRECTORY="$(git rev-parse --show-toplevel)/cast/tests/utils/"
+UTILS_DIRECTORY="$(git rev-parse --show-toplevel)/cast/tests/utils/"
 CAIRO_REPO="https://github.com/starkware-libs/cairo/releases/download"
 COMPILER_VERSION="v1.1.1"
 
@@ -13,15 +13,15 @@ SCARB_VERSION="0.4.1"
 
 if [ ! -x "$COMPILER_DIRECTORY/cairo/bin/starknet-sierra-compile" ]; then
   if [[ $(uname -s) == 'Darwin' ]]; then
-    wget "$CAIRO_REPO/$COMPILER_VERSION/release-aarch64-apple-darwin.tar" -P "$DIRECTORY" || exit 1
-    pushd "$DIRECTORY"
-    tar -xvf "$DIRECTORY/release-aarch64-apple-darwin.tar" cairo/bin/starknet-sierra-compile || exit 1
+    wget "$CAIRO_REPO/$COMPILER_VERSION/release-aarch64-apple-darwin.tar" -P "$UTILS_DIRECTORY" || exit 1
+    pushd "$UTILS_DIRECTORY"
+    tar -xvf "$UTILS_DIRECTORY/release-aarch64-apple-darwin.tar" cairo/bin/starknet-sierra-compile || exit 1
     popd
 
   elif [[ $(uname -s) == 'Linux' ]]; then
-    wget "$CAIRO_REPO/$COMPILER_VERSION/release-x86_64-unknown-linux-musl.tar.gz" -P "$DIRECTORY" || exit 1
-    pushd "$DIRECTORY"
-    tar -xzvf "$DIRECTORY/release-x86_64-unknown-linux-musl.tar.gz" cairo/bin/starknet-sierra-compile || exit 1
+    wget "$CAIRO_REPO/$COMPILER_VERSION/release-x86_64-unknown-linux-musl.tar.gz" -P "$UTILS_DIRECTORY" || exit 1
+    pushd "$UTILS_DIRECTORY"
+    tar -xzvf "$UTILS_DIRECTORY/release-x86_64-unknown-linux-musl.tar.gz" cairo/bin/starknet-sierra-compile || exit 1
     popd
   fi
 fi
